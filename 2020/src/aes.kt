771df6e47f0b7a0ff0212fcbe36207d5b1126549
kk <kyoya.p@gmail.com>
2020-04-15 08:37:03 +0900

    no

--- 2020/src/aes.kt
+++ 2020/src/aes.kt
@@ -1,21 +1,25 @@
+import java.util.Base64
 import javax.crypto.Cipher
+import javax.crypto.spec.IvParameterSpec
 import javax.crypto.spec.SecretKeySpec
+import kotlin.random.Random
 
 fun main() {
-    val enc = "abcdefg".toByteArray(Charsets.UTF_8).encript("0123456789abcdef")
-    val dec = enc.decript("0123456789abcdef").let { String(it, Charsets.UTF_8) }
-
+    val enc = "abcdefg".encript()
+    println(enc)
+    val dec = enc.decript()
     println(dec)
 }
 
-fun ByteArray.encript(key: String): ByteArray {
+// mode: Cipher.ENCRYPT_MODE or Cipher.DECRYPT_MODE
+fun ByteArray.cript(mode: Int): ByteArray {
+    val key = SecretKeySpec("0123456789abcdef".toByteArray(), "AES")
+    val iv = IvParameterSpec("0123456789abcdef".toByteArray())
     val cipher = Cipher.getInstance("AES/CBC/PKCS5Padding")
-    cipher.init(Cipher.ENCRYPT_MODE, SecretKeySpec(key.toByteArray(), "AES"))
+    cipher.init(mode, key, iv)
     return cipher.doFinal(this)
 }
 
-fun ByteArray.decript(key: String): ByteArray {
-    val cipher = Cipher.getInstance("AES/CBC/PKCS5Padding")
-    cipher.init(Cipher.DECRYPT_MODE, SecretKeySpec(key.toByteArray(), "AES"))
-    return cipher.doFinal(this)
-}
+fun solt() = Random.Default.nextBytes(8)
+fun String.encript() = toByteArray(Charsets.UTF_8).let { solt() + it }.cript(Cipher.ENCRYPT_MODE).let { String(Base64.getEncoder().encode(it), Charsets.UTF_8) }
+fun String.decript() = Base64.getDecoder().decode(this).cript(Cipher.DECRYPT_MODE).drop(8).let{String(it.toByteArray(),Charsets.UTF_8)}
