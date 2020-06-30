50bd1e79da67b6f8ff2c379a847cec3fc902a3b4
kk <kyoya.p@gmail.com>
2020-06-30 17:46:20 +0900

    no

---
+++ snmp/src/walk.kt
@@ -0,0 +1,40 @@
+package mibtool
+
+import com.charleskorn.kaml.Yaml
+import org.snmp4j.CommunityTarget
+import org.snmp4j.PDU
+import org.snmp4j.Snmp
+import org.snmp4j.asn1.BER.ENDOFMIBVIEW
+import org.snmp4j.mp.SnmpConstants
+import org.snmp4j.smi.*
+import org.snmp4j.transport.DefaultUdpTransportMapping
+import java.net.InetAddress
+
+fun main(args: Array<String>) {
+    if (args.size == 0) {
+        println("syntax: java -jar walk.jar params...")
+        println("example: java -jar walk.jar addr: 192.168.1.2, oids: [1.3.6.1,1.3.6.2] ")
+        println("params=")
+        println(Yaml.default.stringify(SnmpParams.serializer(), SnmpParams("<addr>")))
+        System.exit(-1)
+    }
+    val p = Yaml.default.parse(SnmpParams.serializer(), "{" + args.joinToString(" ") + "}")
+
+    val transport = DefaultUdpTransportMapping()
+    val snmp = Snmp(transport)
+    transport.listen()
+
+    val target = CommunityTarget(UdpAddress(InetAddress.getByName(p.addr), 161), OctetString(p.comm))
+    target.version = SnmpConstants.version2c
+    target.timeout = 5_000 //ms
+
+    val initVbl = listOf(VariableBinding(OID(p.oid)))
+    snmp.walk(initVbl, target).map { it[0] }.map { VB(it.oid.toString(), it.variable) }.forEach {
+        println(Yaml.default.stringify(VB.serializer(), it))
+    }
+}
+
+fun <A : Address> Snmp.walk(initVbl: List<VariableBinding>, target: CommunityTarget<A>) = generateSequence(initVbl) { vbl ->
+    send(PDU(PDU.GETNEXT, vbl), target).response.variableBindings
+}.drop(1).takeWhile { it.zip(initVbl).any { (vb, ivb) -> vb.syntax != ENDOFMIBVIEW && vb.oid.startsWith(ivb.oid) } }
+
