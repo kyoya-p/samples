fd5d09c370aa57d8556a948819851c73c600e9ce
kk <kyoya.p@gmail.com>
2020-10-27 18:35:35 +0900

    n

--- 2020kotlin/src/s7.flow.scope2.kt
+++ 2020kotlin/src/s7.flow.scope2.kt
@@ -1,62 +1,65 @@
 import kotlinx.coroutines.*
 import kotlinx.coroutines.channels.awaitClose
 import kotlinx.coroutines.flow.*
+import java.util.*
 
 @ExperimentalCoroutinesApi
 fun main(): Unit = runBlocking {
+    println("sample0-----")
+    reset()
+    srcFlow(3, 100).collect { aTask(it) } // aTaskが完了するまで次のFlowは保留
+
     println("sample1-----")
-    srcFlow(5).toAdd(3).collect { println(it) }
+    reset()
+    srcFlow(3, 100).collectLatest { aTask(it) } //collectLatestによって実行中のaTask()はcancelされる
 
     println("sample2-----")
-    val j1 = launch { srcFlow(5).toAdd(3).collect { println(it) } }
-    delay(200)
-    j1.cancel()
-
-    println("sample3-----")
-    val j3 = launch {
-        srcFlow(1).toAdd(3).collect { r ->
-            launch {
-                repeat(r) { i ->
-                    println("$r $i")
-                    delay(100)
-                }
-            }
-        }
+    reset()
+    val j2 = launch {
+        srcFlow(3, 100).collectLatest { aTask(it) }
     }
     delay(200)
+    j2.cancel() //全体フローも200msで明示的にcancelされる
+
+    println("sample3-----")
+    reset()
+    val j3 = launch {
+        srcFlow(3, 100).collect { launch { aTask(it) } } // aTask()は同時実行される
+    }
+    delay(500)
     j3.cancel()
 
     println("sample4-----")
-    launch {
-        srcFlow(3).toAdd(1).toTimes(100).rescheduleFlow(5).collect {
-            println(it)
-        }
+    reset()
+    val j4 = launch {
+        launch { srcFlow(3, 100).collectLatest { aTask(it) } }
+        launch { srcFlow(3, 100).collectLatest { aTask(it + 10) } }
     }
+    delay(200)
+    j4.cancel() //複数のフローも200msで明示的にcancelされる
+
 }
 
-@ExperimentalCoroutinesApi
-fun srcFlow(r: Int) = callbackFlow {
-    repeat(r) {
-        offer(it)
+var start = 0L
+fun reset() {
+    start = Date().time
+}
+
+fun rap() = "000${(Date().time - start) % 10000}".takeLast(4)
+
+suspend fun aTask(id: Int) {
+    println("${rap()} Start task $id")
+    repeat(5) { j ->
+        println("${rap()} task=$id : $j ${if (j == 4) 'x' else ' '}")
         delay(100)
     }
-    close()
-    awaitClose()
 }
 
 @ExperimentalCoroutinesApi
-fun Flow<Int>.toAdd(x: Int) = channelFlow {
-    this@toAdd.collect {
-        offer(it + x)
-    }
-    close()
-    awaitClose()
-}
-
-@ExperimentalCoroutinesApi
-fun Flow<Int>.toTimes(x: Int) = channelFlow {
-    this@toTimes.collect {
-        offer(it * x)
+fun srcFlow(times: Int, inteval: Long) = callbackFlow {
+    repeat(times) {
+        offer(it)
+        delay(inteval)
     }
     close()
     awaitClose()
@@ -64,7 +67,7 @@ fun Flow<Int>.toTimes(x: Int) = channelFlow {
 
 @ExperimentalCoroutinesApi
 fun Flow<Int>.rescheduleFlow(x: Int) = channelFlow {
-    this@rescheduleFlow.collectLatest { sc ->
+    collectLatest { sc ->
         launch {
             for (i in 0..x) {
                 offer(sc + i)
