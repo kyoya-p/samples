7bdbc24aa63da40d16a701465df1b891e25f0bed
kyoya <kyoya.p@gmail.com>
2020-10-30 08:07:25 +0900

    Initiate project

--- fsCustomAuthSvr/src/aes.kt
+++ fsCustomAuthSvr/src/aes.kt
@@ -6,6 +6,16 @@ import javax.crypto.spec.IvParameterSpec
 import javax.crypto.spec.SecretKeySpec
 import kotlin.random.Random
 
+fun main(args: Array<String>) {
+    if (args.size != 2) {
+        println("usage: aesKt srcfile keystring")
+        System.exit(-1)
+    }
+    val fn = args[0]
+    val key = args[1]
+    //TODO
+}
+
 // mode: Cipher.ENCRYPT_MODE or Cipher.DECRYPT_MODE
 fun ByteArray.cript(mode: Int): ByteArray {
     val key = SecretKeySpec("0123456789abcdef".toByteArray(), "AES")
@@ -17,4 +27,4 @@ fun ByteArray.cript(mode: Int): ByteArray {
 
 fun solt() = Random.Default.nextBytes(8)
 fun String.encript() = toByteArray(Charsets.UTF_8).let { solt() + it }.cript(Cipher.ENCRYPT_MODE).let { String(Base64.getEncoder().encode(it), Charsets.UTF_8) }
-fun String.decript() = Base64.getDecoder().decode(this).cript(Cipher.DECRYPT_MODE).drop(8).let{String(it.toByteArray(),Charsets.UTF_8)}
+fun String.decript() = Base64.getDecoder().decode(this).cript(Cipher.DECRYPT_MODE).drop(8).let { String(it.toByteArray(), Charsets.UTF_8) }
