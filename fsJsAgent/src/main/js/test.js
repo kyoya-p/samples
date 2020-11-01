f9b670d1ceeefbe11d8de7faed863d33299f25e3
kyoya <kyoya.p@gmail.com>
2020-11-02 08:00:14 +0900

    Nodejs版Firestoreクライアント実装(途中)

---
+++ fsJsAgent/src/main/js/test.js
@@ -0,0 +1,40 @@
+var firebase = require("firebase/app");
+require("firebase/auth");
+require("firebase/firestore");
+
+firebase.initializeApp({
+        apiKey: "AIzaSyDrO7W7Sb6RCpHTsY3GaP-zODRP_HtY4nI",
+        authDomain: "road-to-iot.firebaseapp.com",
+        projectId: "road-to-iot"
+    });
+
+firebase.auth().signInWithEmailAndPassword("kyoya.p4@gmail.com", "kyoyap4").catch(function(error) {
+  var errorCode = error.code;
+  var errorMessage = error.message;
+  console.log(errorCode);
+  console.log(errorMessage);
+});
+
+firebase.auth().onAuthStateChanged(function(user) {
+  if (user) {
+    console.log("Signed-in: "+user.email);
+    dbOperation()
+  } else {
+    console.log("Not signed-in");
+  }
+});
+
+function dbOperation() {
+    var db = firebase.firestore();
+    var docRef = db.collection("device").doc("agent1");
+
+    docRef.get().then(function(doc) {
+        if (doc.exists) {
+            console.log("Document data:", doc.data());
+        } else {
+            console.log("No such document!");
+        }
+    }).catch(function(error) {
+        console.log("Error getting document:", error);
+    });
+}
--- fsJsAgent/src/main/kotlin/AgentMain.kt
+++ fsJsAgent/src/main/kotlin/AgentMain.kt
@@ -1,6 +1,36 @@
 external fun require(module: String): dynamic //javascriptのrequire()を呼ぶ
 
 fun main() {
+    firestoreTest()
+}
+
+fun firestoreTest() {
+
+    println("Start Firestore Client.")
+    // Firestore接続
+    // See: https://firebase.google.com/docs/firestore/quickstart?hl=ja
+    val firebase = require("firebase/app")
+    require("firebase/auth")
+    require("firebase/firestore")
+    firebase.initializeApp(
+        mapOf(
+            "apiKey" to "AIzaSyDrO7W7Sb6RCpHTsY3GaP-zODRP_HtY4nI",
+            "authDomain" to "road-to-iot.firebaseapp.com",
+            "projectId" to "road-to-iot"
+        )
+    )
+    val db = firebase.firestore()
+
+    // Firebase認証
+    // See: https://firebase.google.com/docs/auth/web/password-auth?hl=ja
+    try {
+        firebase.auth().signInWithEmailAndPassword("kyoya.p4@gmail.com", "kyoyap4")
+    } catch (e: Exception) {
+        e.printStackTrace()
+    }
+}
+
+fun webServerTest() {
     val exp = require("express")
     val svr = exp()
     svr.get("/") { _, res ->
