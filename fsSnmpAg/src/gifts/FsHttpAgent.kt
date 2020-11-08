2ab32414e58ded00b74dd95383278712eeab973e
kyoya <kyoya.p@gmail.com>
2020-11-08 15:31:05 +0900

    multi agent化

--- fsSnmpAg/src/gifts/FsHttpAgent.kt
+++ fsSnmpAg/src/gifts/FsHttpAgent.kt
@@ -4,27 +4,26 @@ import firestoreInterOp.firestoreDocumentFlow
 import gdvm.agent.mib.*
 import io.ktor.client.*
 import io.ktor.client.request.*
+import io.ktor.client.statement.*
 import io.ktor.http.*
-import kotlinx.coroutines.InternalCoroutinesApi
+import io.ktor.util.*
+import kotlinx.coroutines.*
 import kotlinx.coroutines.NonCancellable.isActive
-import kotlinx.coroutines.delay
 import kotlinx.coroutines.flow.collectLatest
-import kotlinx.coroutines.launch
-import kotlinx.coroutines.runBlocking
 import kotlinx.serialization.Serializable
 
 @Serializable
 data class HttpAgentRequest(
         val schedule: Schedule = Schedule(1),
-        val request: HttpRequest,
+        val request: FsHttpRequest,
 )
 
 @Serializable
-data class HttpRequest(
+data class FsHttpRequest(
         val url: String,
         val method: Method = Method.GET,
         val headers: Map<String, String> = mapOf(),
-        val body: String,
+        val body: String? = "",
 ) {
     enum class Method(val methodString: String) {
         GET("GET"),
@@ -32,6 +31,14 @@ data class HttpRequest(
     }
 }
 
+
+@Serializable
+data class FsHttpResponse(
+        val status: Int,
+        val httpVer: String,
+        val headers: Map<String, String> = mapOf(),
+        val body: String,
+)
 
 @InternalCoroutinesApi
 fun main(args: Array<String>): Unit = runBlocking {
@@ -51,15 +58,27 @@ val httpClient = HttpClient()
 suspend fun runHttpAgent(agentId: String) {
     do {
         firestore.firestoreDocumentFlow<HttpAgentRequest> { collection("devConfig").document(agentId) }.collectLatest { req ->
-            println(req)
+            println("Request: ${req.request.url}")
             runCatching {
-                val res = httpClient.request<String> {
+                val res = httpClient.request<HttpResponse> {
                     url(req.request.url)
                     method = HttpMethod(req.request.method.methodString)
-                    req.request.headers.map { (k, v) -> header(k, v) }
-                    body = req.request.body
+                    req.request.headers.map { (k, v) ->
+                        println("$k:$v")
+                        kotlin.runCatching { header(k, v) }.onFailure { it.printStackTrace() }
+                    }
+                    if (req.request.body != null) {
+                        body = req.request.body
+                    }
                 }
-                firestore.collection("devStatus").document(agentId).set(mapOf("response" to res))
+                val resBody = res.readText()
+                val r = FsHttpResponse(
+                        status = res.status.value,
+                        httpVer = res.version.toString(),
+                        headers = res.headers.toMap().map { (k, v) -> k to v[0] }.toMap(),
+                        body = resBody,
+                )
+                firestore.collection("devStatus").document(agentId).set(r).get()
             }.onFailure {
                 it.printStackTrace()
                 firestore.collection("devStatus").document(agentId).set(mapOf("stacktrace" to it.stackTraceToString()))
