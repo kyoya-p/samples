38c7d6ad0b3fb6e4ebbfbece63ab7da2659b5c3e
kyoya <kyoya.p@gmail.com>
2021-02-22 17:20:25 +0900

    n

--- 2021/graphQL/apolloClient/README.md
+++
@@ -1,7 +0,0 @@
-Apollo GraphQL CLient
-========
-
-refrence
-----
-apollographql.com/docs/android/essentials/get-started-multiplatform/
-
--- 2021/graphQL/apolloClient/build.gradle.kts
+++ 2021/graphQL/apolloClient/build.gradle.kts
@@ -1,6 +1,12 @@
+
+/*
+https://www.apollographql.com/docs/android/essentials/get-started-kotlin/
+
+ */
 plugins {
-    kotlin("multiplatform") version "1.4.30"
-    id("com.apollographql.apollo").version("2.5.3") // plugins.gradle.org/plugin/com.apollographql.apollo
+    //java
+    kotlin("jvm") version "1.4.30"
+    id("com.apollographql.apollo") version "2.5.3" // https://plugins.gradle.org/plugin/com.apollographql.apollo
 }
 
 group = "org.example"
@@ -10,34 +16,13 @@ repositories {
     mavenCentral()
 }
 
+dependencies {
+    implementation(kotlin("stdlib"))
+    implementation("com.apollographql.apollo:apollo-runtime:2.5.3") // https://mvnrepository.com/artifact/com.apollographql.apollo/apollo-runtime
+    implementation("com.apollographql.apollo:apollo-coroutines-support:2.5.3") // https://mvnrepository.com/artifact/com.apollographql.apollo/apollo-coroutines-support
+    //testCompile("junit", "junit", "4.12")
+}
 
-kotlin {
-    /* Targets configuration omitted. 
-    *  To find out how to configure the targets, please follow the link:
-    *  https://kotlinlang.org/docs/reference/building-mpp-with-gradle.html#setting-up-targets */
-
-    jvm {
-        withJava()
-    }
-    js {
-        browser {
-            binaries.executable()
-        }
-    }
-
-    sourceSets {
-        val commonMain by getting {
-            dependencies {
-                implementation(kotlin("stdlib-common"))
-                implementation("com.apollographql.apollo:apollo-runtime-kotlin:2.5.3") //https://search.maven.org/artifact/com.apollographql.apollo/apollo-runtime-kotlin
-
-            }
-        }
-        val commonTest by getting {
-            dependencies {
-                implementation(kotlin("test-common"))
-                implementation(kotlin("test-annotations-common"))
-            }
-        }
-    }
+apollo { //
+    generateKotlinModels.set(true)
 }
