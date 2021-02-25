730fc4c11884cab1f10b8c8d869d58d85d79b89d
kyoya <kyoya.p@gmail.com>
2021-02-25 20:10:08 +0900

    n

--- 2021/pahoj/build.gradle.kts
+++ 2021/pahoj/build.gradle.kts
@@ -13,7 +13,7 @@ repositories {
 dependencies {
     implementation(kotlin("stdlib"))
     implementation(group = "org.eclipse.paho", name = "org.eclipse.paho.client.mqttv3", version = "1.2.5")
-    implementation("com.apollographql.apollo:apollo-runtime:2.5.3")
+    implementation("com.apollographql.apollo:apollo-runtime:2.5.4")
     implementation("android.arch.work:work-runtime:1.0.1") // https://mvnrepository.com/artifact/com.apollographql.apollo/apollo-runtime
     implementation("io.ktor:ktor-client-cio:1.5.1") //https://mvnrepository.com/artifact/io.ktor/ktor-client-cio
 }
