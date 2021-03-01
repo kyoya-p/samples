10eac7559b470431e93d271cf08bef6c8c014933
kyoya <kyoya.p@gmail.com>
2021-03-02 08:10:40 +0900

    n

--- 2021/UniAgJvmKt/src/main/kotlin/main.kt
+++ 2021/UniAgJvmKt/src/main/kotlin/main.kt
@@ -1,3 +1,15 @@
-fun main() {
+import io.ktor.application.*
+import io.ktor.response.*
+import io.ktor.routing.*
+import io.ktor.server.cio.*
+import io.ktor.server.engine.*
 
+fun main() {
+    embeddedServer(CIO, port = 8000) {
+        routing {
+            get("/") {
+                call.respondText("Hello, world!")
+            }
+        }
+    }.start(wait = true)
 }
