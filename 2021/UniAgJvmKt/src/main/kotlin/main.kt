75166265d71c31ef71ec5278e7fff36ba88ae2ea
kyoya <kyoya.p@gmail.com>
2021-03-03 12:36:40 +0900

    n

--- 2021/UniAgJvmKt/src/main/kotlin/main.kt
+++ 2021/UniAgJvmKt/src/main/kotlin/main.kt
@@ -1,22 +1,20 @@
 import io.ktor.application.*
 import io.ktor.features.*
 import io.ktor.http.cio.websocket.*
-import io.ktor.response.*
 import io.ktor.routing.*
 import io.ktor.server.cio.*
 import io.ktor.server.engine.*
 import io.ktor.websocket.*
-import kotlinx.coroutines.channels.mapNotNull
 
 // Websocket:
 // https://jp.ktor.work/servers/features/websockets.html
 
 fun main() {
     embeddedServer(CIO, port = 8000) {
-        install(CORS)
+        //install(CORS)
         install(WebSockets)
         routing {
-            webSocket("/tcp") {
+            webSocket("/") {
                 println("onConnect")
                 for (frame in incoming) {
                     when (frame) {
