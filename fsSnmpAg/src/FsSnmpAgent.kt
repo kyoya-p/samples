bce1811c2514f90111ccdc69108eb5a02340ab4d
kk <kyoya.p@gmail.com>
2021-03-09 09:12:05 +0900

    n

--- fsSnmpAg/src/FsSnmpAgent.kt
+++ fsSnmpAg/src/FsSnmpAgent.kt
@@ -44,38 +44,38 @@ suspend fun runAgent(agentId: String) = coroutineScope {
     println("runAgent ${agentId}")
     callbackFlow<Agent> {  // Agent情報をチェックし、存在すれば/更新されれば flow
         val listener = firestore.collection("device").document(agentId)
-            .addSnapshotListener(object : EventListener<DocumentSnapshot?> {
-                override fun onEvent(snapshot: DocumentSnapshot?, ex: FirestoreException?) {
-                    println("called: ") //TODO
-                    if (ex == null && snapshot != null && snapshot.data != null) {
-                        println("called: ${snapshot.data!!.toJsonObject()}") //TODO
-                        val ag = Json { ignoreUnknownKeys = true }
-                            .decodeFromJsonElement<MfpMibAgentDevice>(snapshot.data!!.toJsonObject())
-                        offer(Agent(snapshot, ag))
-                    } else {
-                        close()
+                .addSnapshotListener(object : EventListener<DocumentSnapshot?> {
+                    override fun onEvent(snapshot: DocumentSnapshot?, ex: FirestoreException?) {
+                        println("called: ") //TODO
+                        if (ex == null && snapshot != null && snapshot.data != null) {
+                            println("called: ${snapshot.data!!.toJsonObject()}") //TODO
+                            val ag = Json { ignoreUnknownKeys = true }
+                                    .decodeFromJsonElement<MfpMibAgentDevice>(snapshot.data!!.toJsonObject())
+                            offer(Agent(snapshot, ag))
+                        } else {
+                            close()
+                        }
                     }
-                }
-            })
+                })
         awaitClose { listener.remove() }
     }.flatMapLatest { agent -> // クエリをチェックし、未処理のクエリがあれば クエリ毎に実行
         println("Agent: $agent")
         val queryCollectionPath = agent.data.dev.notification
-            ?: firestore.collection("device").document(agentId).collection("query").path
+                ?: firestore.collection("device").document(agentId).collection("query").path
         callbackFlow<Query> {
             val listener =
-                firestore.collection(queryCollectionPath).addSnapshotListener(object : EventListener<QuerySnapshot?> {
-                    override fun onEvent(value: QuerySnapshot?, error: FirestoreException?) {
-                        if (error == null && value != null) {
-                            for (e in value.documents) {
-                                val query = Json {
-                                    ignoreUnknownKeys = true
-                                }.decodeFromJsonElement<MfpMibAgentQuery>(e.data.toJsonObject())
-                                offer(Query(e, query, agent))
-                            }
-                        } else close()
-                    }
-                })
+                    firestore.collection(queryCollectionPath).addSnapshotListener(object : EventListener<QuerySnapshot?> {
+                        override fun onEvent(value: QuerySnapshot?, error: FirestoreException?) {
+                            if (error == null && value != null) {
+                                for (e in value.documents) {
+                                    val query = Json {
+                                        ignoreUnknownKeys = true
+                                    }.decodeFromJsonElement<MfpMibAgentQuery>(e.data.toJsonObject())
+                                    offer(Query(e, query, agent))
+                                }
+                            } else close()
+                        }
+                    })
             awaitClose { listener.remove() }
         }
     }.flatMapLatest { query -> // スケジュール時間になれば、次に進む //TODO
@@ -94,34 +94,41 @@ suspend fun runAgent(agentId: String) = coroutineScope {
                 query.data.scanAddrSpecs.forEach { target ->
                     discoveryDeviceFlow(target, snmp).collect { ev -> // 発見デバイス情報を次に流す
                         val res = ResponseEvent(
-                            reqPdu = PDU.from(ev.request), reqTarget = target, resPdu = PDU.from(ev.response),
-                            resTarget = SnmpTarget(
-                                addr = ev.peerAddress.inetAddress.hostAddress, port = ev.peerAddress.port,
-                                credential = target.credential, retries = target.retries, interval = target.interval
-                            )
+                                reqPdu = PDU.from(ev.request), reqTarget = target, resPdu = PDU.from(ev.response),
+                                resTarget = SnmpTarget(
+                                        addr = ev.peerAddress.inetAddress.hostAddress, port = ev.peerAddress.port,
+                                        credential = target.credential, retries = target.retries, interval = target.interval
+                                )
                         )
                         println("Detected: ${res.resTarget.addr}")
                         val devId = "type=mfp.mib:model=${res.resPdu.vbl[0].value}:sn=${res.resPdu.vbl[1].value}"
                         if (query.data.autoRegistration) { // 自己デバイス登録
                             firestore.collection("device").document(devId).set(
-                                MfpMibDevice(
-                                    id = devId,
-                                    cluster = query.agent.data.cluster, // Agentと同じClusterに登録
-                                    dev = GdvmDeviceInfo(
-                                        password = query.agent.data.dev.password, // (デフォルトPWのほうがよいだろうか?)
-                                        name = devId,
-                                        ip = ev.peerAddress.inetAddress.hostAddress,
-                                        host = ev.peerAddress.inetAddress.hostAddress,
-                                    ),
-                                    tags = listOf(
-                                        "detectedBy:$agentId", //TODO
-                                        "cluster:${query.agent.data.cluster}", //TODO
-                                        "testTag1", //TODO
-                                        "testTag2", //TODO
-                                    ),
-                                )
+                                    MfpMibDevice(
+                                            id = devId,
+                                            cluster = query.agent.data.cluster, // Agentと同じClusterに登録
+                                            dev = GdvmDeviceInfo(
+                                                    password = query.agent.data.dev.password, // (デフォルトPWのほうがよいだろうか?)
+                                                    name = devId,
+                                                    ip = ev.peerAddress.inetAddress.hostAddress,
+                                                    host = ev.peerAddress.inetAddress.hostAddress,
+                                            ),
+                                            tags = listOf(
+                                                    "detectedBy:$agentId", //TODO
+                                                    "cluster:${query.agent.data.cluster}", //TODO
+                                            ),
+                                    )
                             )
-                            //TODO 仮クエリ設定
+                            // TODO デバイス(の代わりに)レポートを送信。 本来はデバイスの新スタンスが送信する
+                            val report = StateSnmpReport(
+                                    deviceId = devId,
+                                    cluster = query.agent.data.cluster,
+                                    pdu = res.resPdu,
+                            )
+                            firestore.collection("device").document(devId).collection("state").document("snmp").set(report)
+                            firestore.collection("device").document(devId).collection("logs").document().set(report)
+
+                            // TODO 仮クエリ設定
                             //firestore.collection("device").document(devId).collection("query").document("default").set()
                         }
                         devSet.add(devId)
@@ -130,11 +137,12 @@ suspend fun runAgent(agentId: String) = coroutineScope {
                 }
             }.join()
             // 検索結果をlogに記載
-            val rep = LogReport(
-                time = Date().time,
-                deviceId = agentId,
-                log = GdvmLog(cluster = query.agent.data.cluster, targets = listOf("device/${agentId}")),
-                result = Result(detected = devSet.toList()),
+            val rep = LogAgentReport(
+                    time = Date().time,
+                    deviceId = agentId,
+                    log = GdvmLog(cluster = query.agent.data.cluster, targets = listOf("device/${agentId}")),
+                    result = Result(detected = devSet.toList()),
+                    cluster = query.agent.data.cluster,
             )
             query.snapshot.reference.collection("results").document().set(rep)
             query.agent.snapshot.reference.collection("logs").document().set(rep)
@@ -175,9 +183,9 @@ suspend fun discoveryDeviceFlow(target: SnmpTarget, snmp: Snmp) = channelFlow {
         }
     } else {
         snmp.scanFlow(
-            pdu.toSnmp4j(),
-            target.toSnmp4j(),
-            InetAddress.getByName(target.addrRangeEnd ?: target.addr)
+                pdu.toSnmp4j(),
+                target.toSnmp4j(),
+                InetAddress.getByName(target.addrRangeEnd ?: target.addr)
         ).collect {
             offer(it)
         }
