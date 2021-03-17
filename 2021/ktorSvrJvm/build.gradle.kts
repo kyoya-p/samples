b41b8de2f87ba0525fed2b19f6ed963581360d31
kyoya <kyoya.p@gmail.com>
2021-03-17 13:37:28 +0900

    n

---
+++ 2021/ktorSvrJvm/build.gradle.kts
@@ -0,0 +1,20 @@
+plugins {
+    kotlin("jvm") version "1.4.31"
+}
+
+group = "org.example"
+version = "1.0-SNAPSHOT"
+
+val ktor_version = "1.5.2"
+
+repositories {
+    mavenCentral()
+}
+
+dependencies {
+    implementation(kotlin("stdlib"))
+    implementation("io.ktor:ktor-server-netty:$ktor_version")
+    implementation("io.ktor:ktor-websockets:$ktor_version")
+
+    implementation("io.ktor:ktor-network-tls-certificates:$ktor_version")
+}
