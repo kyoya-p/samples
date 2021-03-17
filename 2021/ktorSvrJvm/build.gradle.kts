bc56cb123683736153b34fee39bc19d6006b5396
kyoya <kyoya.p@gmail.com>
2021-03-18 08:09:32 +0900

    n

--- 2021/ktorSvrJvm/build.gradle.kts
+++ 2021/ktorSvrJvm/build.gradle.kts
@@ -1,5 +1,7 @@
 plugins {
+    application
     id("org.jetbrains.kotlin.jvm") version "1.4.31" // https://plugins.gradle.org/plugin/org.jetbrains.kotlin.jvm
+    id("com.github.johnrengelman.shadow") version "6.1.0" // https://imperceptiblethoughts.com/shadow
 }
 
 group = "org.example"
@@ -17,4 +19,16 @@ dependencies {
     implementation("io.ktor:ktor-websockets:$ktor_version")
 
     implementation("io.ktor:ktor-network-tls-certificates:$ktor_version")
+}
+
+
+application {
+    mainClass.set("io.ktor.server.netty.EngineMain")
+}
+
+tasks.withType<Jar> {
+    manifest {
+        attributes(mapOf("Main-Class" to application.mainClass))
+    }
+    copy{from}
 }
