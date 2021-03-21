8b7cf073919b0f669daeb2bff1d23ee228ef2e9b
kyoya <kyoya.p@gmail.com>
2021-03-22 06:16:46 +0900

    n

--- 2021/ktorSvrJvm/src/main/kotlin/ktorServer_SSL_JVM.kt
+++ 2021/ktorSvrJvm/src/main/kotlin/ktorServer_SSL_JVM.kt
@@ -7,33 +7,36 @@ import io.ktor.server.engine.*
 import io.ktor.server.netty.Netty
 import io.ktor.util.*
 import java.io.File
+import java.security.KeyStore
 import kotlin.text.toCharArray
-
 
 // https://proandroiddev.com/ssl-with-kotlin-and-ktor-61b3d7dccbc5
 
 
 @KtorExperimentalAPI
 fun main() {
-    val keyStoreFile = File("build/temp.jks")
+    val keyStoreFile = File("keystore.jks")
     val certAlias = "mycert"
     val storePass = "changeit"
     val certPass = "Soft2cream"
-    val keystore = buildKeyStore {
-        certificate(certAlias) {
-            hash = HashAlgorithm.SHA256
-            sign = SignatureAlgorithm.ECDSA
-            keySizeInBits = 256
-            password = storePass
-        }
+    val keystore = if (keyStoreFile.exists()) {
+        KeyStore.getInstance(keyStoreFile, storePass.toCharArray())
+    } else {
+        buildKeyStore {
+            certificate(certAlias) {
+                hash = HashAlgorithm.SHA256
+                sign = SignatureAlgorithm.ECDSA
+                keySizeInBits = 256
+                password = storePass
+            }
+        }.apply { saveToFile(keyStoreFile, storePass) }
     }
-    keystore.saveToFile(keyStoreFile, storePass)
 
     fun appEnv(module: Application.() -> Unit) = applicationEngineEnvironment {
         connector { port = 80 }
         sslConnector(keystore, certAlias, { certPass.toCharArray() }, { storePass.toCharArray() }) {
             port = 443
-            keyStorePath = keyStoreFile.absoluteFile
+            //keyStorePath = keyStoreFile.absoluteFile
         }
         module(module)
     }
