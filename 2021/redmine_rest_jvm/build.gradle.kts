a4f2e06576b289a060f83ffc7cce6b4684298f3a
kyoya <kyoya.p@gmail.com>
2021-10-12 04:43:22 +0900

    n

--- 2021/redmine_rest_jvm/build.gradle.kts
+++ 2021/redmine_rest_jvm/build.gradle.kts
@@ -1,5 +1,6 @@
 plugins {
     kotlin("jvm") version "1.5.31"
+    id("org.jetbrains.kotlin.plugin.serialization") version "1.5.31"
 }
 
 group = "org.example"
@@ -10,9 +11,11 @@ repositories {
 }
 
 dependencies {
-    implementation(kotlin("stdlib"))
+    implementation(kotlin("stdlib","1.5.31"))
+    implementation("org.jetbrains.kotlin:kotlin-serialization:1.5.31")
 
-    implementation("io.ktor:ktor-client-core-jvm:1.6.4")  // https://mvnrepository.com/artifact/io.ktor/ktor-client-core-jvm
-    implementation("io.ktor:ktor-client-cio-jvm:1.6.4") // https://mvnrepository.com/artifact/io.ktor/ktor-client-cio-jvm
-
+    implementation("io.ktor:ktor-client-core:1.6.4")  // https://mvnrepository.com/artifact/io.ktor/ktor-client-core-jvm
+    implementation("io.ktor:ktor-client-cio:1.6.4") // https://mvnrepository.com/artifact/io.ktor/ktor-client-cio-jvm
+    implementation("io.ktor:ktor-client-serialization:1.6.4") // https://mvnrepository.com/artifact/io.ktor/ktor-client-serialization
+    implementation("io.ktor:ktor-client-json:1.6.4") // https://mvnrepository.com/artifact/io.ktor/ktor-client-json
 }
--- 2021/redmine_rest_jvm/src/main/kotlin/main.kt
+++ 2021/redmine_rest_jvm/src/main/kotlin/main.kt
@@ -1,10 +1,49 @@
 import io.ktor.client.*
 import io.ktor.client.engine.cio.*
 import io.ktor.client.features.*
+import io.ktor.client.features.json.*
+import io.ktor.client.features.json.serializer.*
+import io.ktor.client.request.*
+import kotlinx.serialization.Serializable
 
-fun main() {
 
-    val client = HttpClient(CIO)
+val url = "http://172.29.240.236:8090/srdm/projects.json?key=dbce860f7ef8cb6e183f97edd545190e37599f64"
 
-    client.get("http://")
+@Serializable
+data class Parent(val id: Int, val name: String)
+
+@Serializable
+data class Project(
+    val id: Int,
+    val name: String,
+    val identifier: String,
+    val description: String,
+    val parent: Parent?=null,
+    val status: Int,
+    val is_public: Boolean=false,
+    val created_on: String,
+    val updated_on: String,
+)
+
+@Serializable
+data class ProjectsRes(
+    val projects: List<Project>,
+    val total_count: Int,
+    val offset: Int,
+    val limit: Int,
+)
+
+suspend fun main() {
+
+    val client = HttpClient(CIO) {
+        install(JsonFeature) {
+            serializer = defaultSerializer()
+        }
+    }
+
+
+    val r2: String = client.get(url)
+    println(r2)
+    val r: ProjectsRes = client.get(url)
+    println(r)
 }
