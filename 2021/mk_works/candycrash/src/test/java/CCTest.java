8f2333402e3bff63ad68f16805b2e7a24edc46b7
kyoya <kyoya.p@gmail.com>
2021-10-29 08:15:39 +0900

    MK宿題

---
+++ 2021/mk_works/candycrash/src/test/java/CCTest.java
@@ -0,0 +1,33 @@
+import org.junit.jupiter.api.Test;
+
+import java.io.ByteArrayInputStream;
+
+public class CCTest {
+    @Test
+    void test1() {
+        System.setIn(new ByteArrayInputStream("7 1 1 1 2 2 2 3 3".getBytes()));
+        CandyCrush.main(new String[0]);
+    }
+
+    @Test
+    void test2() {
+        System.setIn(new ByteArrayInputStream(
+                ("7 2 " +
+                        "1 2 3 1 2 3 1 " +
+                        "3 3 1 2 1 2 3 ").getBytes()));
+        CandyCrush.main(new String[0]);
+    }
+
+    @Test
+    void test3() {
+        System.setIn(new ByteArrayInputStream(
+                ("5 5 " +
+                        "0 1 2 3 4 " +
+                        "2 2 4 5 6 " +
+                        "4 5 2 7 8 " +
+                        "6 7 8 2 0 " +
+                        "8 9 0 1 0 ").getBytes()));
+        CandyCrush.main(new String[0]);
+    }
+
+}
