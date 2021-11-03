f96aec27867e65739fb387f085e7c3af9e7d8a06
kyoya <kyoya.p@gmail.com>
2021-11-03 19:43:27 +0900

    n

--- 2021/SNMP4J/MibTool_ktJvm/src/main/kotlin/Walker.kt
+++ 2021/SNMP4J/MibTool_ktJvm/src/main/kotlin/Walker.kt
@@ -1,15 +1,21 @@
-package sc
-
-import kotlinx.coroutines.flow.Flow
+import kotlinx.coroutines.ExperimentalCoroutinesApi
+import kotlinx.coroutines.channels.awaitClose
+import kotlinx.coroutines.flow.callbackFlow
 import kotlinx.coroutines.flow.toList
+import kotlinx.serialization.*
+import kotlinx.serialization.json.Json
+import kotlinx.serialization.modules.SerializersModule
+import kotlinx.serialization.modules.contextual
+import org.snmp4j.CommunityTarget
+import org.snmp4j.Target
 import org.snmp4j.PDU
 import org.snmp4j.Snmp
-import org.snmp4j.fluent.PduBuilder
 import org.snmp4j.fluent.SnmpBuilder
 import org.snmp4j.smi.*
 import java.net.InetAddress
-import java.util.concurrent.Semaphore
 
+
+@ExperimentalCoroutinesApi
 suspend fun main(args: Array<String>) {
     if (args.size == 0) {
         println("Syntax: java -jar walker.jar addr [topOid]")
@@ -18,47 +24,39 @@ suspend fun main(args: Array<String>) {
     val addr = InetAddress.getByName(args[0])!!
     val topOid = OID(args.getOrNull(1) ?: "1")
 
-    val snmpBuilder = SnmpBuilder()
-    val snmp = snmpBuilder.udp().v1().threads(2).build()!!
+    val snmp = SnmpBuilder().udp().v1().threads(2).build()!!
     snmp.listen()
 
-    val topPdu = PDU().apply { listOf(VariableBinding(topOid)) }
-    val res = snmp.walk(addr, topPdu).toList().associate { it.oid!! to it.variable!! }.toSortedMap()
-    println(res)
-}
 
-fun Snmp.walk(addr: InetAddress, topPdu: PDU): Flow<VariableBinding> {
-    
-}
-
-fun SNMPManager.walkBulkAll(addr: String, initOid: List<Int> = listOf(1)): TreeMap<OID, VariableBinding> {
-    val tm = TreeMap<OID, VariableBinding>()
-    val semMaxSnmpRequest = Semaphore(1)
-    semMaxSnmpRequest.acquire(1)
-    walkBulk(addr, initOid, cbTerminate = { _, _ ->
-        semMaxSnmpRequest.release()
-    }) {
-        tm[it.oid] = it
-        SNMPManager.ResultCode.CONTINUE
-    }
-    semMaxSnmpRequest.acquire(1)
-    return tm
-}
-
-fun VariableBinding.toVBFileString(): String {
-    fun escape(it: Byte): String {
-        return if (it < 0x20 || 0x7f < it || it == ':'.toByte() || it == '"'.toByte()) ":%02x".format(it.toInt() and 0xff)
-        else it.toChar().toString()
+    val module = SerializersModule {
+        contextual(VariableBindingAsStringSerializer)
     }
 
-    val v = variable
-    return oid.toString() + " " + variable.syntax + " " + when {
-        v is OctetString -> "\"" + v.value.joinToString(separator = "") { escape(it) } + "\""
-        v is OID -> v.toString()
-        v is TimeTicks -> v.value.toString()
-        // TBD...
-        else -> v.toString()
+    val json = Json {
+        prettyPrint = true
+        serializersModule = module
     }
+
+    runCatching {
+        val topPdu = PDU().apply { variableBindings = listOf(VariableBinding(topOid)) }
+        val target = CommunityTarget(UdpAddress(addr, 161), OctetString("public"))
+        val res = snmp.walkFlow(topPdu, target).toList()
+        println(json.encodeToString(res))
+    }.onFailure { it.printStackTrace() }
+    snmp.close()
 }
 
+@ExperimentalCoroutinesApi
+fun Snmp.walkFlow(topPdu: PDU, target: Target<UdpAddress>) = callbackFlow<VariableBinding> {
+    generateSequence(topPdu) { reqPdu ->
+        reqPdu.type = PDU.GETNEXT
+        this@walkFlow.send(reqPdu, target).response
+    }.drop(1).takeWhile { pdu ->
+        pdu.errorStatus == PDU.noError && pdu.variableBindings[0].oid.startsWith(topPdu.variableBindings[0].oid)
+    }.forEach {
+        trySend(it.variableBindings[0]).isSuccess
+    }
+    close()
+    awaitClose {}
+}
 
