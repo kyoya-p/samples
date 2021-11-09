b483a1892e6c6d6bfc41c87a5e53e9700337474b
kyoya <kyoya.p@gmail.com>
2021-11-10 08:29:39 +0900

    n

--- 2021/SNMP4J/SnmpDiscovery_KtJvm/src/main/kotlin/Json2Csv.kt
+++ 2021/SNMP4J/SnmpDiscovery_KtJvm/src/main/kotlin/Json2Csv.kt
@@ -1,11 +1,23 @@
 package jp.`live-on`.shokkaa
 
+import kotlinx.serialization.ExperimentalSerializationApi
 import kotlinx.serialization.encodeToString
+import kotlinx.serialization.json.Json
 import kotlinx.serialization.json.decodeFromStream
 import java.io.File
 
 fun main() {
-    val devs = jsonSnmp4j.decodeFromStream<List<Device>>(File("build/res.json").inputStream())
+    val a = ByteArray(256) { it.toByte() }
+    println(a.joinToString())
+    val b = a.escaped()
+    val c = b.unescaped()
+    println(c.joinToString())
+}
+
+
+@ExperimentalSerializationApi
+fun main2() {
+    val devs = jsonSnmp4j.decodeFromStream<List<Device>>(File("samples/res.json").inputStream())
     devs.forEach { d ->
         println((listOf(d.ip) + d.vbl.map { jsonSnmp4j.encodeToString(it.variable) }).joinToString())
     }
--- 2021/SNMP4J/SnmpDiscovery_KtJvm/src/main/kotlin/Scanner.kt
+++ 2021/SNMP4J/SnmpDiscovery_KtJvm/src/main/kotlin/Scanner.kt
@@ -158,21 +158,40 @@ object VariableBindingAsStringSerializer : KSerializer<VariableBinding> {
         return VariableBinding(OID(sOid), value)
     }
 
-    const val z = '0'.code.toByte()
-    fun ByteArray.escaped() = fold(ByteArray(0)) { a, b ->
-        a + when {
-            b < 0x20 || b >= 0x80 || b == 0x3a.toByte() -> arrayOf(0x3b, z + b / 0x10, z + (b and 0xf))
-            else -> arrayOf(b.toInt())
-        }.map { it.toByte() }.toByteArray()
-    }.decodeToString()
+}
 
-    fun String.unescaped(): ByteArray = also{println(it)}.encodeToByteArray().let { ba ->
-        generateSequence(0 to 0.toByte()) { (i, _) ->
-            when {
-                i >= ba.size -> null
-                ba[i] == ':'.code.toByte() -> (i + 3) to ((ba[i + 1] - z) / 0x10 + (ba[i + 2] and 0xf)).toByte()
-                else -> i + 1 to ba[i]
-            }
-        }.map { it.second }.toList().toByteArray()
+private val z = '0'.code.toUByte().toInt()
+private val digit = "0123456789abcdef".map { it.code.toByte() }
+private val ascii = List(256) {
+    when {
+        ('0'..'9').contains(it.toChar()) -> it - '0'.code
+        ('a'..'f').contains(it.toChar()) -> it - 'a'.code + 10
+        ('A'..'F').contains(it.toChar()) -> it - 'A'.code + 10
+        else -> -1
     }
+}
+
+fun ByteArray.escaped() = fold(ByteArray(0)) { a, b ->
+    val d = b.toUByte().toInt()
+    a + when {
+        d < 0x20 || d >= 0x7f || d == 0x3a -> arrayOf(0x3a, digit[d / 0x10], digit[d and 0xf])
+        else -> arrayOf(d)
+    }.map { it.toByte() }.toByteArray()
+}.decodeToString()
+
+fun String.unescaped() = encodeToByteArray().let { ba ->
+    generateSequence(0 to 0.toByte()) { (i, _) ->
+        when {
+            i >= ba.size -> null
+            ba[i] == ':'.code.toByte() -> {
+                // println(i)
+                if (i >= ba.size - 2) throw Exception("Illegal Escape Position: Index=$i: \"${ba.joinToString()}\" ")
+                val c1 = ascii[ba[i + 1].toUByte().toInt()]
+                val c2 = ascii[ba[i + 2].toUByte().toInt()]
+                if (c1 < 0 || c2 < 0) throw Exception("Illegal Escape Code: Index=$i: \"${ba.joinToString()}\" ")
+                (i + 3) to (c1 * 0x10 + c2).toByte()
+            }
+            else -> (i + 1) to ba[i]
+        }
+    }.drop(1).map { it.second }.toList().toByteArray()
 }
