954c3b8d0166a999758200bebc9d7dc40eb02112
WIN\s117781 <a>
2021-11-26 18:02:13 +0900

    n

--- 2021/ktorClientsJvm/src/test/kotlin/KtorClient_CIO.kt
+++ 2021/ktorClientsJvm/src/test/kotlin/KtorClient_CIO.kt
@@ -7,16 +7,20 @@ import io.ktor.client.request.*
 import kotlinx.coroutines.runBlocking
 import org.junit.jupiter.api.Test
 import java.net.URL
+import javax.net.ssl.TrustManager
 
 class KtorClient_CIO {
     @Test
     fun t1(): Unit = runBlocking {
         val client = HttpClient(CIO) {
             engine {
-                proxy = ProxyBuilder.http("http://192.168.81.175:3080")
+                //proxy = ProxyBuilder.http("http://192.168.81.175:3080") //プロキシ設定
+                https {
+                    //trustManager = TrustManager()
+                }
             }
         }
-        val r = client.get<String>(URL("https://google.com"))
+        val r = client.get<String>(URL("https://sec-auth03.nara.sharp.co.jp"))
         println(r)
     }
 
