72b1ea0a3efaccb50381590a9be29029bfd76e36
kyoya <kyoya.p@gmail.com>
2021-12-11 22:16:59 +0900

    n

--- 2021/SNMP4J/Snmp4jUtils_KtJvm/build.gradle.kts
+++ 2021/SNMP4J/Snmp4jUtils_KtJvm/build.gradle.kts
@@ -15,13 +15,13 @@ repositories {
 dependencies {
     implementation("org.jetbrains.kotlin:kotlin-stdlib:1.6.0")
     implementation("org.jetbrains.kotlinx:kotlinx-coroutines-core:1.5.2-native-mt")
-    implementation("org.jetbrains.kotlinx:kotlinx-serialization-json:1.3.0")
+    implementation("org.jetbrains.kotlinx:kotlinx-serialization-json:1.3.1")
     implementation("org.jetbrains.kotlinx:kotlinx-datetime:0.3.1")
 
     implementation("org.snmp4j:snmp4j:3.6.0")
     implementation("com.charleskorn.kaml:kaml:0.37.0") // https://github.com/charleskorn/kaml/releases/latest
 
-    testImplementation("org.junit.jupiter:junit-jupiter:5.8.1")
+    testImplementation("org.junit.jupiter:junit-jupiter:5.8.2")
 }
 
 tasks.test {
@@ -46,23 +46,5 @@ publishing {
         maven {
             url = uri(layout.buildDirectory.dir("repo"))
         }
-    }
-
-}
-
-kover {
-    isEnabled = true                        // false to disable instrumentation of all test tasks in all modules
-    coverageEngine.set(kotlinx.kover.api.CoverageEngine.INTELLIJ) // change instrumentation agent and reporter
-    intellijEngineVersion.set("1.0.614")    // change version of IntelliJ agent and reporter
-    jacocoEngineVersion.set("0.8.7")        // change version of JaCoCo agent and reporter
-    generateReportOnCheck.set(true)         // false to do not execute `koverReport` task before `check` task
-}
-
-tasks.test {
-    extensions.configure(kotlinx.kover.api.KoverTaskExtension::class) {
-        isEnabled = true
-        binaryReportFile.set(file("$buildDir/custom/result.bin"))
-        includes = listOf("com\\.example\\..*")
-        excludes = listOf("com\\.example\\.subpackage\\..*")
     }
 }
--- 2021/SNMP4J/Snmp4jUtils_KtJvm/src/main/kotlin/Common.kt
+++ 2021/SNMP4J/Snmp4jUtils_KtJvm/src/main/kotlin/Common.kt
@@ -12,11 +12,11 @@ class SnmpSuspendable(val snmp: Snmp) {
     suspend fun send(pdu: PDU, target: Target<UdpAddress>) =
         suspendCoroutine<ResponseEvent<UdpAddress>> { continuation ->
             @Suppress("BlockingMethodInNonBlockingContext")
-            snmp.send(pdu, target, null, object : org.snmp4j.event.ResponseListener {
-                override fun <A : org.snmp4j.smi.Address?> onResponse(r: org.snmp4j.event.ResponseEvent<A>?) {
+            snmp.send(pdu, target, null, object : ResponseListener {
+                override fun <A : org.snmp4j.smi.Address?> onResponse(r: ResponseEvent<A>?) {
                     snmp.cancel(pdu, this)
-                    @kotlin.Suppress("UNCHECKED_CAST")
-                    continuation.resume(r as org.snmp4j.event.ResponseEvent<org.snmp4j.smi.UdpAddress>)
+                    @Suppress("UNCHECKED_CAST")
+                    continuation.resume(r as ResponseEvent<UdpAddress>)
                 }
             })
             return@suspendCoroutine
@@ -25,6 +25,7 @@ class SnmpSuspendable(val snmp: Snmp) {
     suspend fun get(pdu: PDU, target: Target<UdpAddress>) = send(pdu.apply { type = PDU.GET }, target)
     suspend fun getNext(pdu: PDU, target: Target<UdpAddress>) = send(pdu.apply { type = PDU.GETNEXT }, target)
 
+    @Suppress("unused")
     fun cancel(pdu: PDU, listener: ResponseListener) = snmp.cancel(pdu, listener)
     fun listen() = snmp.listen()
     fun close() = snmp.close()
@@ -32,7 +33,7 @@ class SnmpSuspendable(val snmp: Snmp) {
 
 fun Snmp.suspendable() = SnmpSuspendable(this)
 
-
+@Suppress("EnumEntryName", "SpellCheckingInspection")
 enum class SampleOID(val oid: OID, val oidName: String) {
     sysDescr(OID("1.3.6.1.2.1.1.1"), "sysDescr"),
     sysName(OID("1.3.6.1.2.1.1.5"), "sysName"),
