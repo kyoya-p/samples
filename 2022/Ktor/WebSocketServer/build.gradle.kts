ef88888b74070c268d7b3ef92dc9dfe3290d4799
kyoya <kyoya.p@gmail.com>
2021-12-27 03:16:32 +0900

    n

--- 2022/Ktor/WebSocketServer/build.gradle.kts
+++ 2022/Ktor/WebSocketServer/build.gradle.kts
@@ -12,7 +12,7 @@ repositories {
 dependencies {
     implementation(kotlin("stdlib"))
     implementation("io.ktor:ktor-server-core:1.6.7") // Serverを作成する場合
-    implementation("io.ktor:ktor-server-netty:1.6.7") // Bettyを使用する場合
+    implementation("io.ktor:ktor-server-netty:1.6.7") // Nettyを使用する場合
     implementation("io.ktor:ktor-websockets:1.6.7") // WebSocketを使用する場合
 
     implementation("ch.qos.logback:logback-classic:1.2.9") // ログを取得する場合
