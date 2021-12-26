fc359a6972aa487b0f25e73d4bbc09dc71077aee
kyoya <kyoya.p@gmail.com>
2021-12-26 22:52:33 +0900

    Ktor Sample WebSocket Client

---
+++ 2022/Ktor/WebSocketClient/build.gradle.kts
@@ -0,0 +1,17 @@
+plugins {
+    kotlin("jvm") version "1.6.10"
+}
+
+group = "org.example"
+version = "1.0-SNAPSHOT"
+
+repositories {
+    mavenCentral()
+}
+
+dependencies {
+    implementation(kotlin("stdlib"))
+    implementation("io.ktor:ktor-client-cio:1.6.7") // CIO-Clientの場合
+
+
+}
