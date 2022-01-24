56581ef3960f996cb3787badc54a93f5bea3cbfb
kyoya <kyoya.p@gmail.com>
2022-01-24 22:15:14 +0900

    n

---
+++ 2022/kotlinMP_win/build.gradle.kts
@@ -0,0 +1,40 @@
+plugins {
+    kotlin("multiplatform") version "1.6.10"
+}
+
+group = "org.example"
+version = "1.0-SNAPSHOT"
+
+repositories {
+    mavenCentral()
+}
+
+kotlin {
+    /* Targets configuration omitted. 
+    *  To find out how to configure the targets, please follow the link:
+    *  https://kotlinlang.org/docs/reference/building-mpp-with-gradle.html#setting-up-targets */
+
+    mingwX64("mingwX64") {
+        binaries { executable() }
+        compilations.getByName("main") {
+            dependencies {
+                implementation(kotlin("stdlib"))
+                implementation(kotlin("stdlib-common"))
+            }
+            enableEndorsedLibs = true
+        }
+    }
+    sourceSets {
+        val commonMain by getting {
+            dependencies {
+                implementation(kotlin("stdlib-common"))
+            }
+        }
+        val commonTest by getting {
+            dependencies {
+                implementation(kotlin("test-common"))
+                implementation(kotlin("test-annotations-common"))
+            }
+        }
+    }
+}
---
+++ 2022/kotlinMP_win/settings.gradle.kts
@@ -0,0 +1,2 @@
+rootProject.name = "kotlinMP_win"
+
