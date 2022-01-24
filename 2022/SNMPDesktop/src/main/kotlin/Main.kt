a32204e4b52fe1264cca6b321871ab44fc19e994
kyoya <kyoya.p@gmail.com>
2022-01-24 22:24:50 +0900

    n

--- 2022/SNMPDesktop/src/main/kotlin/Main.kt
+++ 2022/SNMPDesktop/src/main/kotlin/Main.kt
@@ -3,8 +3,6 @@ import androidx.compose.foundation.layout.*
 import androidx.compose.foundation.rememberScrollState
 import androidx.compose.foundation.verticalScroll
 import androidx.compose.material.*
-import androidx.compose.material.icons.Icons
-import androidx.compose.material.icons.filled.Search
 import androidx.compose.runtime.*
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
