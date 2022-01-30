17d9985cb02abb7aaba4d1ad6166c83e27430e96
kyoya <kyoya.p@gmail.com>
2022-01-31 08:54:25 +0900

    n

--- 2022/kotlinNativePosix/src/nativeMain/kotlin/Main.kt
+++ 2022/kotlinNativePosix/src/nativeMain/kotlin/Main.kt
@@ -4,15 +4,16 @@ import kotlinx.cinterop.*
 fun main() = memScoped {
     val endpoint = "127.0.0.1"
     val port = 8080
-    //var socketDescriptor = 0
-    val serverAddr = alloc<sockaddr_in>()
     val socketDescriptor = socket(AF_INET, SOCK_STREAM, 0)
-    with(serverAddr) {
+    val serverAddr = alloc<sockaddr_in>().apply {
         memset(this.ptr, 0, sockaddr_in.size.convert())
         sin_family = AF_INET.convert()
         sin_addr.S_un.S_addr = inet_addr(endpoint)
         sin_port = ((port shr 8) or ((port and 0xff) shl 8)).toUShort()
     }
     bind(socketDescriptor, serverAddr.ptr.reinterpret(), sockaddr_in.size.convert())
+    listen(socketDescriptor, 2)
+    //val buf = CValues
+    //recv(socketDescriptor, buf,buf.size)
     println("sd=$socketDescriptor")
 }
