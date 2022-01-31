ce0e41f1e60721e598a2def86728731ee69db760
WIN\s117781 <a>
2022-01-31 22:26:53 +0900

    n

--- 2022/kotlinMP_win_TODO/src/mingwX64Main/kotlin/Main.kt
+++ 2022/kotlinMP_win_TODO/src/mingwX64Main/kotlin/Main.kt
@@ -6,6 +6,17 @@ fun main() {
     commonMain()
 }
 
-fun a() {
-
+actual fun winDialog_SAMPLE() {
+    val message = StringBuilder()
+    memScoped {
+        val buffer = allocArray<UShortVar>(MAX_PATH)
+        GetModuleFileNameW(null, buffer, MAX_PATH)
+        val path = buffer.toKString().split("\\").dropLast(1).joinToString("\\")
+        message.append("PATH $path\n")
+    }
+    MessageBoxW(
+        null,
+        "Text\n$message",
+        "Dialog", (MB_YESNOCANCEL or MB_ICONQUESTION).convert()
+    )
 }
