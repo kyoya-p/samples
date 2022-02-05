42586c7981915ebb35eb0e72347297bf80bf58fe
kyoya <kyoya.p@gmail.com>
2022-02-05 21:03:48 +0900

    n

--- 2021/kotlin2021/src/test/kotlin/T5-Collections.kt
+++ 2021/kotlin2021/src/test/kotlin/T5-Collections.kt
@@ -1,4 +1,11 @@
+import kotlinx.serialization.ExperimentalSerializationApi
+import kotlinx.serialization.decodeFromString
+import kotlinx.serialization.encodeToString
+import kotlinx.serialization.json.Json
+import kotlinx.serialization.json.decodeFromStream
+import kotlinx.serialization.json.encodeToStream
 import org.junit.jupiter.api.Test
+import java.io.ByteArrayOutputStream
 
 @Suppress("NonAsciiCharacters")
 class `T5-Collections` {
@@ -6,7 +13,7 @@ class `T5-Collections` {
     fun `t1-無限シーケンス`() {
         fun infinit() = sequence { while (true) yield(Unit) }
 
-        infinit().take(10).forEachIndexed { i, e ->
+        infinit().take(10).forEachIndexed { i, _ ->
             println(i)
         }
     }
@@ -18,4 +25,17 @@ class `T5-Collections` {
         sequenceOf(1, 2, 3).repeat(2).forEach { println(it) }
     }
 
+    @ExperimentalSerializationApi
+    @Test
+    fun `t3-Json化`() {
+        val json = Json
+
+        assert(json.decodeFromString<List<Int>>("[1,5,2,4]") == listOf(1, 5, 2, 4))
+        assert(json.encodeToString(listOf(5, 4, 1)) == "[5,4,1]")
+        assert(json.decodeFromStream<List<Int>>("[1,5,2,4]".byteInputStream()) == listOf(1, 5, 2, 4))
+
+        val byteArrayStream = ByteArrayOutputStream()
+        json.encodeToStream(listOf(5, 4, 1), byteArrayStream)
+        assert(byteArrayStream.toString() == "[5,4,1]")
+    }
 }
