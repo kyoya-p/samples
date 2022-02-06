fc514b9ec679859fb3ad01fc2cec09d69b469781
kyoya <kyoya.p@gmail.com>
2022-02-07 08:57:45 +0900

    n

--- 2022/WebSED/build.gradle.kts
+++ 2022/WebSED/build.gradle.kts
@@ -1,5 +1,6 @@
 plugins {
     kotlin("js") version "1.6.10"
+    kotlin("plugin.serialization") version "1.6.10"
 }
 
 group = "me.kyoya"
@@ -11,6 +12,10 @@ repositories {
 }
 
 dependencies {
+    implementation(kotlin("stdlib-js"))
+    implementation("org.jetbrains.kotlinx:kotlinx-coroutines-core:1.6.0")
+    implementation("org.jetbrains.kotlinx:kotlinx-serialization-runtime-js:1.0-M1-1.4.0-rc")
+
     //testImplementation(kotlin("test"))
     //implementation("org.jetbrains.kotlinx:kotlinx-html:0.7.2")
 }
