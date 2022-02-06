02066b1ac5717041a6d74939f40a286dca7a646c
kyoya <kyoya.p@gmail.com>
2022-02-06 18:42:42 +0900

    n

--- 2022/WebSED/src/main/kotlin/Client.kt
+++ 2022/WebSED/src/main/kotlin/Client.kt
@@ -3,9 +3,6 @@ import kotlinx.html.dom.append
 import org.w3c.dom.Node
 import kotlinx.browser.document
 import kotlinx.browser.window
-import org.w3c.dom.NodeFilter
-import org.w3c.dom.Text
-import org.w3c.dom.Window
 
 fun main() {
     window.onload = { document.body?.sayHello() }
