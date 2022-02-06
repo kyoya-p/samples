ab704d3510b0fd40ea5fcd58163da46d09fcd349
kyoya <kyoya.p@gmail.com>
2022-02-06 23:04:29 +0900

    n

--- 2022/WebSED/build.gradle.kts
+++ 2022/WebSED/build.gradle.kts
@@ -7,12 +7,12 @@ version = "1.0-SNAPSHOT"
 
 repositories {
     mavenCentral()
-    maven("https://maven.pkg.jetbrains.space/public/p/kotlinx-html/maven")
+    //maven("https://maven.pkg.jetbrains.space/public/p/kotlinx-html/maven")
 }
 
 dependencies {
-    testImplementation(kotlin("test"))
-    implementation("org.jetbrains.kotlinx:kotlinx-html:0.7.2")
+    //testImplementation(kotlin("test"))
+    //implementation("org.jetbrains.kotlinx:kotlinx-html:0.7.2")
 }
 
 kotlin {
