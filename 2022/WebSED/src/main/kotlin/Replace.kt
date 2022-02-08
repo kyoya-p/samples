204a232a77977d31d0b4100ce16669968d622c67
kyoya <kyoya.p@gmail.com>
2022-02-08 09:07:28 +0900

    n

--- 2022/WebSED/src/main/kotlin/Replace.kt
+++ 2022/WebSED/src/main/kotlin/Replace.kt
@@ -1,12 +1,13 @@
 import kotlinx.browser.document
 import org.w3c.dom.Element
 import org.w3c.dom.Text
+import kotlin.js.RegExp
 
 
 @ExperimentalJsExport
 @JsExport
 fun r() {
-    val regexTable = mapOf("[aA]" to "[Aa]")
+    val regexTable = mapOf("([aA])" to "$1")
     replaceNodeText(document.body) { sed(it, regexTable) }
 }
 
