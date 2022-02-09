2edcf85a72bc43be884a12970364127bf17a9c74
kyoya <kyoya.p@gmail.com>
2022-02-10 08:28:13 +0900

    n

--- 2022/RXWeb/build.gradle.kts
+++ 2022/RXWeb/build.gradle.kts
@@ -1,5 +1,6 @@
 plugins {
     id("org.jetbrains.kotlin.js") version "1.6.10"
+    kotlin("plugin.serialization") version "1.6.10"
 }
 
 group = "org.example"
@@ -7,10 +8,16 @@ version = "1.0-SNAPSHOT"
 
 repositories {
     mavenCentral()
+    maven { url = uri("https://maven.pkg.jetbrains.space/public/p/kotlinx-html/maven") }
 }
 
 dependencies {
     implementation(kotlin("stdlib-js"))
+    implementation("org.jetbrains.kotlinx:kotlinx-coroutines-core:1.6.0")
+    implementation("org.jetbrains.kotlinx:kotlinx-serialization-runtime-js:1.0-M1-1.4.0-rc")
+
+    implementation("org.jetbrains.kotlinx:kotlinx-html:0.7.2")
+    implementation("org.jetbrains.kotlinx:kotlinx-html-js:0.7.2")
 }
 
 kotlin {
@@ -18,7 +25,7 @@ kotlin {
         browser {
             webpackTask {
                 cssSupport.enabled = true
-                output.libraryTarget = "commonjs2"
+                //output.libraryTarget = "commonjs2"
             }
 
             runTask {
