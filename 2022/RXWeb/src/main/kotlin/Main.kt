d358d8791046d99bb3799240659b21d7584e8a9f
kyoya <kyoya.p@gmail.com>
2022-02-15 08:55:25 +0900

    n

--- 2022/RXWeb/src/main/kotlin/Main.kt
+++ 2022/RXWeb/src/main/kotlin/Main.kt
@@ -1,13 +1,15 @@
 import kotlinx.browser.document
 import kotlinx.browser.window
 import kotlinx.coroutines.await
-import org.w3c.fetch.Headers
+import org.w3c.fetch.CORS
 import org.w3c.fetch.RequestInit
+import org.w3c.fetch.RequestMode
 
 //@JsName("CryptoJS")
 //external fun CryptoJS()
 
 suspend fun main() {
+    window.alert("start 1")
     load("https://cdnjs.com/libraries/crypto-js")
     val hash = js("CryptoJS.MD5('Message')")
     window.alert(hash)
@@ -18,8 +20,12 @@ suspend fun main() {
 //    window.alert(document.body?.outerHTML ?: "nobody")
 }
 
-suspend fun load(url: String) = window.fetch(url,
-    RequestInit(headers = Headers().apply { set("Access-Control-Allow-Origin", url) })
+suspend fun load(url: String) = window.fetch(
+    url,
+    RequestInit(
+        //headers = Headers().apply { set("Access-Control-Allow-Origin", url) },
+        mode = RequestMode.CORS,
+    ),
 ).await().text().await()
 
 //fun function(jscode: String) = js("window.Function(jscode)")
