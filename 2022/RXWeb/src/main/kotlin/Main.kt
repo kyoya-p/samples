cc3220308325f4288ab8cf3ec8f1ca3f8f1bbe2b
kyoya <kyoya.p@gmail.com>
2022-02-15 18:46:59 +0900

    n

--- 2022/RXWeb/src/main/kotlin/Main.kt
+++ 2022/RXWeb/src/main/kotlin/Main.kt
@@ -25,7 +25,7 @@ suspend fun main() {
 suspend fun load(url: String) = window.fetch(
     url,
     RequestInit(
-//        headers = Headers().apply { set("Access-Control-Allow-Origin", url) },
+        headers = Headers().apply { set("Access-Control-Allow-Origin", url) },
         //headers = Headers().apply { set("myheader", url) },
         //mode = RequestMode.CORS,
     ),
