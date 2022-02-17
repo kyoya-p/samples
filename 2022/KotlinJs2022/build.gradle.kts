c703786f83adf4bfb859dcab75d998375f0bbae1
kyoya <kyoya.p@gmail.com>
2022-02-17 21:33:38 +0900

    n

---
+++ 2022/KotlinJs2022/build.gradle.kts
@@ -0,0 +1,36 @@
+plugins {
+    id("org.jetbrains.kotlin.js") version "1.6.10"
+}
+
+group = "org.example"
+version = "1.0-SNAPSHOT"
+
+repositories {
+    mavenCentral()
+}
+
+dependencies {
+    implementation(kotlin("stdlib-js"))
+}
+
+kotlin {
+    js {
+        browser {
+            webpackTask {
+                cssSupport.enabled = true
+            }
+
+            runTask {
+                cssSupport.enabled = true
+            }
+
+            testTask {
+                useKarma {
+                    useChromeHeadless()
+                    webpackConfig.cssSupport.enabled = true
+                }
+            }
+        }
+        binaries.executable()
+    }
+}
