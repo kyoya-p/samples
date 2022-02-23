bbdecec2367dc99b9b57a44ea52dbef774d04b07
kyoya <kyoya.p@gmail.com>
2022-02-23 16:26:20 +0900

    n

---
+++ 2022/ComposeWeb/Readme.md
@@ -0,0 +1,12 @@
+
+## Project作成
+[Github](https://github.com/material-components/material-components-web/) から、 /examples/web-landing
+をコピー
+
+## Run
+```
+gradlew jsRun
+```
+ブラウザが起動しサンプルページが表示される。
+
+[メモ - Qiita]()
--- 2022/ComposeWeb/build.gradle.kts
+++ 2022/ComposeWeb/build.gradle.kts
@@ -1,43 +1,37 @@
-import org.jetbrains.compose.compose
+import org.jetbrains.kotlin.gradle.targets.js.nodejs.NodeJsRootExtension
 
 plugins {
-    kotlin("multiplatform") version "1.5.31"
-    id("org.jetbrains.compose") version "1.0.0"
+    kotlin("multiplatform")
+    id("org.jetbrains.compose")
 }
 
-group = "me.kyoya"
-version = "1.0"
-
 repositories {
-    google()
     mavenCentral()
     maven("https://maven.pkg.jetbrains.space/public/p/compose/dev")
+    google()
 }
 
 kotlin {
     js(IR) {
-        browser {
-            testTask {
-                testLogging.showStandardStreams = true
-                useKarma {
-                    useChromeHeadless()
-                    useFirefox()
-                }
-            }
-        }
+        browser()
         binaries.executable()
     }
+
     sourceSets {
         val jsMain by getting {
             dependencies {
+                implementation(npm("highlight.js", "10.7.2"))
                 implementation(compose.web.core)
                 implementation(compose.runtime)
             }
         }
-        val jsTest by getting {
-            dependencies {
-                implementation(kotlin("test-js"))
-            }
-        }
+    }
+}
+
+// a temporary workaround for a bug in jsRun invocation - see https://youtrack.jetbrains.com/issue/KT-48273
+afterEvaluate {
+    rootProject.extensions.configure<NodeJsRootExtension> {
+        versions.webpackDevServer.version = "4.0.0"
+        versions.webpackCli.version = "4.9.0"
     }
 }
