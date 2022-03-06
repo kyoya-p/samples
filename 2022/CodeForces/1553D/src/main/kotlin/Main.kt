fd9dac0acc23f1df91c78ce406126849632c7811
kyoya <kyoya.p@gmail.com>
2022-03-07 08:05:38 +0900

    n

--- 2022/CodeForces/1553D/src/main/kotlin/Main.kt
+++ 2022/CodeForces/1553D/src/main/kotlin/Main.kt
@@ -1,3 +1,15 @@
 fun main() {
+    val keys = readln()
+    keys.forEach { println("[$it]") }
+    val line = readln()
 
+    sequenceBackspace(keys, "", "").forEach {
+        println(it)
+    }
+}
+
+fun sequenceBackspace(keys: String, line: String, s: String): Sequence<String> = when {
+    keys.isEmpty() -> sequenceOf(line)
+    else ->
+        sequenceBackspace(keys.drop(1), line + keys.take(1), s)  + sequenceBackspace(keys.drop(1), line.dropLast(1), s)
 }
