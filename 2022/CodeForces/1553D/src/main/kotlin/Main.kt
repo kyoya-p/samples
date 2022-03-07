cd78ebf5304858cd515972e573df9fe65ea969f7
kyoya <kyoya.p@gmail.com>
2022-03-08 08:30:05 +0900

    n

--- 2022/CodeForces/1553D/src/main/kotlin/Main.kt
+++ 2022/CodeForces/1553D/src/main/kotlin/Main.kt
@@ -1,15 +1,23 @@
 fun main() {
     val keys = readln()
-    keys.forEach { println("[$it]") }
-    val line = readln()
+    val target = readln()
 
-    sequenceBackspace(keys, "", "").forEach {
+    sequenceBackspace(keys, "", target).filter { it == target }.forEach {
         println(it)
     }
 }
 
-fun sequenceBackspace(keys: String, line: String, s: String): Sequence<String> = when {
+fun sequenceBackspace(keys: String, line: String, target: String): Sequence<String> = when {
     keys.isEmpty() -> sequenceOf(line)
+    !canEnterString(line + keys, target) -> sequenceOf()
     else ->
-        sequenceBackspace(keys.drop(1), line + keys.take(1), s)  + sequenceBackspace(keys.drop(1), line.dropLast(1), s)
+        sequenceBackspace(keys.drop(1), line + keys.take(1), target) + sequenceBackspace(keys.drop(1),
+            line.dropLast(1),
+            target)
+}
+
+fun canEnterString(keys: String, target: String): Boolean {
+    val km = keys.groupingBy { it }.eachCount()
+    val tm = target.groupingBy { it }.eachCount()
+    return tm.all { (ch, c) -> (km[ch] ?: 0) >= c }.also { println("($keys,$target):$it") }
 }
