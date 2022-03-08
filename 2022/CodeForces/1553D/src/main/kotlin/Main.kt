c8095b8496ef3280e9dd00bed7be88a0b801d454
kyoya <kyoya.p@gmail.com>
2022-03-09 07:59:49 +0900

    n

--- 2022/CodeForces/1553D/src/main/kotlin/Main.kt
+++ 2022/CodeForces/1553D/src/main/kotlin/Main.kt
@@ -1,23 +1,20 @@
 fun main() {
-    val keys = readln()
-    val target = readln()
-
-    sequenceBackspace(keys, "", target).filter { it == target }.forEach {
-        println(it)
+    repeat(readln().toInt()) {
+        val keys = readln()
+        val target = readln()
+        val r = bs(keys, "", target).any { it == target }
+        println(if (r) "Yes" else "No")
     }
 }
 
-fun sequenceBackspace(keys: String, line: String, target: String): Sequence<String> = when {
+fun bs(keys: String, line: String, target: String): Sequence<String> = when {
     keys.isEmpty() -> sequenceOf(line)
     !canEnterString(line + keys, target) -> sequenceOf()
-    else ->
-        sequenceBackspace(keys.drop(1), line + keys.take(1), target) + sequenceBackspace(keys.drop(1),
-            line.dropLast(1),
-            target)
+    else -> bs(keys.drop(1), line + keys.take(1), target) + bs(keys.drop(1), line.dropLast(1), target)
 }
 
 fun canEnterString(keys: String, target: String): Boolean {
     val km = keys.groupingBy { it }.eachCount()
     val tm = target.groupingBy { it }.eachCount()
-    return tm.all { (ch, c) -> (km[ch] ?: 0) >= c }.also { println("($keys,$target):$it") }
+    return tm.all { (ch, c) -> (km[ch] ?: 0) >= c } //.also { println("($keys,$target):$it") }
 }
