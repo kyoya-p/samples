81c6c9689ce1f547b2a893da205936a7583171b9
kyoya <kyoya.p@gmail.com>
2022-03-28 07:11:53 +0900

    n

--- 2022/programming/AtCoder/ARC137/src/main/kotlin/B.kt
+++ 2022/programming/AtCoder/ARC137/src/main/kotlin/B.kt
@@ -1,41 +1,36 @@
 package main.b
 
 import testenv.stdioEmulatior
-import testenv.testEnv
 import testenv.testEnv_AtCoder
+
 import java.lang.Integer.max
 import java.lang.Integer.min
 
-fun <T> T.err() = also { System.err.print("[$it]") }
-fun <T> T.errln() = also { System.err.println(it) }
+val <T> T.err get() = also { System.err.print("[$it]") }
+val <T> T.errln get() = also { System.err.println(it) }
+
+val rln get() = readLine()!!
+val rlni get() = rln.toInt()
+val String.sp get() = split(" ")
+val Iterable<String>.mapi get() = map { it.toInt() }
 
 fun main() {
-
     // --------------------------------
     fun main() {
-        val N = readLine()!!.toInt()
-        val V = readLine()!!.split(" ").map { it.toInt() }
-        val c = mutableListOf(0, 0)
-        var dmin = 0
-        var dmax = 0
-        for (e in V) {
-            c[e]++
-            val d = c[1] - c[0]
-            dmin = min(dmin, d)
-            dmax = max(dmax, d)
-            "$e : $dmin ~ $dmax : ${dmax - dmin}".errln()
+        var (d01, dmin, dmax, amax, amin) = listOf(0, 0, 0, 0, 0)
+        val N = rlni
+        rln.sp.mapi.forEach { e ->
+            if (e == 0) --d01 else ++d01
+            amin = min(amin, d01 - dmax)
+            amax = max(amax, d01 - dmin)
+            dmin = min(dmin, d01)
+            dmax = max(dmax, d01)
+            //"$e : $dmax, $dmin, $amax, $amin".errln
         }
-        println(dmax - dmin + 2)
+        println(amax - amin + 1)
     }
 // --------------------------------
-    val e = testEnv {
-        intake.println("5")
-        intake.println("0 0 0")
-        outlet.readLine() == "4"
-    }
-    stdioEmulatior(//listOfNotNull(e) +
-        testEnv_AtCoder("ARC137", "B")
-    ) {
+    stdioEmulatior(testEnv_AtCoder("ARC137", "B")) {
         main()
     }
 }
