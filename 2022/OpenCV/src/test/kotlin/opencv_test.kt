66cbb270ba914597c7975855dd13a81c84eb48c3
kyoya <kyoya.p@gmail.com>
2022-05-03 15:59:25 +0900

    n

--- 2022/OpenCV/src/test/kotlin/opencv_test.kt
+++ 2022/OpenCV/src/test/kotlin/opencv_test.kt
@@ -1,30 +1,52 @@
 import org.junit.jupiter.api.Test
 import org.opencv.core.Core
+import org.opencv.core.Core.hconcat
 import org.opencv.core.CvType
 import org.opencv.core.Mat
 import org.opencv.core.Rect
 import org.opencv.imgcodecs.Imgcodecs.imread
 import org.opencv.imgcodecs.Imgcodecs.imwrite
+import java.io.File
 
 
 class TestOpenCVtext {
     @Test
-    fun test1() {
+    fun Mat() {
         System.loadLibrary(Core.NATIVE_LIBRARY_NAME)
         val mat = Mat.eye(3, 3, CvType.CV_8UC1)
         System.out.println("mat = " + mat.dump())
     }
 
     @Test
-    fun test2() {
-        val x = 41
-        val y = 41
+    fun 画像切抜x3() {
         System.loadLibrary(Core.NATIVE_LIBRARY_NAME)
-        val srcImg = imread("samples/5.jpg")!!
-        val width = srcImg.width() - 41 - x
-        val height = srcImg.height() - 41 - y
-        val rect = Rect(x, y, width, height)
-        val cloppedImg = Mat(srcImg, rect)
-        imwrite("build/out.png", cloppedImg)
+        File("\\\\192.168.11.14\\public\\tmp\\aaaa").walk().filter { it.name.endsWith(".out.png") }.forEach {
+            println(it.name)
+            val srcImg = imread(it.path)!!
+            val dstImg = srcImg.trim(41).tile3()
+            imwrite("build/${it.name}", dstImg)
+        }
+    }
+
+    fun Mat.tile3(): Mat {
+        val dst = clone()!!
+        hconcat(listOf(this, this, this), dst)
+        return dst
+    }
+
+    fun Mat.trim(w: Int): Mat {
+        val left = w
+        val top = w
+        val width = width() - w - left
+        val height = height() - w - top
+        val rect = Rect(left, top, width, height)
+        return Mat(this, rect)
+    }
+
+
+    @Test
+    fun 文字認識() {
+        System.loadLibrary(Core.NATIVE_LIBRARY_NAME)
+
     }
 }
