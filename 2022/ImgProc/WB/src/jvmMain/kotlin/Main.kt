153303fb6d632305077ba58dc090cd02c5428f5e
kyoya <kyoya.p@gmail.com>
2022-05-06 08:50:50 +0900

    n

--- 2022/ImgProc/WB/src/jvmMain/kotlin/Main.kt
+++ 2022/ImgProc/WB/src/jvmMain/kotlin/Main.kt
@@ -4,9 +4,8 @@
 import androidx.compose.desktop.ui.tooling.preview.Preview
 import androidx.compose.foundation.Canvas
 import androidx.compose.foundation.ExperimentalFoundationApi
-import androidx.compose.foundation.layout.*
-import androidx.compose.foundation.lazy.GridCells
-import androidx.compose.foundation.lazy.LazyVerticalGrid
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.material.*
 import androidx.compose.runtime.*
 import androidx.compose.ui.ExperimentalComposeUiApi
@@ -14,25 +13,24 @@ import androidx.compose.ui.Modifier
 import androidx.compose.ui.geometry.Offset
 import androidx.compose.ui.geometry.Size
 import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.input.pointer.PointerEvent
 import androidx.compose.ui.input.pointer.PointerEventType
 import androidx.compose.ui.input.pointer.onPointerEvent
-import androidx.compose.ui.text.font.FontWeight
-import androidx.compose.ui.text.style.TextAlign
-import androidx.compose.ui.unit.dp
-import androidx.compose.ui.unit.sp
 import androidx.compose.ui.window.Window
 import androidx.compose.ui.window.application
+import kotlinx.coroutines.flow.MutableStateFlow
 
 @Composable
 @Preview
 fun App() {
-
     MaterialTheme {
         Scaffold(topBar = { TopAppBar({ Text("White Board") }) },
             content = {
                 var text by remember { mutableStateOf("Try") }
                 Column(content = {
-                    wb()
+                    // countButton_Test(MutableStateFlow(0))
+                    val pixs = MutableList(28) { MutableList(28) { 0 } }
+                    wb(pixs)
                     Button(onClick = { text = "Hello, Desktop!" }) { Text(text) }
                 })
             }
@@ -40,30 +38,55 @@ fun App() {
     }
 }
 
+@Suppress("unused")
+@Composable
+fun countButton_Test(counter: MutableStateFlow<Int>) =
+    Button(onClick = { counter.value++ }) { Text("${counter.collectAsState().value}") }
+
+data class Pixel(val x: Int, val y: Int, val v: Int)
+
+typealias WB = MutableList<MutableList<Int>>
+
 @OptIn(ExperimentalFoundationApi::class, ExperimentalComposeUiApi::class)
 @Composable
-fun wb() {
+fun wb(pixels: WB) {
     val pixelSize = 10
     val width by remember { mutableStateOf(28) }
     val height by remember { mutableStateOf(28) }
-    val pixels by remember { mutableStateListOf<Double>() }
+    val pixFlow = MutableStateFlow(Pixel(0, 0, 0))
+    val pixChg = pixFlow.collectAsState()
+    var drawColor by remember { mutableStateOf(0) }
+    fun onPixel(ev: PointerEvent, onMouse: (Pixel) -> Unit) {
+        val pos = ev.changes.first().position
+        val x = pos.x.toInt() / pixelSize
+        val y = pos.y.toInt() / pixelSize
+        if ((0 until width).contains(x) && (0 until height).contains(y)) onMouse(Pixel(x, y, pixels[y][x]))
+    }
+
     var msPress by remember { mutableStateOf(false) }
     Canvas(modifier = Modifier.fillMaxSize()
         .onPointerEvent(PointerEventType.Move) { ev ->
-            val pos = ev.changes.first().position
-            val x = pos.x.toInt() / pixelSize
-            val y = pos.y.toInt() / pixelSize
-            println("($x,$y) $msPress")
-            if ((0 until width).contains(x) && (0 until height).contains(y)) {
-                pixels[pos.x.toInt() / pixelSize][pos.y.toInt() / pixelSize] = 1.0
+            onPixel(ev) { p ->
+                if (msPress && p.v != drawColor) {
+                    pixels[p.y][p.x] = drawColor
+                    pixFlow.value = Pixel(p.x, p.y, drawColor)
+                }
             }
         }
-        .onPointerEvent(PointerEventType.Press) { msPress = true }
-        .onPointerEvent(PointerEventType.Release) { msPress = false }
+        .onPointerEvent(PointerEventType.Press) { ev ->
+            onPixel(ev) { p ->
+                msPress = true
+                drawColor = 1 - p.v
+                pixels[p.y][p.x] = drawColor
+                pixFlow.value = Pixel(p.x, p.v, drawColor)
+            }
+        }
+        .onPointerEvent(PointerEventType.Release) { ev -> msPress = false }
     ) {
+        println(pixChg.value) // DrawScope内でStateを参照しないと画面が更新されない
         for (y in pixels.indices) {
             for (x in pixels[y].indices) {
-                val color = if (pixels[y][x] == 0.0) Color.Black else Color.White
+                val color = if (pixels[y][x] == 0) Color.Black else Color.White
                 drawRect(color,
                     Offset(x * pixelSize.toFloat(), y * pixelSize.toFloat()),
                     Size(pixelSize.toFloat(), pixelSize.toFloat())
@@ -72,55 +95,6 @@ fun wb() {
         }
     }
 }
-
-@OptIn(ExperimentalMaterialApi::class, ExperimentalComposeUiApi::class)
-@Composable
-fun cell(index: Int, onMouse: () -> Double) {
-    var pixel by remember { mutableStateOf(0.0) }
-    Card(
-        backgroundColor = if (pixel == 0.0) Color.Black else Color.White,
-        modifier = Modifier.padding(0.dp).width(10.dp).height(10.dp)
-            //    .onPointerEvent(PointerEventType.Move) { ev -> pixel = onMouse() }
-            .onPointerEvent(PointerEventType.Enter) { pixel = 1.0 }
-            .onPointerEvent(PointerEventType.Exit) { pixel = 0.0 },
-        onClick = { pixel = 1.0 - pixel },
-    ) {}
-}
-
-
-@OptIn(ExperimentalFoundationApi::class)
-@Composable
-fun grid1() {
-    val list = (1..10).map { it.toString() }
-
-    LazyVerticalGrid(
-        cells = GridCells.Adaptive(128.dp),
-        contentPadding = PaddingValues(start = 12.dp, top = 16.dp, end = 12.dp, bottom = 16.dp),
-        content = {
-            items(list.size) { index ->
-                Card(
-                    backgroundColor = Color.Red,
-                    modifier = Modifier
-                        .padding(1.dp)
-                        .fillMaxWidth(),
-                    elevation = 8.dp,
-                ) {
-                    Text(
-                        text = list[index],
-                        fontWeight = FontWeight.Bold,
-                        fontSize = 30.sp,
-                        color = Color(0xFFFFFFFF),
-                        textAlign = TextAlign.Center,
-                        modifier = Modifier.padding(16.dp)
-                    )
-                }
-            }
-        }
-
-
-    )
-}
-
 
 fun main() = application {
     Window(onCloseRequest = ::exitApplication) {
