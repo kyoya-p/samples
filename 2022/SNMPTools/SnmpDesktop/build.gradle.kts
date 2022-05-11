0523c7bf295b4ea7f9e3d222c6d471019583aff9
WIN\s117781 <a>
2022-05-11 12:35:12 +0900

    n

---
+++ 2022/SNMPTools/SnmpDesktop/build.gradle.kts
@@ -0,0 +1,52 @@
+import org.jetbrains.compose.compose
+import org.jetbrains.compose.desktop.application.dsl.TargetFormat
+import org.jetbrains.kotlin.gradle.tasks.KotlinCompile
+
+plugins {
+    kotlin("multiplatform")
+    id("org.jetbrains.compose")
+}
+
+group = "jp.wjg.shokkaa"
+version = "1.0"
+
+
+repositories {
+    google()
+    mavenCentral()
+    maven("https://maven.pkg.jetbrains.space/public/p/compose/dev")
+
+    mavenLocal()
+}
+
+
+kotlin {
+    jvm {
+        compilations.all {
+            kotlinOptions.jvmTarget = "11"
+        }
+        withJava()
+    }
+    sourceSets {
+        val jvmMain by getting {
+            dependencies {
+                implementation(compose.desktop.currentOs)
+                implementation("jp.wjg.shokkaa:snmp4jutils:1.1")
+                //implementation(project("Snmp4jUtils_KtJvm"))
+                implementation("org.snmp4j:snmp4j:3.6.3")
+            }
+        }
+        val jvmTest by getting
+    }
+}
+
+compose.desktop {
+    application {
+        mainClass = "MainKt"
+        nativeDistributions {
+            targetFormats(TargetFormat.Dmg, TargetFormat.Msi, TargetFormat.Deb)
+            packageName = "demo1"
+            packageVersion = "1.0.0"
+        }
+    }
+}
