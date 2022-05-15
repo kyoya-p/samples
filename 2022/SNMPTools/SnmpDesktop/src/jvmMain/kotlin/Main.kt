3ea9b3d7babb7b0a19b4407d605700ff4f9ec015
kyoya <kyoya.p@gmail.com>
2022-05-15 12:07:54 +0900

    n

--- 2022/SNMPTools/SnmpDesktop/src/jvmMain/kotlin/Main.kt
+++ 2022/SNMPTools/SnmpDesktop/src/jvmMain/kotlin/Main.kt
@@ -6,18 +6,22 @@ import java.io.File
 import java.util.*
 import kotlin.reflect.KProperty
 
+val properties = AppProperty()
+
 @ExperimentalCoroutinesApi
 fun main() = application {
     Window(onCloseRequest = ::exitApplication, title = "SNMP Agent Desktop") { WinApp(window) }
 }
 
-class FileProperty(val prop: Properties = Properties(), val propFile: File = File("build/my.properties")) {
-
+class AppProperty(
+    val prop: Properties = Properties(),
+    val propFile: File = File("${System.getProperty("user.home")}/.snmpagent.properties"),
+) {
     fun <T> T.applyIf(t: Boolean, op: T.() -> Unit) = if (t) apply { op() } else this
     val loadProp get() = prop.applyIf(propFile.exists()) { load(propFile.inputStream()) }
-    fun setAndStore(k:String,v: String) = loadProp.setProperty(k, v)
+    fun setAndStore(k: String, v: String) = loadProp.apply { setProperty(k, v) }.store(propFile.outputStream(), "")
+
     operator fun getValue(r: String?, p: KProperty<*>) = loadProp.getProperty(p.name)
-    operator fun setValue(r: String?, p: KProperty<*>, v: String) = setAndStore(v)
-    loadProp.setProperty(p.name, v).store(propFile.outputStream(), "Comment")
+    operator fun setValue(r: String?, p: KProperty<*>, v: String) = setAndStore(p.name, v)
 }
 
