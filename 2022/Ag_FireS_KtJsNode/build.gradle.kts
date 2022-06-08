507eb240d74283fd3f3ab56f5a600136736bce2c
kyoya <kyoya.p@gmail.com>
2022-06-09 06:43:09 +0900

    TODO

--- 2022/Ag_FireS_KtJsNode/build.gradle.kts
+++ 2022/Ag_FireS_KtJsNode/build.gradle.kts
@@ -11,6 +11,8 @@ repositories {
 }
 
 dependencies {
+    implementation("org.jetbrains.kotlinx:kotlinx-coroutines-core-js:1.6.2") // https://mvnrepository.com/artifact/org.jetbrains.kotlinx/kotlinx-coroutines-core-js
+    //implementation("org.jetbrains.kotlinx:kotlinx-coroutines-core-js:1.6.2") // https://mvnrepository.com/artifact/org.jetbrains.kotlinx/kotlinx-coroutines-core-js
     implementation("org.jetbrains.kotlinx:kotlinx-nodejs:0.0.7")
     implementation(npm("@firebase/firestore", "3.4.9")) // https://www.npmjs.com/package/@firebase/firestore
 }
--- 2022/Ag_FireS_KtJsNode/src/main/kotlin/Main.kt
+++ 2022/Ag_FireS_KtJsNode/src/main/kotlin/Main.kt
@@ -1,5 +1,10 @@
-fun main() {
-    //FirebaseFirestore.getInstance()
+import kotlinx.coroutines.CoroutineScope
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.delay
+
+ fun main() = CoroutineScope(Dispatchers.Default){
+    val job = launch { delay(1000) }
+    delay(500)
     println(greeting("Ag_FireS_KtJsNode"))
 }
 
