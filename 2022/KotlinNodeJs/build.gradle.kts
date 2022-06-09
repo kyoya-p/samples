6ced487ef2963d21f2e792748f7e9f727a00053e
kyoya <kyoya.p@gmail.com>
2022-06-10 08:02:47 +0900

    n

--- 2022/KotlinNodeJs/build.gradle.kts
+++ 2022/KotlinNodeJs/build.gradle.kts
@@ -1,6 +1,5 @@
 plugins {
     kotlin("js") version "1.6.21"
-    id("io.github.turansky.kfc.webpack") version "5.30.0"
 }
 
 group = "me.kyoya"
@@ -11,10 +10,8 @@ repositories {
 }
 
 dependencies {
-    //implementation("org.jetbrains.kotlinx:kotlinx-coroutines-core:1.6.2")
     implementation("org.jetbrains.kotlinx:kotlinx-coroutines-core-js:1.6.2")
     testImplementation(kotlin("test"))
-    //testImplementation("org.jetbrains.kotlinx:kotlinx-coroutines-test:1.6.2")
     testImplementation("org.jetbrains.kotlinx:kotlinx-coroutines-test-js:1.6.2")
 }
 
