69cc5186587a461d8a2ee33f5318e7332456d9a3
kyoya <kyoya.p@gmail.com>
2022-06-10 09:03:39 +0900

    n

--- 2022/KotlinNodeJs/src/main/kotlin/Main.kt
+++ 2022/KotlinNodeJs/src/main/kotlin/Main.kt
@@ -1,9 +1,8 @@
 import kotlinx.coroutines.*
 import kotlinx.coroutines.flow.flow
 
-
 @OptIn(DelicateCoroutinesApi::class)
-suspend fun main() {
+fun main() {
     GlobalScope.launch {
         val jA = launch { scan("192.168.11.1","192.168.11.254" }
         launch { scan("192.168.11.1","192.168.11.254" }
