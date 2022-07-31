024709099892c94d8e3685260b1e6405b724c0bf
kyoya.p <kyoya.p@gmail.com>
2022-07-31 21:24:59 +0900

    n

--- 2022/FileTree_TODO/src/main/kotlin/Main.kt
+++ 2022/FileTree_TODO/src/main/kotlin/Main.kt
@@ -1,16 +1,34 @@
-import org.apache.commons.compress.archivers.zip.ZipFile
-import org.apache.commons.compress.compressors.CompressorStreamFactory
+import org.apache.commons.compress.archivers.jar.JarArchiveInputStream
+import org.apache.commons.compress.archivers.zip.ZipArchiveInputStream
 import java.io.File
 import java.util.*
 
 fun main(args: Array<String>) {
-    File(args.getOrNull(0) ?: ".").walk().forEach { file ->
-        when (file.extension.lowercase(Locale.getDefault())) {
-            "jar", "zip" -> {
-                println("File: ${file.path}")
-                val zipFile = ZipFile(file)
-                zipFile.entries.asSequence().forEach { f -> println(f.name) }
+    //val file = File("./build/libs/filetree-1.0-SNAPSHOT.jar")
+    //val zis = ZipArchiveInputStream(file.inputStream(), null)
+    //file.unjar().forEach { println("name:${it.name} dir:${it.isDirectory}") }
+    tree(File(args.getOrNull(0) ?: "."))
+}
+
+fun tree(file: File) {
+    //println(file.path)
+    val ext = file.extension.lowercase(Locale.getDefault())
+    when {
+        ext == "jar" -> {
+            println("jar: ${file.path}")
+            file.unjar().forEach { ent ->
+                // println("${file.path} : ${ent.name}")
             }
+        }
+        ext == "zip" || ext == "tar" || ext == "ar" -> {
+
+        }
+        file.isDirectory -> {
+            file.listFiles().forEach { child -> tree(child) }
         }
     }
 }
+
+fun unzip(file: File) = ZipArchiveInputStream(file.inputStream(), null).let { generateSequence { it.nextZipEntry } }
+fun File.unjar() = JarArchiveInputStream(inputStream(), null).let { generateSequence { it.nextJarEntry } }
+
