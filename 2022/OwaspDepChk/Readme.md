3c39067a32ccfe6689e41a9542f396bfad3e0999
kyoya.p <kyoya.p@gmail.com>
2022-07-31 15:02:06 +0900

    n

--- 2022/OwaspDepChk/Readme.md
+++ 2022/OwaspDepChk/Readme.md
@@ -1,2 +1,14 @@
-脆弱性チェック: `gradlew dependencyCheckAnalyze`
+OWASP Dependencycheck
+===
+
+脆弱性チェック
+---
+```
+gradlew dependencyCheckAnalyze
+```
+チェック結果: `build/reports` 以下にhtmlファイルが生成される
+
+サンプルプロジェクト
+---
+org.apache.tomcat:tomcat-catalina:8.5.4
 
--- 2022/OwaspDepChk/build.gradle.kts
+++ 2022/OwaspDepChk/build.gradle.kts
@@ -13,7 +13,10 @@ repositories {
 }
 
 dependencies {
-    implementation("org.apache.logging.log4j:log4j-core:2.14.0")
+    // チェックサンプルモジュール
+    // ※脆弱性を含むので検証以外に使用不可
+    //implementation("org.apache.tomcat:tomcat-catalina:8.5.4") // https://mvnrepository.com/artifact/org.apache.tomcat/tomcat-catalina/8.5.4
+    //implementation("org.apache.logging.log4j:log4j-core:2.14.0")  // https://mvnrepository.com/artifact/org.apache.logging.log4j/log4j-core/2.14.0
     implementation("org.apache.activemq:activemq-all:5.15.10") // https://mvnrepository.com/artifact/org.apache.activemq/activemq-all
 }
 
