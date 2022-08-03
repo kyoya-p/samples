684c112c56613c1a679e971436522ca881d5138b
kyoya <kyoya.p@gmail.com>
2022-08-03 13:19:26 +0900

    n

--- 2022/KtMp/KtComposeDesktopWin/Readme.md
+++ 2022/KtMp/KtComposeDesktopWin/Readme.md
@@ -5,3 +5,20 @@ IntelliJ 2022.2
 
 File ➔ 新規 ➔ プロジェクト ➔ Compose Multiplatform 
 ➔ SinglePlatform / Desktop
+
+ビルド/実行
+---
+## Windows 実行ファイル.exe
+```
+gradlew createDistributable
+```
+#### ターゲットファイル
+`.\build\compose\binaries\main\app\KtComposeDesktopWin`
+
+#### 実行
+`.\build\compose\binaries\main\app\KtComposeDesktopWin\KtComposeDesktopWin.exe`
+
+## Windows インストーラ.msi
+```
+gradlew packageMsi
+```
--- 2022/KtMp/KtComposeDesktopWin/src/jvmMain/kotlin/Main.kt
+++ 2022/KtMp/KtComposeDesktopWin/src/jvmMain/kotlin/Main.kt
@@ -1,8 +1,7 @@
 // Copyright 2000-2021 JetBrains s.r.o. and contributors. Use of this source code is governed by the Apache 2.0 license that can be found in the LICENSE file.
-import androidx.compose.material.MaterialTheme
 import androidx.compose.desktop.ui.tooling.preview.Preview
-import androidx.compose.material.Button
-import androidx.compose.material.Text
+import androidx.compose.foundation.layout.Column
+import androidx.compose.material.*
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.getValue
 import androidx.compose.runtime.mutableStateOf
@@ -10,22 +9,40 @@ import androidx.compose.runtime.remember
 import androidx.compose.runtime.setValue
 import androidx.compose.ui.window.Window
 import androidx.compose.ui.window.application
+import java.net.ServerSocket
 
 @Composable
 @Preview
-fun App() {
-    var isOpened by remember { mutableStateOf(false) }
+fun App() = MaterialTheme {
+    var socket by remember { mutableStateOf<ServerSocket?>(null) }
+    var port by remember { mutableStateOf("0") }
 
-    MaterialTheme {
-        Button(onClick = {
+    Scaffold(topBar = { TopAppBar(title = { Text("TCP Port Opener") }) }) {
+        Column {
 
-        }) {
-            if (isOpened) Text("Listen")
-            else Text("Close")
+            TextField(port, onValueChange = { u ->
+                runCatching { port.toInt() }.onSuccess { port = u }
+            })
+            Button(onClick = {
+                if (socket == null) {
+                    socket = ServerSocket(port.toInt())
+                } else {
+                    socket?.close()
+                    socket = null
+                }
+            }) {
+                if (socket == null) {
+                    Text("Start listening")
+                } else {
+                    Text("Stop listening")
+                }
+            }
         }
     }
 }
 
 fun main() = application {
-    Window(onCloseRequest = ::exitApplication) { App() }
+    Window(onCloseRequest = ::exitApplication) {
+        App()
+    }
 }
