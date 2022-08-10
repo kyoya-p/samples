a82c6ce33af0d904e0a1d3f5991230812da9c94d
kyoya.p <kyoya.p@gmail.com>
2022-08-10 21:37:07 +0900

    n

--- 2022/KtMp/KtNativeWin/src/nativeMain/kotlin/sample/InstallService.kt
+++ 2022/KtMp/KtNativeWin/src/nativeMain/kotlin/sample/InstallService.kt
@@ -3,114 +3,107 @@ package sample
 import kotlinx.cinterop.*
 import platform.windows.*
 
-fun installService() = memScoped {
-    /* See: https://docs.microsoft.com/en-us/windows/win32/api/winsvc/nf-winsvc-openscmanagerw
-    SC_HANDLE OpenSCManagerW(
-      [in, optional] LPCWSTR lpMachineName,
-      [in, optional] LPCWSTR lpDatabaseName,
-      [in]           DWORD   dwDesiredAccess
-    );
-    */
-    val hSCManager = OpenSCManagerW(
-        null,
-        null,
-        SC_MANAGER_ALL_ACCESS.toUInt()
+val serviceName = "My Service1"
+
+fun createService() = memScoped {
+    val hSCManager = openSCManager()
+    if (hSCManager == null) return@memScoped null
+
+    val binaryPathName = getModuleFileName()
+    val hService = CreateServiceW(
+        hSCManager = hSCManager,
+        lpServiceName = serviceName,
+        lpDisplayName = serviceName,
+        dwDesiredAccess = GENERIC_EXECUTE,
+        dwServiceType = SERVICE_WIN32_OWN_PROCESS,
+        dwStartType = SERVICE_AUTO_START,
+        dwErrorControl = SERVICE_ERROR_NORMAL,
+        lpBinaryPathName = binaryPathName,
+        lpdwTagId = null,
+        lpLoadOrderGroup = null,
+        lpDependencies = null,
+        lpServiceStartName = null,
+        lpPassword = null,
     )
-
-    val serviceState = SERVICE_ACTIVE or SERVICE_INACTIVE
-    val bytesNeeded = alloc<DWORDVar>()
-    val servicesReturned = alloc<DWORDVar>()
-    val resumeHandle = alloc<DWORDVar> { value = 0u }
-
-    EnumServicesStatusW(
-        hSCManager,
-        SERVICE_WIN32_OWN_PROCESS.toUInt(),
-        serviceState.toUInt(),
-        null,
-        0,
-        bytesNeeded.ptr,
-        servicesReturned.ptr,
-        null,
-    )
-
-    // unionの使用
-    // https://kotlinlang.org/docs/mapping-struct-union-types-from-c.html#inspect-generated-kotlin-apis-for-a-c-library
-
-//    val cUnion = cValue<Union1> {
-//        i = 0x01020304
-//    }
-//    val i = cUnion.useContents { i }
-//    val b = cUnion.useContents { b }
-//    val b1 = (3 downTo 0).map { b[it].toString(0x10) }.joinToString(",")
-//
-//    println("cUnion.i=${i.toString(0x10)}")
-//    println("cUnion.b=$b1")
-
-    val bufferSize = bytesNeeded.value.toLong()
-    val buffer = allocArray<ByteVar>(bufferSize)
-    val services = buffer.reinterpret<ENUM_SERVICE_STATUSW>()
-    resumeHandle.value = 0u
-
-    sequence {
-        /* See: https://docs.microsoft.com/en-us/windows/win32/api/winsvc/nf-winsvc-enumservicesstatusw
-               BOOL EnumServicesStatusW(
-                 [in]                SC_HANDLE              hSCManager,
-                 [in]                DWORD                  dwServiceType,
-                 [in]                DWORD                  dwServiceState,
-                 [out, optional]     LPENUM_SERVICE_STATUSW lpServices,
-                 [in]                DWORD                  cbBufSize,
-                 [out]               LPDWORD                pcbBytesNeeded,
-                 [out]               LPDWORD                lpServicesReturned,
-                 [in, out, optional] LPDWORD                lpResumeHandle
-               );
-               */
-        while (true) {
-            val rc = EnumServicesStatusW(
-                hSCManager,
-                SERVICE_WIN32_OWN_PROCESS.toUInt(),
-                serviceState.toUInt(),
-                services[0].ptr,
-                bufferSize.toUInt(),
-                bytesNeeded.ptr,
-                servicesReturned.ptr,
-                resumeHandle.ptr,
-            )
-            println("rc: $rc")
-            println("bytesNeeded: ${bytesNeeded.value}")
-            println("resumeHandle: ${resumeHandle.value}")
-            println("servicesReturned: ${servicesReturned.value}")
-
-            val result = (0 until servicesReturned.value.toInt()).map { services[it] }
-
-            if (rc == 0) {
-                val err = GetLastError().toInt()
-                val msg = when (err) {
-                    ERROR_ACCESS_DENIED -> "ERROR_ACCESS_DENIED"
-                    ERROR_INVALID_HANDLE -> "ERROR_INVALID_HANDLE"
-                    ERROR_INVALID_PARAMETER -> "ERROR_INVALID_PARAMETER"
-                    ERROR_MORE_DATA -> "ERROR_MORE_DATA"
-                    else -> "UNK($err)"
-                }
-                println("LastError: $msg")
-                if (err == ERROR_MORE_DATA) yield(result)
-                else return@sequence
-            } else {
-                yield(result)
-                return@sequence
-            }
+    if (hService == null) {
+        val msg = when (val err = GetLastError().toInt()) {
+            ERROR_ACCESS_DENIED -> "ERROR_ACCESS_DENIED"
+            ERROR_CIRCULAR_DEPENDENCY -> "ERROR_CIRCULAR_DEPENDENCY"
+            ERROR_DUPLICATE_SERVICE_NAME -> "ERROR_DUPLICATE_SERVICE_NAME"
+            ERROR_INVALID_HANDLE -> "ERROR_INVALID_HANDLE"
+            ERROR_INVALID_NAME -> "ERROR_INVALID_NAME"
+            ERROR_INVALID_PARAMETER -> "ERROR_INVALID_PARAMETER"
+            ERROR_INVALID_SERVICE_ACCOUNT -> "ERROR_INVALID_SERVICE_ACCOUNT"
+            ERROR_SERVICE_EXISTS -> "ERROR_SERVICE_EXISTS"
+            ERROR_SERVICE_MARKED_FOR_DELETE -> "ERROR_SERVICE_MARKED_FOR_DELETE"
+            else -> "Unknown($err)"
         }
-    }.flatMap { it }.forEachIndexed { i, e ->
-        print("$i,")
-        print(" ${e.lpServiceName?.toKString()},")
-        print(" ${e.lpDisplayName?.toKString()},")
-        print(" ${e.ServiceStatus.dwServiceType},")
-        print(" ${e.ServiceStatus.dwCheckPoint},")
-        print(" ${e.ServiceStatus.dwWaitHint},")
-        print(" ${e.ServiceStatus.dwCurrentState},")
-        print(" ${e.ServiceStatus.dwControlsAccepted},")
-        print(" ${e.ServiceStatus.dwServiceSpecificExitCode},")
-        print(" ${e.ServiceStatus.dwWin32ExitCode}")
-        println()
+        println("LastError=$msg")
+    } else {
+        CloseServiceHandle(hService)
     }
     CloseServiceHandle(hSCManager)
+}
+
+fun openSCManager(): SC_HANDLE? {
+    val hSCManager = OpenSCManagerW(null, null, SC_MANAGER_ALL_ACCESS)
+
+    if (hSCManager == null) {
+        val msg = when (val err = GetLastError().toInt()) {
+            ERROR_ACCESS_DENIED -> "ERROR_ACCESS_DENIED"
+            ERROR_DATABASE_DOES_NOT_EXIST -> "ERROR_DATABASE_DOES_NOT_EXIST"
+            else -> "Unknown($err)"
+        }
+        println("LastError=$msg")
+        return null
+    }
+    return hSCManager
+}
+
+fun getModuleFileName() = memScoped {
+    val path = allocArray<WCHARVar>(MAX_PATH + 1)
+    val pathLen = GetModuleFileNameW(
+        hModule = null,
+        lpFilename = path,
+        nSize = MAX_PATH.toUInt()
+    )
+    path[pathLen.toInt()] = 0x00u
+    path.toKString()
+}
+
+fun openService(): SC_HANDLE? {
+    val hSCManager = openSCManager()
+    if (hSCManager == null) return null
+
+    val hService = OpenServiceW(hSCManager, serviceName, SC_MANAGER_ALL_ACCESS)
+    if (hService == null) {
+        val msg = when (val err = GetLastError().toInt()) {
+            ERROR_ACCESS_DENIED -> "ERROR_ACCESS_DENIED"
+            ERROR_INVALID_HANDLE -> "ERROR_INVALID_HANDLE"
+            ERROR_INVALID_NAME -> "ERROR_INVALID_NAME"
+            ERROR_SERVICE_DOES_NOT_EXIST -> "ERROR_SERVICE_DOES_NOT_EXIST"
+            else -> "Unknown($err)"
+        }
+        println("LastError=$msg")
+    }
+
+    CloseServiceHandle(hSCManager)
+    return hService
+}
+
+fun deleteService() {
+    val hService = openService()
+    if (hService == null) return
+
+    if (DeleteService(hService) == 0) {
+        val msg = when (val err = GetLastError().toInt()) {
+            ERROR_ACCESS_DENIED -> "ERROR_ACCESS_DENIED"
+            ERROR_INVALID_HANDLE -> "ERROR_INVALID_HANDLE"
+            ERROR_SERVICE_MARKED_FOR_DELETE -> "ERROR_SERVICE_MARKED_FOR_DELETE"
+            else -> "Unknown($err)"
+        }
+        println("LastError=$msg")
+    }
+
+    CloseServiceHandle(hService)
 }
--- 2022/KtMp/KtNativeWin/src/nativeTest/kotlin/Test.kt
+++ 2022/KtMp/KtNativeWin/src/nativeTest/kotlin/Test.kt
@@ -1,9 +1,35 @@
+import kotlinx.cinterop.*
+import platform.windows.*
+import sample.getModuleFileName
+import sample.createService
+import sample.deleteService
 import sample.unionSample
 import kotlin.test.Test
 
 class test {
     @Test
-    fun uniontest(){
+    fun uniontest() {
         unionSample()
+    }
+
+    @Test
+    fun getCommandLine() {
+        val pCommand = GetCommandLineW()
+        println(pCommand?.toKString())
+    }
+
+    @Test
+    fun GetModuleFileName() {
+        println(getModuleFileName())
+    }
+
+    @Test
+    fun installServiceTest() {
+        createService()
+    }
+
+    @Test
+    fun deleteServiceTest() {
+        deleteService()
     }
 }
