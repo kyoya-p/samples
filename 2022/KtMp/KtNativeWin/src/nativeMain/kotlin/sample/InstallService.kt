18a9689ed96dac8dc7968fd73125d52bcc962e33
kyoya.p <kyoya.p@gmail.com>
2022-08-11 23:01:05 +0900

    n

--- 2022/KtMp/KtNativeWin/src/nativeMain/kotlin/sample/InstallService.kt
+++ 2022/KtMp/KtNativeWin/src/nativeMain/kotlin/sample/InstallService.kt
@@ -45,6 +45,47 @@ fun createService() = memScoped {
     CloseServiceHandle(hSCManager)
 }
 
+fun createServiceA() = memScoped {
+    val hSCManager = openSCManager()
+    if (hSCManager == null) return@memScoped null
+
+    val binaryPathName = getModuleFileName()
+
+    val hService = CreateServiceA(
+        hSCManager = hSCManager,
+        lpServiceName = serviceName,
+        lpDisplayName = serviceName,
+        dwDesiredAccess = GENERIC_EXECUTE,
+        dwServiceType = SERVICE_WIN32_OWN_PROCESS,
+        dwStartType = SERVICE_AUTO_START,
+        dwErrorControl = SERVICE_ERROR_NORMAL,
+        lpBinaryPathName = binaryPathName,
+        lpdwTagId = null,
+        lpLoadOrderGroup = null,
+        lpDependencies = null,
+        lpServiceStartName = null,
+        lpPassword = null,
+    )
+    if (hService == null) {
+        val msg = when (val err = GetLastError().toInt()) {
+            ERROR_ACCESS_DENIED -> "ERROR_ACCESS_DENIED"
+            ERROR_CIRCULAR_DEPENDENCY -> "ERROR_CIRCULAR_DEPENDENCY"
+            ERROR_DUPLICATE_SERVICE_NAME -> "ERROR_DUPLICATE_SERVICE_NAME"
+            ERROR_INVALID_HANDLE -> "ERROR_INVALID_HANDLE"
+            ERROR_INVALID_NAME -> "ERROR_INVALID_NAME"
+            ERROR_INVALID_PARAMETER -> "ERROR_INVALID_PARAMETER"
+            ERROR_INVALID_SERVICE_ACCOUNT -> "ERROR_INVALID_SERVICE_ACCOUNT"
+            ERROR_SERVICE_EXISTS -> "ERROR_SERVICE_EXISTS"
+            ERROR_SERVICE_MARKED_FOR_DELETE -> "ERROR_SERVICE_MARKED_FOR_DELETE"
+            else -> "Unknown($err)"
+        }
+        println("LastError=$msg")
+    } else {
+        CloseServiceHandle(hService)
+    }
+    CloseServiceHandle(hSCManager)
+}
+
 fun openSCManager(): SC_HANDLE? {
     val hSCManager = OpenSCManagerW(null, null, SC_MANAGER_ALL_ACCESS)
 
@@ -68,6 +109,17 @@ fun getModuleFileName() = memScoped {
         nSize = MAX_PATH.toUInt()
     )
     path[pathLen.toInt()] = 0x00u
+    path.toKString()
+}
+
+fun getModuleFileNameA() = memScoped {
+    val path = allocArray<CHARVar>(MAX_PATH + 1)
+    val pathLen = GetModuleFileNameA(
+        hModule = null,
+        lpFilename = path,
+        nSize = MAX_PATH.toUInt()
+    )
+    path[pathLen.toInt()] = 0x00
     path.toKString()
 }
 
--- 2022/KtMp/KtNativeWin/src/nativeTest/kotlin/Test.kt
+++ 2022/KtMp/KtNativeWin/src/nativeTest/kotlin/Test.kt
@@ -1,3 +1,5 @@
+import interop.func_in_params
+import interop.func_out_params
 import kotlinx.cinterop.*
 import platform.windows.*
 import sample.getModuleFileName
@@ -31,5 +33,40 @@ class test {
     @Test
     fun deleteServiceTest() {
         deleteService()
+    }
+
+    @Test
+    fun test_in_params() = memScoped {
+        val dw: DWORD = 0x12345678.toUInt() // DWORD = UInt
+        val c: CHAR = 'c'.code.toByte() // CHAR = Byte
+        val wc: WCHAR = 'w'.code.toUShort() //WCHAR = UShort
+        val lpCStr: String = "const string" //  @CCall.CString LPCSTR型の仮引数にはkotlin.String?型を渡す
+        val lpCWStr: String = "const wide string" //  @CCall.WCString LPCWSTR型の仮引数にはkotlin.String?型を渡す
+
+        func_in_params(
+            dw = dw,
+            c = c,
+            wc = wc,
+            lpCStr = lpCStr,
+            lpCWStr = lpCWStr
+        )
+    }
+
+    @Test
+    fun test_out_params() = memScoped {
+        val lpDw_Out: LPDWORD = alloc<DWORDVar>().ptr //  LPDWORD = CPointer<DWORDVar>
+        val lpStr_Out: LPSTR = allocArray<CHARVar>(256) //  LPSTR = CPointer<CHARVar>
+        val lpWStr_Out: LPWSTR = allocArray<WCHARVar>(256) // LPWSTR = CPointer<WCHARVar>
+
+        func_out_params(
+            lpDW_Out = lpDw_Out,
+            lpStr_Out = lpStr_Out,
+            lpWStr_Out = lpWStr_Out
+        )
+
+        //出力を参照
+        val resDw: UInt = lpDw_Out.pointed.value
+        val resStr: String = lpStr_Out.toKString()
+        val resWStr: String = lpWStr_Out.toKString()
     }
 }
