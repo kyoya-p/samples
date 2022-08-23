8a07d95cb617e7f95c2b73479cd47e6566898815
WIN\s117781 <a>
2022-08-23 13:28:28 +0900

    n

--- 2022/Ktor/AuthBasicClient/build.gradle.kts
+++ 2022/Ktor/AuthBasicClient/build.gradle.kts
@@ -1,5 +1,5 @@
 plugins {
-    kotlin("jvm") version "1.6.10"
+    kotlin("jvm") version "1.7.10" // https://kotlinlang.org/docs/releases.html
 }
 
 group = "org.example"
@@ -10,9 +10,8 @@ repositories {
 }
 
 dependencies {
-    implementation(kotlin("stdlib"))
-    implementation("io.ktor:ktor-client-cio:1.6.7") // CIO-Clientの場合
-    implementation("io.ktor:ktor-client-auth-jvm:1.6.7")
+    implementation("io.ktor:ktor-client-cio:2.0.3") // CIO-Clientの場合
+    implementation("io.ktor:ktor-client-auth:2.0.3")
 
     //implementation("ch.qos.logback:logback-classic:1.2.9") //ログを取る場合
 }
--- 2022/Ktor/AuthBasicClient/src/main/kotlin/Client.kt
+++ 2022/Ktor/AuthBasicClient/src/main/kotlin/Client.kt
@@ -1,17 +1,24 @@
 import io.ktor.client.*
-import io.ktor.client.features.auth.*
-import io.ktor.client.features.auth.providers.*
+import io.ktor.client.engine.cio.*
+import io.ktor.client.plugins.auth.*
+import io.ktor.client.plugins.auth.providers.*
 import io.ktor.client.request.*
+import io.ktor.client.statement.*
 import kotlinx.coroutines.runBlocking
 
-fun main() = runBlocking {
-    val client = HttpClient() {
-        install(Auth) {
-            basic {
-                credentials { BasicAuthCredentials("aaa", "aaa") }
-            }
-        }
+fun main(args: Array<String>) = runBlocking {
+    val url = args.getOrNull(0) ?: "http://127.0.0.1:8080/"
+    val userId = args.getOrNull(1) ?: "userName"
+    val password = args.getOrNull(2) ?: "secret"
+
+    val client = HttpClient{
+//        install(Auth) {
+//            basic {
+//                credentials { BasicAuthCredentials(userId, password) }
+//            }
+//        }
+    }.use{client->
+        val res = client.get(url).bodyAsText()
+        println(res)
     }
-    val res = client.get<String>("http://127.0.0.1:8080/")
-    println(res)
 }
