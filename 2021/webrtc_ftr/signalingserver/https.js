163b128e1dc5b4362d093e00014a1bfd07a29f62
kyoya.p <kyoya.p@gmail.com>
2022-08-25 07:34:06 +0900

    no

--- 2021/webrtc_ftr/signalingserver/https.js
+++ 2021/webrtc_ftr/signalingserver/https.js
@@ -10,16 +10,23 @@ const options = {
 //    cert: fs.readFileSync(sslServerCrt)
 };
 
-const server = http.createServer(options,
-    (request, response) => {
-	const url = request.url;
-        fs.readFile("."+url, 'UTF-8',
-        (error, data) => {
-            response.writeHead(200, {'Content-Type':'text/html'});
-            response.write(data);
-            response.end();
-        });
-    }
-);
+const server = http.createServer(options,(request, response) => {
+    const url = request.url;
+    console.log('file:'+url);
+    fs.readFile("."+url, 'UTF-8',
+		(error, data) => {
+		    console.log(error);
+		    if(error!=null) {
+			console.log('no file:'+url);
+			response.writeHead(404);
+			response.end();
+		    }else{
+			response.writeHead(200, {'Content-Type':'text/html'});
+			response.write(data);
+			response.end();
+		    }
+		});
+});
 server.listen(port);
-console.log('server running port:'+port)
+console.log('server running port:'+port);
+
