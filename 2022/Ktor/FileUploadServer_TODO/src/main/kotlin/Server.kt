113c5992c743d8673b39c80ca6fe56d6d3174058
kyoya.p <kyoya.p@gmail.com>
2022-10-01 06:42:19 +0900

    n

--- 2022/Ktor/FileUploadServer_TODO/src/main/kotlin/Server.kt
+++
@@ -1,9 +0,0 @@
-import io.ktor.application.*
-
-fun main(args: Array<String>) = io.ktor.server.netty.EngineMain.main(args)
-
-@Suppress("unused")
-fun Application.module() {
-    install() {
-    }
-}
