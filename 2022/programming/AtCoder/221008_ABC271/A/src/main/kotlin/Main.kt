aaf7595cfdc0487069876bd6f7ff8b3fa0c49dbc
kyoya.p <kyoya.p@gmail.com>
2022-10-08 11:17:27 +0900

    n

--- 2022/programming/AtCoder/221008_ABC271/A/src/main/kotlin/Main.kt
+++ 2022/programming/AtCoder/221008_ABC271/A/src/main/kotlin/Main.kt
@@ -1,3 +1,4 @@
+@file:Suppress("unused")
 
 val <T> T.err get() = also { System.err.print("[$it]") }
 val <T> T.errln get() = also { System.err.println(it) }
@@ -6,15 +7,15 @@ fun <T : Comparable<T>> min(a: T, b: T) = if (a < b) a else b
 fun <T : Comparable<T>> max(vararg a: T) = a.max()!!
 fun <T : Comparable<T>> min(vararg a: T) = a.min()!!
 
-val rln get() = readLine()!!
-val rlni get() = rln.toInt()
+val rl get() = readLine()!!
+val rli get() = rl.toInt()
 val String.sp get() = split(" ")
 val Iterable<String>.mapi get() = map { it.toInt() }
-val rlnvi get() = rln.sp.mapi
+val rlvi get() = rl.sp.mapi
 
 
 fun main() {
-    val i = rlni
+    val i = rli
     val h = "0123456789ABCDEF"
     println("${h[i / 16]}${h[i.rem(16)]}")
 }
