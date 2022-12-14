1af7902a32f7217725b0b138ab7740640787a0f4
kyoya.p <kyoya.p@gmail.com>
2022-12-15 08:22:12 +0900

    n

---
+++ 2022/KtGraphQL/KGraphQL_2_AzFunc/Readme.md
@@ -0,0 +1,1 @@
+https://qiita.com/drafts/bd9767e229ad499900c0/edit
---
+++ 2022/KtGraphQL/KGraphQL_2_AzFunc/src/main/kotlin/com/example/Application.kt
@@ -0,0 +1,25 @@
+package com.example
+
+import io.ktor.server.application.*
+import io.ktor.server.engine.*
+import io.ktor.server.cio.*
+//import com.example.plugins.*
+import com.apurebase.kgraphql.GraphQL
+
+fun main() {
+    embeddedServer(CIO, port = 8080, host = "0.0.0.0", module = Application::module)
+        .start(wait = true)
+}
+
+fun Application.module(testing: Boolean = false) {
+    install(GraphQL) {
+        //configureRouting()
+        playground = true
+        schema {
+            query("hello") {
+                resolver { -> "GQL World" }
+            }
+        }
+    }
+}
+
