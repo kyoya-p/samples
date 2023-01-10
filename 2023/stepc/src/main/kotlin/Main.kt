1f0177d3f85fcaa96b587a046679d679683805c6
kyoya.p <kyoya.p@gmail.com>
2023-01-11 08:54:16 +0900

    n

--- 2023/stepc/src/main/kotlin/Main.kt
+++ 2023/stepc/src/main/kotlin/Main.kt
@@ -3,8 +3,9 @@ import java.io.File
 
 fun main(args: Array<String>): Unit = runBlocking {
     val tgPath = File(args.getOrNull(0) ?: ".")
-    val grFiles = args.drop(1).flatMap { File(it).readLines() }
+    val grFiles = args.drop(1).asSequence().flatMap { File(it).readLines() }
     val lines = tgPath.walk().filter { it.isFile /* && it.extension.lowercase() == "java"*/ }.sumOf { f ->
+        println("${f.canonicalPath} -> ${f.canonicalPath == File("./gradlew").canonicalPath}")
         f.readLines().size
     }
     println(lines)
