0b74baa11890023ccabbfba3de841c5f845f583b
kyoya.p <kyoya.p@gmail.com>
2023-01-12 08:59:54 +0900

    n

--- 2023/stepc/src/main/kotlin/Main.kt
+++ 2023/stepc/src/main/kotlin/Main.kt
@@ -1,12 +1,13 @@
-import kotlinx.coroutines.runBlocking
 import java.io.File
 
-fun main(args: Array<String>): Unit = runBlocking {
-    val tgPath = File(args.getOrNull(0) ?: ".")
-    val grFiles = args.drop(1).asSequence().flatMap { File(it).readLines() }
-    val lines = tgPath.walk().filter { it.isFile /* && it.extension.lowercase() == "java"*/ }.sumOf { f ->
-        println("${f.canonicalPath} -> ${f.canonicalPath == File("./gradlew").canonicalPath}")
-        f.readLines().size
+fun main(args: Array<String>) {
+    val tgDir = File(args.getOrNull(0) ?: ".").absoluteFile
+    val grList = args.drop(1).asSequence().flatMap { File(it).readLines() }.map { it.split(",") }
+    val grMap = grList.sortedBy { -it[1].length }.associate { tgDir.resolve(it[1]) to it[0] }
+    val counters = mutableMapOf<String, Int>()
+    tgDir.walk().filter { it.isFile && it.extension.lowercase() == "txt" }.forEach { f ->
+        val gr = grMap.entries.first { (k, _) -> f.startsWith(k) }.value
+        counters[gr] = (counters[gr] ?: 0) + f.readLines().size
     }
-    println(lines)
+    println(counters)
 }
