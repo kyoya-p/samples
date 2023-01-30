24db0bdca2280c2784f322c301edadd22b411a92
kyoya.p <kyoya.p@gmail.com>
2023-01-30 17:47:34 +0900

    no

--- 2023/AzureWebPubSub/src/pub.ts
+++ 2023/AzureWebPubSub/src/pub.ts
@@ -1,11 +1,30 @@
+import { WebSocket } from "ws"
 import { WebPubSubServiceClient } from "@azure/web-pubsub"
 
-async function main(){
+async function main() {
   const hub = "hub1"
   // let service = new WebPubSubServiceClient(process.env.WebPubSubConnectionString, hub);
   let service = new WebPubSubServiceClient("Endpoint=https://k230116-pubsub.webpubsub.azure.com;AccessKey=uE9qsmpKl9nN2K9L4FseDvt4Jr4B7yxRelebBdfJDs8=;Version=1.0;", hub)
 
   // by default it uses `application/json`, specify contentType as `text/plain` if you want plain-text
   // service.sendToAll(process.argv[2], { contentType: "text/plain" });
-  await service.sendToAll("Hello!", { contentType: "text/plain" })
+
+
+  await service.sendToAll(Date(), { contentType: "text/plain" })
+
+  // let token = await service.getClientAccessToken()
+  // console.log(token.url)
+  // let ws = new WebSocket(token.url)
+  // ws.on('open', () => {
+  //   console.log('connected')
+  //   ws.send(JSON.stringify({
+  //     type: 'sendToGroup',
+  //     event: 'message',
+  //     dataType: 'json',
+  //     data: ['Hello WS']
+  //   }))
+  // })
+
 }
+
+main()
--- 2023/AzureWebPubSub/src/sub.ts
+++ 2023/AzureWebPubSub/src/sub.ts
@@ -4,10 +4,11 @@ import { WebPubSubServiceClient } from "@azure/web-pubsub"
 async function main() {
   const hub = "hub1"
 //  let service = new WebPubSubServiceClient(process.env.WebPubSubConnectionString, hub)
-  let service = new WebPubSubServiceClient("Endpoint=https://k230116-pubsub.webpubsub.azure.com;AccessKey=uE9qsmpKl9nN2K9L4FseDvt4Jr4B7yxRelebBdfJDs8=;Version=1.0;", hub)
-  let token = await service.getClientAccessToken()
-  console.log(token.url)
-  let ws = new WebSocket(token.url)
+  // // let service = new WebPubSubServiceClient("Endpoint=https://k230116-pubsub.webpubsub.azure.com;AccessKey=uE9qsmpKl9nN2K9L4FseDvt4Jr4B7yxRelebBdfJDs8=;Version=1.0;", hub)
+  // let token = await service.getClientAccessToken()
+  // console.log(token.url)
+  // let ws = new WebSocket(token.url)
+  let ws = new WebSocket('wss://k230116-pubsub.webpubsub.azure.com/client/hubs/hub1?access_token=eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJhdWQiOiJ3c3M6Ly9rMjMwMTE2LXB1YnN1Yi53ZWJwdWJzdWIuYXp1cmUuY29tL2NsaWVudC9odWJzL2h1YjEiLCJpYXQiOjE2NzUwNjc3MjQsImV4cCI6MTY3NTA3MTMyNH0.-nyLE-inRagBtR8cYJ_mW4GE18W1tuZLCz_7GLfj6tU')
   ws.on('open', () => console.log('connected'))
   ws.on('message', data => console.log('Message received: %s', data))
 }
