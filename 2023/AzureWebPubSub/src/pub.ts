5bc4b2648463b162cbd3ae1d0e81efd2339fc56f
kyoya.p <kyoya.p@gmail.com>
2023-01-30 18:09:52 +0900

    no

--- 2023/AzureWebPubSub/src/pub.ts
+++ 2023/AzureWebPubSub/src/pub.ts
@@ -9,8 +9,10 @@ async function main() {
   // by default it uses `application/json`, specify contentType as `text/plain` if you want plain-text
   // service.sendToAll(process.argv[2], { contentType: "text/plain" });
 
-
-  await service.sendToAll(Date(), { contentType: "text/plain" })
+  setInterval(
+    () => service.sendToAll(Date(), { contentType: "text/plain" })
+    , 1000 * 60 * 5
+  )
 
   // let token = await service.getClientAccessToken()
   // console.log(token.url)
--- 2023/AzureWebPubSub/src/sub.ts
+++ 2023/AzureWebPubSub/src/sub.ts
@@ -1,9 +1,9 @@
-import { WebSocket} from "ws"
+import { WebSocket } from "ws"
 import { WebPubSubServiceClient } from "@azure/web-pubsub"
 
-async function main() {
+ function main() {
   const hub = "hub1"
-//  let service = new WebPubSubServiceClient(process.env.WebPubSubConnectionString, hub)
+  //  let service = new WebPubSubServiceClient(process.env.WebPubSubConnectionString, hub)
   // // let service = new WebPubSubServiceClient("Endpoint=https://k230116-pubsub.webpubsub.azure.com;AccessKey=uE9qsmpKl9nN2K9L4FseDvt4Jr4B7yxRelebBdfJDs8=;Version=1.0;", hub)
   // let token = await service.getClientAccessToken()
   // console.log(token.url)
