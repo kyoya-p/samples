4f2cba9c9f82e019d20d4e095b3cf3b9aff0461f
kyoya.p <kyoya.p@gmail.com>
2023-02-03 06:52:06 +0900

    no

---
+++ 2023/AzureWebPubSub/src/pub0.ts
@@ -0,0 +1,43 @@
+import { WebSocket } from "ws"
+import { WebPubSubServiceClient } from "@azure/web-pubsub"
+import { argv } from "process"
+
+async function main() {
+  const hub = "hub1"
+  // let service = new WebPubSubServiceClient(process.env.WebPubSubConnectionString, hub);
+  let service = new WebPubSubServiceClient("Endpoint=https://k230116-pubsub.webpubsub.azure.com;AccessKey=uE9qsmpKl9nN2K9L4FseDvt4Jr4B7yxRelebBdfJDs8=;Version=1.0;", hub)
+
+  // by default it uses `application/json`, specify contentType as `text/plain` if you want plain-text
+  // service.sendToAll(process.argv[2], { contentType: "text/plain" });
+
+
+  console.log(Date())
+  service.sendToAll(Date(), { contentType: "text/plain" })
+
+  let ix = 0
+
+  let t = setInterval(() => {
+    let m = `${ix}:${Date()}`
+    console.log(m)
+    service.sendToAll(m, { contentType: "text/plain" })
+    if (++ix >= 10000) clearInterval(t)
+  }
+    , 100
+  )
+
+  // let token = await service.getClientAccessToken()
+  // console.log(token.url)
+  // let ws = new WebSocket(token.url)
+  // ws.on('open', () => {
+  //   console.log('connected')
+  //   ws.send(JSON.stringify({
+  //     type: 'sendToGroup',
+  //     event: 'message',
+  //     dataType: 'json',
+  //     data: ['Hello WS']
+  //   }))
+  // })
+
+}
+
+main()
