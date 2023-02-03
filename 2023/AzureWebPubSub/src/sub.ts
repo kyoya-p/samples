cdd776683ea3c85b18f7950efa4991953c81b03d
kyoya.p <kyoya.p@gmail.com>
2023-02-03 09:49:49 +0900

    no

--- 2023/AzureWebPubSub/src/sub.ts
+++ 2023/AzureWebPubSub/src/sub.ts
@@ -1,7 +1,7 @@
 import { WebSocket } from "ws"
 
  function main() {
-  const url=  process.env.WebPubSunUrl ?? ""
+  const url=  process.env.WebPubSubUrl ?? ""
   let ws = new WebSocket(url)
   ws.on('open', () => console.log('connected'))
   ws.on('message', data => console.log('Message received: %s', data))
