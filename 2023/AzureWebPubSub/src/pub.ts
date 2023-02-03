00cc561e167104b82d18ea8b311c5d847d2a7245
kyoya.p <kyoya.p@gmail.com>
2023-02-03 09:52:15 +0900

    no

--- 2023/AzureWebPubSub/src/pub.ts
+++ 2023/AzureWebPubSub/src/pub.ts
@@ -1,7 +1,8 @@
 import { WebSocket } from "ws"
 
 function main() {
-  const ws = new WebSocket('wss://k230116-pubsub.webpubsub.azure.com/client/hubs/hub1?access_token=eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJhdWQiOiJ3c3M6Ly9rMjMwMTE2LXB1YnN1Yi53ZWJwdWJzdWIuYXp1cmUuY29tL2NsaWVudC9odWJzL2h1YjEiLCJpYXQiOjE2NzUyNTE3NDUsImV4cCI6MTY3NTI1NTM0NSwicm9sZSI6WyJ3ZWJwdWJzdWIuc2VuZFRvR3JvdXAiLCJ3ZWJwdWJzdWIuam9pbkxlYXZlR3JvdXAiXX0.6-krUpUBsqzG8SfT6XWEnucLosEDn9J2WSnAEXgh1BA')
+  const url=  process.env.WebPubSubUrl ?? ""
+  const ws = new WebSocket(url)
 
   ws.on('open', () => {
     let t = setInterval(() => {
