383a49cec6b170769f84fef91c48fddd69cdbfb2
WIN\s117781 <a>
2023-03-22 15:21:40 +0900

    n

---
+++ 2023/KtJsSample/build.gradle.kts
@@ -0,0 +1,23 @@
+plugins {
+    kotlin("js") version "1.8.0"
+}
+
+group = "org.example"
+version = "1.0-SNAPSHOT"
+
+repositories {
+    mavenCentral()
+}
+
+dependencies {
+    testImplementation(kotlin("test"))
+}
+
+kotlin {
+    js {
+        binaries.executable()
+        nodejs {
+
+        }
+    }
+}
