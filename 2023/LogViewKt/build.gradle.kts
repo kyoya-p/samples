cfa67efb4360ba4e803134c9b2bfa3bdc330d780
kyoya <kyoya.p@gmail.com>
2023-04-07 05:44:43 +0900

    n

---
+++ 2023/LogViewKt/build.gradle.kts
@@ -0,0 +1,29 @@
+plugins {
+    kotlin("jvm") version "1.8.0"
+    application
+}
+
+group = "org.example"
+version = "1.0-SNAPSHOT"
+
+repositories {
+    mavenCentral()
+}
+
+dependencies {
+    implementation("org.jetbrains.kotlinx:kotlinx-datetime:0.4.0") // https://mvnrepository.com/artifact/org.jetbrains.kotlinx/kotlinx-datetime
+
+    testImplementation(kotlin("test"))
+}
+
+tasks.test {
+    useJUnitPlatform()
+}
+
+kotlin {
+    jvmToolchain(11)
+}
+
+application {
+    mainClass.set("MainKt")
+}
---
+++ 2023/LogViewKt/src/main/kotlin/Main.kt
@@ -0,0 +1,18 @@
+import kotlinx.datetime.Instant
+import kotlinx.datetime.TimeZone
+import kotlinx.datetime.toLocalDateTime
+import java.io.File
+import kotlin.time.Duration.Companion.milliseconds
+
+fun main() = File("samples").walk().filter { it.isFile }.forEach { f ->
+    var tOrg: Instant = Instant.fromEpochSeconds(0)
+    sequence { f.useLines { it.forEach { yield(it) } } }.forEach {
+        Regex("""ARMM,.*,(\d+)# .*\((\d+)\).*(task start|set interval timer)""").find(it)?.let {
+            tOrg = Instant.fromEpochMilliseconds(it.groupValues[2].toLong() * 1000 - it.groupValues[1].toLong())
+        }
+        Regex("""ARMM,.*,(\d+)# .*""").find(it)?.let { m ->
+            println("${(tOrg + m.groupValues[1].toLong().milliseconds).toLocalDateTime(TimeZone.currentSystemDefault())} ${f.name} $it")
+        }
+        Regex(""".*\[HTTPCL].*(Requestline|ResponseLine).*""").find(it)?.let { _ -> println(it) }
+    }
+}
