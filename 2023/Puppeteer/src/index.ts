8826bb7c24c20b036e6876f0fa34ab9aa94f18fb
kyoya <kyoya.p@gmail.com>
2023-05-29 08:32:26 +0900

    n

--- 2023/Puppeteer/src/index.ts
+++ 2023/Puppeteer/src/index.ts
@@ -1,27 +1,24 @@
-/**
+/*
+headless Webブラウザ上で画面が更新された場合に画像を連続的に抽出するTSのコードサンプルを
 
-ヘッドレスWebブラウザから画面の画像を連続的に(画面の変更があった場合に)抽出する方法は、次のとおりです。
-
-ヘッドレスWebブラウザを起動します。
-スクリーンショットを撮る関数を定義します。
-スクリーンショットを撮る関数を繰り返し呼び出します。
-画面の変更があった場合に、スクリーンショットを保存します。
 */
 
+import { puppeteer } from "puppeteer";
 
-function takeScreenshot() {
-    // 現在の画面をキャプチャします。
-    const canvas = document.createElement('canvas');
-    canvas.width = window.innerWidth;
-    canvas.height = window.innerHeight;
-    const ctx = canvas.getContext('2d')!;
-    ctx.drawImage(document.body, 0, 0);
-  
-    // キャプチャした画像をバイナリデータとして取得します。
-    const data = canvas.toDataURL();
-  
-    // バイナリデータを画像ファイルとして保存します。
-    const imageFile = new File([data], 'screenshot.png');
-    imageFile.saveAs('screenshot.png');
-  }
-  
+async function main() {
+    const browser = await puppeteer.launch();
+    const page = await browser.newPage();
+
+    await page.goto("https://example.com");
+
+    // 画面が更新されたときに画像をキャプチャする
+    page.on("pageChanged", async () => {
+        const image = await page.screenshot();
+        // 画像を保存する
+        await image.save("image.png");
+    });
+
+    // ブラウザを終了する
+    await browser.close();
+}
+main();
