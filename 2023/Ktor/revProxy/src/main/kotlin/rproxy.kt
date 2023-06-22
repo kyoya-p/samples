bf0a9d2430f176be74db163cad7d9b61997740f1
WIN\s117781 <a>
2023-06-23 02:39:14 +0900

    n

---
+++ 2023/Ktor/revProxy/src/main/kotlin/rproxy.kt
@@ -0,0 +1,73 @@
+package io.ktor.samples.reverseproxy
+
+import io.ktor.client.*
+import io.ktor.client.request.*
+import io.ktor.client.statement.*
+import io.ktor.content.TextContent
+import io.ktor.http.*
+import io.ktor.http.content.*
+import io.ktor.server.application.*
+import io.ktor.server.engine.*
+import io.ktor.server.cio.*
+import io.ktor.server.request.*
+import io.ktor.server.response.*
+import io.ktor.util.*
+import io.ktor.utils.io.*
+
+fun main() {
+    val server = embeddedServer(CIO, port = 8080, module = Application::module)
+    server.start(wait = true)
+}
+
+fun Application.module() {
+    val client = HttpClient()
+    val wikipediaLang = "en"
+
+    intercept(ApplicationCallPipeline.Call) {
+        val response = client.request("https://$wikipediaLang.wikipedia.org${call.request.uri}")
+
+        val proxiedHeaders = response.headers
+        val location = proxiedHeaders[HttpHeaders.Location]
+        val contentType = proxiedHeaders[HttpHeaders.ContentType]
+        val contentLength = proxiedHeaders[HttpHeaders.ContentLength]
+
+        fun String.stripWikipediaDomain() = this.replace(Regex("(https?:)?//\\w+\\.wikipedia\\.org"), "")
+
+        if (location != null) {
+            call.response.header(HttpHeaders.Location, location.stripWikipediaDomain())
+        }
+
+        when {
+            contentType?.startsWith("text/html") == true -> {
+                val text = response.bodyAsText()
+                val filteredText = text.stripWikipediaDomain()
+                call.respond(
+                    TextContent(
+                        filteredText,
+                        ContentType.Text.Html.withCharset(Charsets.UTF_8),
+                        response.status
+                    )
+                )
+            }
+
+            else -> {
+                call.respond(object : OutgoingContent.WriteChannelContent() {
+                    override val contentLength: Long? = contentLength?.toLong()
+                    override val contentType: ContentType? = contentType?.let { ContentType.parse(it) }
+                    override val headers: Headers = Headers.build {
+                        appendAll(proxiedHeaders.filter { key, _ ->
+                            !key.equals(
+                                HttpHeaders.ContentType,
+                                ignoreCase = true
+                            ) && !key.equals(HttpHeaders.ContentLength, ignoreCase = true)
+                        })
+                    }
+                    override val status: HttpStatusCode = response.status
+                    override suspend fun writeTo(channel: ByteWriteChannel) {
+                        response.bodyAsChannel().copyAndClose(channel)
+                    }
+                })
+            }
+        }
+    }
+}
