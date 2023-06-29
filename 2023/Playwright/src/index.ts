c3f829f075933e99122d2d5488bf401d9c9b6800
kk <kyoya.p@gmail.com>
2023-06-30 04:58:43 +0900

    no

--- 2023/Playwright/src/index.ts
+++ 2023/Playwright/src/index.ts
@@ -1,19 +1,39 @@
-import { Page, chromium, devices } from 'playwright';
-import assert from 'node:assert';
-import { setMaxIdleHTTPParsers } from 'node:http';
+import { Page, chromium, devices } from 'playwright'
+import express from "express"
 
 main()
 
 async function main() {
-  const browser = await chromium.launch();
-  const page = await browser.newPage();
-  await page.goto('https://google.com/');
 
-  setInterval(async () => {capture(page)}, 3000);
+  const browser = await chromium.launch()
+  const page = await browser.newPage()
+  await page.goto('https://google.com/')
 
-  await browser.close();
+  webServer(page)
+  setInterval(async () => { page.screenshot({ path: 'result/screenshot.png' }) }, 2000)
+
+  // await browser.close()
 }
 
-async function capture(page:Page) {
-    await page.screenshot({path: 'result/screenshot.png'});
+async function capture(page: Page) {
+  await page.screenshot({ path: 'result/screenshot.png' })
+}
+
+let hash = ""
+
+async function webServer(page: Page) {
+  const app = express()
+  app.use(express.static("."));
+  app.get("/click", (req: any, res) => {
+    const x = parseInt(req.query.x)
+    const y = parseInt(req.query.y)
+    page.mouse.click(x, y, { button: "left" })
+    res.send(`{}`)
+  })
+  app.get("", (req, res) => {
+    res.send(`
+    <script src="client.js"></script>
+    <img src="result/screenshot.png?${hash}"  onclick="handleClick(event)"></img>`)
+  })
+  app.listen(3000, () => { console.log("Server is running on port 3000") })
 }
