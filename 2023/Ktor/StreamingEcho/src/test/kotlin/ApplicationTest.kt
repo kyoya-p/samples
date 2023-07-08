3b7e640d08be30beb9cf8e60d430921c6239b2f3
kk <kyoya.p@gmail.com>
2023-07-08 12:15:19 +0900

    n

--- 2023/Ktor/StreamingEcho/src/test/kotlin/ApplicationTest.kt
+++ 2023/Ktor/StreamingEcho/src/test/kotlin/ApplicationTest.kt
@@ -1,11 +1,16 @@
 import io.ktor.client.request.*
 import io.ktor.client.statement.*
+import io.ktor.http.content.OutgoingContent.*
 import io.ktor.server.testing.*
 import io.ktor.util.*
+import io.ktor.util.cio.*
 import io.ktor.utils.io.*
 import kotlinx.coroutines.flow.channelFlow
+import kotlinx.coroutines.flow.collect
+import kotlinx.coroutines.flow.flow
 import kotlinx.datetime.Clock
 import org.junit.Test
+import java.io.File
 
 class ApplicationTest {
 
@@ -25,12 +30,24 @@ class ApplicationTest {
         println("Res(${now()})=$r3")
     }
 
+    @OptIn(InternalAPI::class)
     @Test
     fun test2() = testApplication {
         application { module2() }
         val r = client.post("/s1") {
-            setBody("aaa")
+            body = object : ReadChannelContent() {
+                override fun readFrom() = File("build.gradle.kts").readChannel()
+            }
         }
-        println(r.bodyAsText())
+        val ch = r.content
+        generateFlow { ch.readUTF8Line() }.collect {
+            println(it)
+        }
+    }
+}
+
+suspend fun <T> generateFlow(op: suspend () -> T?) = channelFlow<T> {
+    while (true) {
+        trySend(op() ?: break)
     }
 }
