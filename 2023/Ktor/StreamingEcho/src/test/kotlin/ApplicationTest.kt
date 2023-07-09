00deb09c4debd3eaec6351830494bba0e14c46bc
kk <kyoya.p@gmail.com>
2023-07-10 08:26:56 +0900

    n

--- 2023/Ktor/StreamingEcho/src/test/kotlin/ApplicationTest.kt
+++ 2023/Ktor/StreamingEcho/src/test/kotlin/ApplicationTest.kt
@@ -6,15 +6,15 @@ import io.ktor.util.*
 import io.ktor.util.cio.*
 import io.ktor.utils.io.*
 import kotlinx.coroutines.channels.consumeEach
-import kotlinx.coroutines.flow.channelFlow
-import kotlinx.coroutines.flow.collect
-import kotlinx.coroutines.flow.flow
 import kotlinx.datetime.Clock
 import org.junit.Test
 import java.io.File
 
 import kotlinx.coroutines.channels.produce
+import kotlinx.coroutines.delay
+import kotlinx.coroutines.flow.*
 import kotlinx.coroutines.runBlocking
+import kotlin.time.Duration.Companion.seconds
 
 class ApplicationTest {
 
@@ -36,34 +36,48 @@ class ApplicationTest {
 
     @OptIn(InternalAPI::class)
     @Test
-    fun test2() = runBlocking{testApplication {
-        application { module2() }
-        val ch = ByteChannel()
-        val r = client.post("/s2") {
-            body = object : ReadChannelContent() {
-                //                override fun readFrom() = File("build.gradle.kts").readChannel()
-                override fun readFrom() = <Byte>{}
+    fun test2() = runBlocking {
+        testApplication {
+            application { module2() }
+            val sharedFlow = flow {
+                for (i in 1..10) {
+                    emit(i)
+                    delay(1.seconds)
+                }
+            }.shareIn(this@runBlocking, started = SharingStarted.WhileSubscribed(), replay = 1)
+
+            val ch = ByteChannel()
+            val r = client.post("/s2") {
+                body = object : ReadChannelContent() {
+                    //                override fun readFrom() = File("build.gradle.kts").readChannel()
+                    override fun readFrom() =
+                }
             }
-        }
-        var fib = 1L
-        repeat(10) {
-            ch.writeStringUtf8("$fib\n")
-            ch.flush()
-            fib = r.content.readUTF8Line()?.toLong() ?: return@repeat
-            println(fib)
-        }
+            var fib = 1L
+            repeat(10) {
+                ch.writeStringUtf8("$fib\n")
+                ch.flush()
+                fib = r.content.readUTF8Line()?.toLong() ?: return@repeat
+                println(fib)
+            }
 //        val ch = r.content
-    }}
+        }
+    }
 
     @Test
-    fun t3()= runBlocking {
-        val channel = produce {
-            send("Hello")
-            send("World")
-            close()
+    fun t3() = runBlocking {
+        val flow = flow {
+            for (i in 1..10) {
+                emit(i)
+            }
         }
 
-        channel.consumeEach { println(it) }
+        val channel = flow.asChannel()
+
+        channel.consumeEach {
+            println(it)
+        }
+
     }
 }
 
