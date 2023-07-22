5aca9df3253465bed8d8bd834501d1b97b16ff55
kk <kyoya.p@gmail.com>
2023-07-22 20:20:47 +0000

    n

--- 2023/WebRTC/src/peer-server.ts
+++ 2023/WebRTC/src/peer-server.ts
@@ -1,12 +1,18 @@
-// import adapter from 'webrtc-adapter'
+main()
 
-const peerConnection = new RTCPeerConnection({ "iceServers": [] })
-connect()
-
-async function connect() {
+async function main() {
+    const peerConnection = new RTCPeerConnection({ "iceServers": [] })
+    connect(peerConnection)
+    setInterval(async (peerConnection: RTCPeerConnection) => { broadcast(peerConnection) },1000)
+}
+async function connect(peerConnection: RTCPeerConnection) {
     const sessionDescription = await peerConnection.createOffer()
     peerConnection.setLocalDescription(sessionDescription)
-    const sdp = document.getElementById('sdp') as HTMLTextAreaElement | null 
+    const sdp = document.getElementById('sdp') as HTMLTextAreaElement | null
     console.log(sessionDescription.sdp)
-    sdp!.innerHTML = sessionDescription.sdp ?? "Error"
+    sdp!["innerHTML"] = sessionDescription.sdp ?? "Error"
+}
+
+async function broadcast(peerConnection: RTCPeerConnection) {
+
 }
