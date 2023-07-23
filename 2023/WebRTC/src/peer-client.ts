1c36c0d82dba4e9010334f74c016b67c352fff7e
kk <kyoya.p@gmail.com>
2023-07-24 08:29:03 +0900

    n

--- 2023/WebRTC/src/peer-client.ts
+++ 2023/WebRTC/src/peer-client.ts
@@ -1,9 +1,13 @@
 clientMain()
 
+const sdpField = document.getElementById('sdp') as HTMLTextAreaElement
+const answerField = document.getElementById('sdp-answer') as HTMLTextAreaElement
+
 async function clientMain() {
+
     const recvConnection = new RTCPeerConnection({ "iceServers": [] })
     ifNotNull(document.getElementById('submit-sdp'), (t) => { t.onclick = () => { onSdpSubmit(recvConnection) } })
-    recvConnection.onicecandidate =  ev => {  recvConnection.addIceCandidate(ev.candidate!) }
+    // recvConnection.onicecandidate = ev => { recvConnection.addIceCandidate(ev.candidate!) }
     console.log('Created send data channel')
 }
 
@@ -12,11 +16,21 @@ function ifNotNull<T, R>(t: T | null | undefined, x: (t: T) => R) {
 }
 
 async function onSdpSubmit(recvConnection: RTCPeerConnection) {
-    ifNotNull(document.getElementById('sdp') as HTMLTextAreaElement, async (elm) => {
-        const sdp = elm.value
-        console.log(`SDP=${sdp}`)
-        let answer = new RTCSessionDescription({ type: 'answer', sdp: sdp, })
-        await recvConnection.setRemoteDescription(answer)
-    })
+    const sdp = sdpField.value
+    console.log(`SDP=${sdp}`)
+    const offer = new RTCSessionDescription({ type: 'offer', sdp: sdp, })
+    await recvConnection.setRemoteDescription(offer)
+    const answer = await recvConnection.createAnswer()
+    answerField.value = answer.sdp ?? "Error"
+}
+
+function getSdpWithCandidates(peer: RTCPeerConnection) {
+    return new Promise<string>(resolve =>
+        peer.onicecandidate = ev => {
+            if (ev.candidate == null) {
+                resolve(peer.localDescription?.sdp ?? "")
+            }
+        }
+    )
 }
 
