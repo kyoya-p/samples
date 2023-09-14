c703b2824baeaefc0911abf7feaf8b43aa0d6fdb
kk <kyoya.p@gmail.com>
2023-09-15 08:29:04 +0900

    n

--- 2023/Azure/FuncAzure/func1/index.ts
+++ 2023/Azure/FuncAzure/func1/index.ts
@@ -1,31 +1,32 @@
 import {  AzureFunction, Context,    HttpRequest,  HttpResponseSimple,} from "@azure/functions";
 
 const httpTrigger: AzureFunction = async (  context: Context) => {
-  await context.log(`**** Start function`);
   const req:HttpRequest=context.req
-  await context.log(`header.cookie=${req.headers.cookie}`);
   const cookies = req.headers.cookie;
   const cookieMap = cookies.split(";").map((e) => e.trim()).reduce((map, cookie) => {
     const [key, value] = cookie.split("=");
-    map[key] = value;
-    return map;
+    map[key] = value
+    return map
   }, {});
 
   if (req.query.tgOrigin) {
-    await context.log(`** Redirect to ${req.query.tgOrigin}`)
     const url = new URL(req.url)
+    const location = `${url.origin}${url.pathname}`
+    const tgOrigin = decodeURIComponent(req.query.tgOrigin)
+    await context.log(`** Redirect to ${location}. cookie[tgOrigin]=${tgOrigin}`)
     const res: HttpResponseSimple = {
       status: 302,
-      headers: { Location: `${url.origin}${url.pathname}` },
-      cookies: [{ name: "tgOrigin", value: `${req.query.tgOrigin}` }],
+      headers: { Location: location },
+      cookies: [{ name: "tgOrigin", value: tgOrigin }],
     }
     context.res = res
   } else if (cookieMap["tgOrigin"]) {
-    await context.log(`** Reverse Proxy to ${cookieMap["tgOrigin"]}`);
-    const tgPath = context.req.query.tgPath as string | undefined;
+    const tgOrigin = decodeURIComponent( (cookieMap["tgOrigin"]))
+    await context.log(`** Reverse Proxy to ${tgOrigin}`);
+    const tgPath = decodeURIComponent(context.req.query.tgPath as string | undefined)
     context.res = <HttpResponseSimple>{
       status: 200,
-      body: `Reverse Proxy to ${cookieMap["tgOrigin"]}`,
+      body: `Reverse Proxy to ${tgOrigin}`,
     };
   } else {
     await context.log(`** Set the tgOrigin parameter to specify the target host.`);
--- 2023/Azure/FuncExpress/func1/index.ts
+++ 2023/Azure/FuncExpress/func1/index.ts
@@ -1,35 +1,17 @@
-import { AzureFunction, Context, HttpRequest } from "@azure/functions"
-import express from "express"
-
+import express from "express";
+import { Context, HttpRequest } from "@azure/functions";
 
 const app = express();
 
 app.get("/", async (req: HttpRequest, context: Context) => {
-  // クエリパラメータを取得する
-  const name = req.query.name || "";
+  // Cookie を読み取る
+  const cookies = req.headers.cookies;
 
   // 応答を返す
   return context.res.json({
-    name,
+    cookies,
   });
 });
 
+// Azure Functions にデプロイする
 export default app;
-
-
-
-const httpTriggerX: AzureFunction = async function (context: Context, req: HttpRequest): Promise<void> {
-    context.log('HTTP trigger function processed a request.');
-    const name = (req.query.name || (req.body && req.body.name));
-    const responseMessage = name
-        ? "Hello, " + name + ". This HTTP triggered function executed successfully."
-        : "This HTTP triggered function executed successfully. Pass a name in the query string or in the request body for a personalized response.";
-
-    context.res = {
-        // status: 200, /* Defaults to 200 */
-        body: responseMessage
-    };
-
-};
-
-// export default httpTrigger;
