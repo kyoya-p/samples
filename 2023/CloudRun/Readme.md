9abee435ffa4e02f803990628831ae63e3bfe1a1
kk <kyoya.p@gmail.com>
2023-10-05 03:56:58 +0000

    no

--- 2023/CloudRun/Readme.md
+++ 2023/CloudRun/Readme.md
@@ -14,13 +14,12 @@ npx tsc --init --rootDir src --outDir build
 ```sh
 npm i
 npx ts-node src/index.ts
-curl http://localhost:8081 
+curl http://localhost:8080
 ```
 
-# Docker
-```sh:Image作成
-sudo docker build -t wsdemo DockerBuild
-```
-```sh:実行:ホストのポート8080でlisten
-sudo docker run --name wsdemo -p 8080:8081 wsdemo
+# Docker ビルド・実行・確認
+```sh
+sudo docker build -t wsdemo DockerBuild --no-cache
+sudo docker run --name wsdemo -p 8080:8080 wsdemo
+curl http://localhost:8080
 ```
--- 2023/CloudRun/src/index.ts
+++ 2023/CloudRun/src/index.ts
@@ -1,24 +1,3 @@
-import { WebSocket, WebSocketServer } from "ws";
-
-const wss = new WebSocketServer({ port: 8080 });
-
-wss.on("connection", (ws) => {
-  console.log("connected!");
-
-  ws.on("message", (data, isBinary) => {
-    for (const client of wss.clients) {
-      if (client.readyState === WebSocket.OPEN) {
-        client.send(data, { binary: isBinary });
-      }
-    }
-  });
-
-  ws.on("close", () => console.log("closed!"));
-});
-
-console.log("start ws server port 8080.")
-
-
 import Koa from 'koa'
 const app = new Koa()
 
@@ -27,6 +6,6 @@ app.use(ctx => {
   ctx.body = 'Hello Koa'
 })
 
-app.listen(8081)
+app.listen(8080)
 
-console.log("start ws server port 8081.")
+console.log("start ws server port 8080.")
