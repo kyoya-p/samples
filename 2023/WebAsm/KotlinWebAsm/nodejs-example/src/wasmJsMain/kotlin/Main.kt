35e42b4e52760d37e4ca927d948c7a8ef2d6a73d
kk <kyoya.p@gmail.com>
2023-10-25 21:36:46 +0900

    n

--- 2023/WebAsm/KotlinWebAsm/nodejs-example/src/wasmJsMain/kotlin/Main.kt
+++ 2023/WebAsm/KotlinWebAsm/nodejs-example/src/wasmJsMain/kotlin/Main.kt
@@ -1,3 +1,10 @@
 fun main() {
-    println("Hello World!")
+    for (i in 1..1000L) {
+        print("$i:")
+        collatz(i).forEach { print("*") }
+        println()
+    }
 }
+
+fun fib() = generateSequence(1 to 1) { (a, b) -> a + b to a }
+fun collatz(x: Long) = generateSequence(x) { e -> if (e % 2 == 0L) (e / 2) else (e * 3 + 1) }.takeWhile { it != 1L }
