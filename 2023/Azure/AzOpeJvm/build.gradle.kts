9e1515dc7fa818f756307d60bae9f55ee1caf354
kk <kyoya.p@gmail.com>
2023-11-13 08:20:02 +0000

    n

--- 2023/Azure/AzOpeJvm/build.gradle.kts
+++ 2023/Azure/AzOpeJvm/build.gradle.kts
@@ -1,5 +1,6 @@
 plugins {
     kotlin("jvm") version "1.9.10"
+    kotlin("plugin.serialization") version "1.9.10"
     application
 }
 
@@ -11,13 +12,9 @@ repositories {
 }
 
 dependencies {
-// https://mvnrepository.com/artifact/com.microsoft.azure/azure-mgmt-resources
-//    implementation("com.microsoft.azure:azure-mgmt-resources:1.41.4")
+    implementation("org.jetbrains.kotlinx:kotlinx-serialization-json:1.3.3")
     implementation("com.azure:azure-identity:1.2.5")
-
-// https://mvnrepository.com/artifact/com.azure.resourcemanager/azure-resourcemanager
     implementation("com.azure.resourcemanager:azure-resourcemanager:2.31.0")
-//    implementation("com.azure.resourcemanager:azure-resourcemanager-maintenance:1.0.0")
 }
 
 kotlin {
