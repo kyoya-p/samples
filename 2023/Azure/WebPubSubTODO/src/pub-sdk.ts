bf013d090f99a1c70f5979ca8c8fd57430e93018
kk <kyoya.p@gmail.com>
2023-11-19 10:49:50 +0000

    no

--- 2023/Azure/WebPubSubTODO/src/pub-sdk.ts
+++
@@ -1,9 +0,0 @@
-import { WebPubSubServiceClient } from '@azure/web-pubsub'
-
-const hub = "hub1";
-const connStr = process.env.WebPubSubConnectionString ?? ""
-console.log(`ConnectionString: ${connStr}`)
-let service = new WebPubSubServiceClient(connStr, hub);
-
-const m = `${Date()}`
-service.sendToAll(m, { contentType: "text/plain" });
