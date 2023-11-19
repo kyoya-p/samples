2e62bf9a7ec36924eb92b54e2722a98593d79f24
kk <kyoya.p@gmail.com>
2023-11-19 13:00:29 +0000

    n

--- 2023/Azure/WebPubSub/src/sub.ts
+++ 2023/Azure/WebPubSub/src/sub.ts
@@ -1,10 +1,24 @@
+import { WebPubSubServiceClient } from "@azure/web-pubsub";
 import { WebSocket } from "ws"
 
- function main() {
-  const url=  process.env.WebPubSubUrl ?? ""
-  let ws = new WebSocket(url)
+main()
+
+async function main() {
+  if (process.argv.length !== 4) {
+    console.log('Usage: node subscribe <connection-string> <hub-name>');
+    return 1;
+  }
+
+  const connStr = process.argv[2]
+  const hub = process.argv[3]
+
+  console.log(`connection-string: ${connStr}`)
+  console.log(`hub-name: ${hub}`)
+
+  const serviceClient = new WebPubSubServiceClient(connStr, hub)
+  const token = await serviceClient.getClientAccessToken()
+  const ws = new WebSocket(token.url)
   ws.on('open', () => console.log('connected'))
-  ws.on('message', data => console.log('Message received: %s', data))
+  ws.on('message', data => console.log(data))
 }
 
-main()
