703b6ba4f789b639e49c9869ceac839dc3b5bd96
kk <kyoya.p@gmail.com>
2024-01-04 23:46:04 +0900

    no

--- 2024/MongoDBAtlas/trial1/src/index.ts
+++ 2024/MongoDBAtlas/trial1/src/index.ts
@@ -2,7 +2,13 @@ import * as Realm from "realm-web";
 
 async function main() {
 	const app = new Realm.App({ id: "application-0-yvxdl" });
-
+	const credentials = Realm.Credentials.anonymous();
+	try {
+	  const user = await app.logIn(credentials);
+	} catch(err) {
+	  console.error("Failed to log in", err);
+	}
 }
 
 main()
+
