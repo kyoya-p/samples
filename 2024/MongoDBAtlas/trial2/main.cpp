d761dba54a47f5b2271829be8195e8de2cfeac96
kk <kyoya.p@gmail.com>
2024-01-14 15:35:03 +0900

    no

--- 2024/MongoDBAtlas/trial2/main.cpp
+++ 2024/MongoDBAtlas/trial2/main.cpp
@@ -1,24 +1,15 @@
-#include <iostream>
-#include <string>
 #include <cpprealm/sdk.hpp>
+
+struct Todo
+{
+  realm::primary_key<realm::object_id> _id{realm::object_id::generate()};
+  std::string summary;
+  bool isComplete;
+  std::string owner_id;
+};
 
 namespace realm
 {
-  struct Todo1
-  {
-    realm::primary_key<realm::object_id> _id{realm::object_id::generate()};
-    std::string summary;
-    std::string isComplete;
-    std::string owner_id;
-  };
-  // REALM_SCHEMA(Todo, _id, summary, isComplete, owner_id);
-  struct Todo
-  {
-    realm::primary_key<realm::object_id> _id{realm::object_id::generate()};
-    std::string summary;
-    bool isComplete;
-    std::string owner_id;
-  };
   REALM_SCHEMA(Todo, _id, summary, isComplete, owner_id);
 }
 
@@ -34,42 +25,34 @@ int main(int argc, char *argv[])
   auto config = realm::db_config();
   auto realm = realm::db(std::move(config));
 
-  auto todo = realm::Todo{
-      .summary = "Create my first todo item",
-      .isComplete = false,
-  };
-
   // DB上にドキュメント作成
   realm.write(
       [&]
       {
-        realm.add(std::move(realm::Todo{
+        realm.add(std::move(Todo{
             .summary = "Initial Document.",
             .isComplete = false,
         }));
       });
 
   // DBからドキュメントを探す
-  auto collection = realm.objects<realm::Todo>();
+  auto collection = realm.objects<Todo>();
   auto imcompletedItems = collection.where(
       [](auto &item)
       { return item.isComplete == false; });
-
   auto item = imcompletedItems[0];
   printItem(item);
 
-  // アップデート
+  // ドキュメントをアップデートしDBを更新
   realm.write([&]
               { 
                 item.isComplete = true; 
-                item.summary = "Updated Document.";
-                });
+                item.summary = "Updated Document."; });
 
   // 改めてDBからドキュメントを探す
   auto completedItems = collection.where(
       [](auto &item)
       { return item.isComplete == true; });
-
   printItem(completedItems[0]);
 
   realm.close();
