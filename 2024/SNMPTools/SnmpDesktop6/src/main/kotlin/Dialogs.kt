81d3dcc5c2738463ba92b59880112c6a9067b254
kk <kyoya.p@gmail.com>
2024-02-05 08:29:08 +0900

    n

--- 2024/SNMPTools/SnmpDesktop6/src/main/kotlin/Dialogs.kt
+++ 2024/SNMPTools/SnmpDesktop6/src/main/kotlin/Dialogs.kt
@@ -1,7 +1,7 @@
-import androidx.compose.desktop.ui.tooling.preview.Preview
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.width
 import androidx.compose.material.Button
 import androidx.compose.material.Text
 import androidx.compose.material.TextField
@@ -11,21 +11,31 @@ import androidx.compose.ui.unit.dp
 import androidx.compose.ui.window.Dialog
 
 
-@Preview
-@Composable
 fun snmpCaptureDialog(ipSpec0: String, onOk: (ipSpec: String) -> Unit) = DialogHandler { dialog ->
     Dialog(title = "SNMP Access Information", onCloseRequest = { dialog.close() }) {
+        val styleField = Modifier.width(280.dp)
         val styleBtn = Modifier.padding(8.dp)
         var ipSpec by remember { mutableStateOf(ipSpec0) }
         var comm by remember { mutableStateOf(app.commStr ?: "public") }
         Column(modifier = Modifier.padding(8.dp)) {
-            TextField(ipSpec, label = { Text("IP") }, onValueChange = { ipSpec = it;app.ip = it })
-            TextField(comm, label = { Text("Community String") }, onValueChange = { comm = it;app.commStr = it })
+            Row {
+                TextField(ipSpec, label = { Text("IP") }, modifier = styleField,
+                    onValueChange = { ipSpec = it;app.ip = it })
+                val scan = snmpScanDialog {close()}.apply { placing() }
+                Button(modifier = styleBtn, onClick = { scan.open() }) { Text("Scan") }
+            }
+            TextField(comm, label = { Text("Community String") }, modifier = styleField,
+                onValueChange = { comm = it;app.commStr = it })
             Row {
                 Button(modifier = styleBtn, onClick = { onOk(ipSpec); dialog.close() }) { Text("Capture") }
                 Button(modifier = styleBtn, onClick = { dialog.close() }) { Text("Cancel") }
             }
         }
+    }
+}
+
+fun snmpScanDialog(onOk: DialogHandler.() -> Unit) = DialogHandler { dialog ->
+    Dialog(title = "SNMP Access Information", onCloseRequest = { dialog.close() }) {
     }
 }
 
