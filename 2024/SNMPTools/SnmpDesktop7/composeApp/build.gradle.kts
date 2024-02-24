f9fb992bbfd9f8e6ababf3362fb23fb6c5cbae5e
kyoya <kyoya.p@gmail.com>
2024-02-24 20:51:48 +0900

    n

---
+++ 2024/SNMPTools/SnmpDesktop7/composeApp/build.gradle.kts
@@ -0,0 +1,41 @@
+import org.jetbrains.compose.ExperimentalComposeLibrary
+import org.jetbrains.compose.desktop.application.dsl.TargetFormat
+
+plugins {
+    alias(libs.plugins.kotlinMultiplatform)
+    
+    alias(libs.plugins.jetbrainsCompose)
+}
+
+kotlin {
+    jvm("desktop")
+    
+    sourceSets {
+        val desktopMain by getting
+        
+        commonMain.dependencies {
+            implementation(compose.runtime)
+            implementation(compose.foundation)
+            implementation(compose.material)
+            implementation(compose.ui)
+            @OptIn(ExperimentalComposeLibrary::class)
+            implementation(compose.components.resources)
+        }
+        desktopMain.dependencies {
+            implementation(compose.desktop.currentOs)
+        }
+    }
+}
+
+
+compose.desktop {
+    application {
+        mainClass = "MainKt"
+
+        nativeDistributions {
+            targetFormats(TargetFormat.Dmg, TargetFormat.Msi, TargetFormat.Deb)
+            packageName = "org.example.project"
+            packageVersion = "1.0.0"
+        }
+    }
+}
---
+++ 2024/SNMPTools/imageviewer/shared/src/commonMain/kotlin/example/imageviewer/SnmpDesktop.common.kt
@@ -0,0 +1,13 @@
+package example.imageviewer
+
+import androidx.compose.material.Scaffold
+import androidx.compose.material.Text
+import androidx.compose.material.TopAppBar
+import androidx.compose.runtime.Composable
+
+@Composable
+fun snmpDesktop() = Scaffold(
+    topBar = { TopAppBar(title = { Text("Sample") }) },
+
+    ) {
+}
