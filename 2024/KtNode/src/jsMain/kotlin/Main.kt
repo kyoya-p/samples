25d73fde48d77db5a04fb9b44915bafdd8c8fce8
kyoya <kyoya.p@gmail.com>
2024-04-16 08:27:52 +0900

    n

---
+++ 2024/KtNode/src/jsMain/kotlin/Main.kt
@@ -0,0 +1,75 @@
+import kotlinx.coroutines.await
+import kotlin.js.Promise
+
+external fun require(s: String): dynamic
+
+fun hello(req: dynamic, res: dynamic) {
+    res.send("Hello, world!")
+}
+
+fun mainX(args: Array<String>) {
+    val express = require("express")
+    val app = express()
+    app.get("/", ::runBrowser)
+    app.listen(8080) { println("Start Server.") }
+}
+
+fun runBrowser(req: dynamic, res: dynamic) {
+    val playwright = require("playwright")
+    val browser = playwright.chromium.launch()
+    val page = browser.newPage()
+    page.goto("https://digital.onl.jp/")
+    val screenshot = page.screenshot()
+    res.setHeader("Content-Type", "image/png")
+    res.send(screenshot)
+    browser.close()
+}
+
+external interface Buffer
+
+@JsModule("playwright")
+external interface Playwright {
+    interface Driver {
+        fun launch(): Promise<Browser>
+    }
+
+    interface Browser {
+        fun newPage(): Promise<Page>
+        fun close(): Promise<Unit>
+    }
+
+    interface Page {
+        fun goto(url: String): Promise<Unit>
+        fun screenshot(): Promise<Buffer>
+    }
+
+    val chromium: Driver
+}
+
+suspend fun main() {
+    val pw: Playwright = require("playwright")
+    val browser = pw.chromium.launch().await()
+    val page = browser.newPage().await()
+    page.goto("https://digital.onl.jp/").await()
+    val img =page.screenshot().await()
+    println(img)
+    browser.close().await()
+}
+
+//fun mainX(args: Array<String>) {
+//    val browser = playwright.chromium.launch()
+//    val page = browser.newPage()
+//    page.goto("https://www.google.com")
+//    page.screenshot(path = "screenshot.png")
+//    browser.close()
+//    fun main() {
+//        val name = "Kotlin"
+//        println("Hello, " + name + "!")
+//
+//        for (i in 1..5) {
+//            println("i = $i")
+//        }
+//
+//    }
+//
+//}
