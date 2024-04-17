bf5d20d71d8547883217269ef5167c1e7a045e70
kyoya <kyoya.p@gmail.com>
2024-04-17 20:42:01 +0900

    n

--- 2024/Playwright.KtJvm/build.gradle.kts
+++ 2024/Playwright.KtJvm/build.gradle.kts
@@ -1,9 +1,16 @@
 plugins {
     kotlin("jvm") version "1.9.23"
+//    kotlin("multiplatform") version "1.9.23"
+    application
+    id("com.github.johnrengelman.shadow") version "8.1.1" // https://plugins.gradle.org/plugin/com.github.johnrengelman.shadow
 }
 repositories { mavenCentral() }
 dependencies {
     implementation("com.microsoft.playwright:playwright:1.43.0")  // https://mvnrepository.com/artifact/com.microsoft.playwright/playwright
-    testImplementation(kotlin("test"))
+//            testImplementation(kotlin("test"))
+}
+
+application {
+    mainClass.set("MainKt")
 }
 
--- 2024/Playwright.KtJvm/src/main/kotlin/Main.kt
+++ 2024/Playwright.KtJvm/src/main/kotlin/Main.kt
@@ -1,38 +1,28 @@
-
 import com.microsoft.playwright.*
 import com.microsoft.playwright.assertions.PlaywrightAssertions.assertThat
 import com.microsoft.playwright.options.AriaRole
 import java.util.regex.Pattern
 
 
-object App {
-    @JvmStatic
-    fun main(args: Array<String>) {
-        Playwright.create().use { playwright ->
-            val browser = playwright.chromium().launch()
-            val page = browser.newPage()
-            page.navigate("http://playwright.dev")
+fun main(args: Array<String>) {
+    Playwright.create().use { playwright ->
+        val browser = playwright.chromium().launch(BrowserType.LaunchOptions().apply { headless = false })
+        val page = browser.newPage()
+        page.navigate("http://playwright.dev")
 
-            // Expect a title "to contain" a substring.
-            assertThat(page).hasTitle(Pattern.compile("Playwright"))
+        assertThat(page).hasTitle(Pattern.compile("Playwright"))
 
-            // create a locator
-            val getStarted = page.getByRole(AriaRole.LINK, Page.GetByRoleOptions().setName("Get Started"))
+        val getStarted = page.getByRole(AriaRole.LINK, Page.GetByRoleOptions().setName("Get Started"))
 
-            // Expect an attribute "to be strictly equal" to the value.
-            assertThat(getStarted).hasAttribute("href", "/docs/intro")
+        assertThat(getStarted).hasAttribute("href", "/docs/intro")
 
-            // Click the get started link.
-            getStarted.click()
+        getStarted.click()
 
-            // Expects page to have a heading with the name of Installation.
-            assertThat(
-                page.getByRole(
-                    AriaRole.HEADING,
-                    Page.GetByRoleOptions().setName("Installation")
-                )
-            ).isVisible()
-        }
+        assertThat(
+            page.getByRole(
+                AriaRole.HEADING,
+                Page.GetByRoleOptions().setName("Installation")
+            )
+        ).isVisible()
     }
 }
-
