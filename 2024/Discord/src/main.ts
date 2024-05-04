9a7c1338629bc3b93c56c91bfde3f17234f0785e
kk <kyoya.p@gmail.com>
2024-05-04 18:46:28 +0900

    no

---
+++ 2024/Discord/src/main.ts
@@ -0,0 +1,19 @@
+const { requestGPIOAccess } = require("node-web-gpio");
+const { promisify } = require("util");
+const sleep = promisify(setTimeout);
+
+async function main() {
+  const gpioAccess = await requestGPIOAccess();
+  const port = gpioAccess.ports.get(26);
+
+  await port.export("out");
+
+  for (;;) {
+    await port.write(1);
+    await sleep(1000);
+    await port.write(0);
+    await sleep(1000);
+  }
+}
+
+main();
