048ec1b8e3f30f320da36b9ff6277345fa2f19ab
kk <kyoya.p@gmail.com>
2024-05-04 11:38:19 +0000

    no

--- 2024/Discord/main.js
+++ 2024/Discord/main.js
@@ -10,8 +10,10 @@ async function main() {
 
   for (;;) {
     await port.write(1);
+      console.log("26=1");
     await sleep(1000);
     await port.write(0);
+      console.log("26=0");
     await sleep(1000);
   }
 }
