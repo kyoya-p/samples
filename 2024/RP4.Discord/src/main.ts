1e35f7e526353670e43f559e480a62adcc006bf3
kk <kyoya.p@gmail.com>
2024-05-05 18:13:57 +0000

    no

--- 2024/RP4.Discord/src/main.ts
+++ 2024/RP4.Discord/src/main.ts
@@ -1,5 +1,6 @@
-import {DiscordSDK} from '@discord/embedded-app-sdk';
-const DISCORD_CLIENT_ID = process.env["DISCORD_CLIENT_ID"] !
+import { DiscordSDK } from '@discord/embedded-app-sdk';
+const DISCORD_CLIENT_ID = process.env["DISCORD_CLIENT_ID"]!
+const DISCORD_CLIENT_SECERT = process.env["DISCORD_CLIENT_SECRET"]!
 const discordSdk = new DiscordSDK(DISCORD_CLIENT_ID);
 
 async function setup() {
@@ -11,7 +12,7 @@ async function setup() {
   console.log(`L2`);
 
   // Pop open the OAuth permission modal and request for access to scopes listed in scope array below
-  const {code} = await discordSdk.commands.authorize({
+  const { code } = await discordSdk.commands.authorize({
     client_id: DISCORD_CLIENT_ID,
     response_type: 'code',
     state: '',
@@ -33,7 +34,7 @@ async function setup() {
 
   console.log(`L4`);
 
-  const {access_token} = await response.json();
+  const { access_token } = await response.json();
 
   // Authenticate with Discord client (using the access_token)
   const auth = await discordSdk.commands.authenticate({
