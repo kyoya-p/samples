6ade338331e71fc18df015f470c04c75b5ac0255
kyoya <kyoya.p@gmail.com>
2024-06-11 11:51:20 +0900

    n

--- 2024/Azure/cosmosdbJava/src/main/kotlin/Main.kt
+++ 2024/Azure/cosmosdbJava/src/main/kotlin/Main.kt
@@ -3,22 +3,20 @@ import com.mongodb.MongoClientURI
 import com.mongodb.client.model.Filters
 import kotlinx.coroutines.channels.awaitClose
 import kotlinx.coroutines.channels.trySendBlocking
-import kotlinx.coroutines.flow.callbackFlow
-import kotlinx.coroutines.flow.collectIndexed
+import kotlinx.coroutines.flow.*
+
 
 suspend fun main(args: Array<String>) = callbackFlow {
     val connStr = System.getenv("CONNSTR")!!
     val (db, collection) = args
-    val filters = args.drop(3).map { it.split("=") }.map { (f, v) -> Filters.eq(f, v) }
+    val filters = args.drop(2).map { it.split("=") }.map { (f, v) -> Filters.eq(f, v) }
     MongoClient(MongoClientURI(connStr)).use { client ->
         client.getDatabase(db).getCollection(collection).apply {
-            when (filters.isEmpty()) {
-                true -> find()
-                else -> find(Filters.and(filters))
-            }.forEach { doc -> trySendBlocking(doc!!) }
+            val query = if (filters.isEmpty()) find() else find(Filters.and(filters))
+            query.forEach { doc -> trySendBlocking(doc!!) }
         }
         close()
     }
     awaitClose()
-}.collectIndexed { i, it -> println("$i: ${it.toJson().take(128)}") }
+}.withIndex().collect { (i, doc) -> println("$i: ${doc.toJson().take(128)}") }
 
