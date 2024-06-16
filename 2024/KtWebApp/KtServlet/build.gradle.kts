df2e69d6bf4483bb51874f197eb528706b7145b7
kyoya.p <kyoya.p@gmail.com>
2024-06-17 06:59:07 +0900

    n

--- 2024/KtWebApp/KtServlet/build.gradle.kts
+++ 2024/KtWebApp/KtServlet/build.gradle.kts
@@ -5,13 +5,14 @@ val logback_version: String by project
 plugins {
     kotlin("jvm") version "2.0.0"
     id("io.ktor.plugin") version "2.3.11"
+    id("org.jetbrains.kotlin.plugin.serialization") version "2.0.0"
 }
 
 group = "com.example"
 version = "0.0.1"
 
 application {
-    mainClass.set("io.ktor.server.tomcat.EngineMain")
+    mainClass.set("com.example.ApplicationKt")
 
     val isDevelopment: Boolean = project.ext.has("development")
     applicationDefaultJvmArgs = listOf("-Dio.ktor.development=$isDevelopment")
@@ -22,11 +23,12 @@ repositories {
 }
 
 dependencies {
-    implementation("io.ktor:ktor-server-cors-jvm")
+    implementation("io.ktor:ktor-server-content-negotiation-jvm")
     implementation("io.ktor:ktor-server-core-jvm")
+    implementation("io.ktor:ktor-serialization-kotlinx-json-jvm")
+    implementation("io.ktor:ktor-server-cors-jvm")
     implementation("io.ktor:ktor-server-tomcat-jvm")
     implementation("ch.qos.logback:logback-classic:$logback_version")
-    implementation("io.ktor:ktor-server-config-yaml")
     testImplementation("io.ktor:ktor-server-tests-jvm")
     testImplementation("org.jetbrains.kotlin:kotlin-test-junit:$kotlin_version")
 }
