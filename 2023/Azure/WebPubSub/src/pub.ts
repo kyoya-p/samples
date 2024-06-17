46cf1c9a0f1370ff16ad86f908d8b6955f9fb849
kk <kyoya.p@gmail.com>
2024-06-17 16:07:36 +0900

    n

--- 2023/Azure/WebPubSub/src/pub.ts
+++ 2023/Azure/WebPubSub/src/pub.ts
@@ -6,13 +6,16 @@ async function main() {
   const connStr = process.argv[2]
   const hub = process.argv[3] ?? "default"
 
-  let serviceClient = new WebPubSubServiceClient(connStr, hub);
+  let serviceClient = new WebPubSubServiceClient(connStr, hub,);  
 
-  for (let i = 0; i < 1000; ++i) {
-    serviceClient.sendToAll(`${i}`, { contentType: "text/plain" });
-    console.log(i)
-    await sleep1s()
+  for (let i = 0; i < 100; ++i) {
+    for (let j = 0; j < 10; ++j) {
+      let msg = `${i}-${j}`
+      serviceClient.sendToAll(msg, { contentType: "text/plain" });
+      console.log(msg)
+    }
+    await sleep(5000)
   }
 }
 
-async function sleep1s() { await new Promise(resolve => setTimeout(resolve, 1000)) }
+async function sleep(t: number) { await new Promise(resolve => setTimeout(resolve, t)) }
