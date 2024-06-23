780347a9b609d7ef084d2427b388520a555dc028
kyoya.p <kyoya.p@gmail.com>
2024-06-24 08:19:30 +0900

    n

--- 2024/KtFirestore/build.gradle.kts
+++ 2024/KtFirestore/build.gradle.kts
@@ -1,8 +1,12 @@
 plugins {
     kotlin("multiplatform") version "2.0.0"
+
+    kotlin("plugin.serialization") version "2.0.0"
 }
 
 repositories {
+//    mavenLocal()
+    google()
     mavenCentral()
 }
 
@@ -10,5 +14,13 @@ kotlin {
     js {
         browser {}
         binaries.executable()
+    }
+    sourceSets {
+        jsMain.dependencies {
+//            implementation(npm("@gitliveapp/firebase-firestore","1.5.19-beta"))
+            implementation("dev.gitlive:firebase-common:1.13.0")
+            implementation("dev.gitlive:firebase-auth:1.13.0")
+            implementation("dev.gitlive:firebase-firestore:1.13.0")
+        }
     }
 }
