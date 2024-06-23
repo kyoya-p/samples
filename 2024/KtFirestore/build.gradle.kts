265cfa9fa19cf6aacb1fd7d9f3c7520f5e813dbd
kyoya.p <kyoya.p@gmail.com>
2024-06-23 17:05:58 +0900

    n

--- 2024/KtFirestore/build.gradle.kts
+++ 2024/KtFirestore/build.gradle.kts
@@ -1,7 +1,14 @@
 plugins {
-    // this is necessary to avoid the plugins to be loaded multiple times
-    // in each subproject's classloader
-    alias(libs.plugins.jetbrainsCompose) apply false
-    alias(libs.plugins.compose.compiler) apply false
-    alias(libs.plugins.kotlinMultiplatform) apply false
+    kotlin("multiplatform") version "2.0.0"
+}
+
+repositories {
+    mavenCentral()
+}
+
+kotlin {
+    js {
+        browser {}
+        binaries.executable()
+    }
 }
