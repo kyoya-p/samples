90aec09661d280081f7cc7c742af8a2da802e2db
kyoya.p <kyoya.p@gmail.com>
2024-06-25 08:33:07 +0900

    n

--- 2024/KtFirestore/src/jsMain/kotlin/Main.kt
+++ 2024/KtFirestore/src/jsMain/kotlin/Main.kt
@@ -2,19 +2,20 @@ import dev.gitlive.firebase.*
 import dev.gitlive.firebase.externals.initializeApp
 import dev.gitlive.firebase.firestore.firestore
 
+val options = FirebaseOptions(
+    apiKey = "AIzaSyCiiIwgR3-hqUrIeCCdmudOr2nKwmviSyU",
+    projectId = "road-to-iot",
+    databaseUrl = "https://road-to-iot.firebaseio.com",
+    applicationId = "1:307495712434:web:e826b0016881e1b5f33bab",
+)
+
+val firebase = initializeApp(options)
+
 suspend fun main() {
 
-    val options = FirebaseOptions(
-        apiKey = "AIzaSyCiiIwgR3-hqUrIeCCdmudOr2nKwmviSyU",
-//        projectId = "road-to-iot",
-        databaseUrl = "https://road-to-iot.firebaseio.com",
-        applicationId = "1:307495712434:web:e826b0016881e1b5f33bab",
-    )
 
 
-    val firebase = initializeApp(options)
-
-    val db=Firebase.firestore
+    val db = Firebase.firestore
     db.document("xxx").set("{xxx:xxx}")
 
 
