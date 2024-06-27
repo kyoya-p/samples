a9721e6bea7f89833616b7ef5c7a72d78a72c6d1
kyoya.p <kyoya.p@gmail.com>
2024-06-28 08:34:00 +0900

    n

--- 2024/KtFirestore/src/jsMain/kotlin/Main.kt
+++ 2024/KtFirestore/src/jsMain/kotlin/Main.kt
@@ -1,88 +1,48 @@
 import dev.gitlive.firebase.*
 import dev.gitlive.firebase.firestore.*
 import kotlinx.browser.document
-import kotlinx.browser.window
 import kotlinx.coroutines.*
+import kotlinx.dom.addClass
 import kotlinx.html.*
 import kotlinx.html.dom.append
 import kotlinx.html.dom.create
 import kotlinx.html.js.button
 import kotlinx.html.js.input
 import kotlinx.html.js.table
-import kotlinx.serialization.Serializable
+import kotlinx.html.js.thead
 
 
 val options = FirebaseOptions(
-    apiKey = "AIzaSyCiiIwgR3-hqUrIeCCdmudOr2nKwmviSyU\n",
-    projectId = "road-to-iot",
+    apiKey = "xxxxxxxxxxxxxxxxx-xxxxxxxxxxxxxxxxx",
+    projectId = "xxxxxx",
     databaseUrl = "https://road-to-iot.firebaseio.com",
-    applicationId = "1:307495712434:web:e826b0016881e1b5f33bab",
+    applicationId = "1:xxxxxxxxxxx:web:xxxxxxxxxxxxxxxxxxxx",
 )
-
-lateinit var firestore: FirebaseFirestore
-
 
 @OptIn(DelicateCoroutinesApi::class)
 suspend fun main() {
-    val app = Firebase.apps(Unit).firstOrNull() ?: Firebase.initialize(Unit, options)
-    val db = Firebase.firestore(app).apply {
-        settings = firestoreSettings(settings) {
-            cacheSettings = memoryCacheSettings {}
-//        cacheSettings = persistentCacheSettings {}
-        }
-    }
-    val name = document.create.input(name = "name")
-    val mail = document.create.input(name = "mail")
-    val book = document.create.table()
+    val app = Firebase.initialize(Unit, options)
+    val db = Firebase.firestore(app)
+    db.settings = firestoreSettings(db.settings) { cacheSettings = persistentCacheSettings { } }
 
-    println("Start!!")
+    fun addItem(item: String) = GlobalScope.launch { db.collection("tmp").add(mapOf("action" to item)) }
+    fun delItem(id: String) = GlobalScope.launch { db.collection("tmp").document(id).delete() }
+
+    val action = document.create.input(name = "name")
+    val book = document.create.table().apply { addClass("table") }
     val body = document.body ?: error("body is null")
-
-    body.append {
-        div { +" Name:" }.append(name)
-        div { +" Mail:" }.append(mail)
-        button { +"Add" }.onclick = {
-            GlobalScope.launch {
-                db.collection("tmp").add(mapOf("name" to name.value, "mail" to mail.value))
-            }
+    body.append(book)
+    db.collection("tmp").snapshots.collect { qs ->
+        book.innerHTML = ""
+        book.tHead = document.create.thead { tr { td {};td { +"To do List" } } }
+        book.insertRow().apply {
+            insertCell().append { button { +"Add" }.onclick = { addItem(action.value) } }
+            insertCell().append(action)
         }
-        div {}.append(book)
-    }
-
-
-    db.collection("tmp").snapshots.collect {
-        it.documents.forEachIndexed { i, ds ->
+        qs.documents.forEach { ds ->
             book.insertRow().apply {
-                insertCell().append {
-                    button { +"Del" }.onclick = { GlobalScope.launch { db.document(ds.id).delete() } }
-                }
-                insertCell().apply { textContent = ds.id }
-                insertCell().apply { textContent = ds.get<String>("name") }
-                insertCell().apply { textContent = ds.get<String>("mail") }
-            }
-        }
-    }
-    val timeP = document.create.p { +"Time: 0" }
-    body.append(timeP)
-    var time = 0
-    window.setInterval({
-        time++
-        timeP.textContent = "Time: $time"
-        return@setInterval null
-    }, 1000)
-}
-
-fun initializeFirebase(persistenceEnabled: Boolean = false) {
-
-    val app = Firebase.apps(Unit).firstOrNull() ?: Firebase.initialize(Unit, options)
-
-    firestore = Firebase.firestore(app).apply {
-//        useEmulator(emulatorHost, 8080)
-        settings = firestoreSettings(settings) {
-            cacheSettings = if (persistenceEnabled) {
-                persistentCacheSettings { }
-            } else {
-                memoryCacheSettings { }
+                insertCell().append { button { +"Del" }.onclick = { delItem(ds.id) } }
+                insertCell().apply { textContent = ds.get<String?>("action") ?: "[No Action]" }
             }
         }
     }
