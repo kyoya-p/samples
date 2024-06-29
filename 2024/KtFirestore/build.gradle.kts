e712ecb395e8eaf2b07a86d0a33db22ff30a499d
kyoya.p <kyoya.p@gmail.com>
2024-06-29 16:42:35 +0900

    n

--- 2024/KtFirestore/build.gradle.kts
+++ 2024/KtFirestore/build.gradle.kts
@@ -1,3 +1,5 @@
+import org.apache.tools.ant.filters.ReplaceTokens
+
 plugins {
     kotlin("multiplatform") version "2.0.0"
 //    kotlin("plugin.serialization") version "2.0.0"
@@ -19,5 +21,14 @@ kotlin {
             implementation("dev.gitlive:firebase-firestore:1.13.0") // https://mvnrepository.com/artifact/dev.gitlive/firebase-firestore
             implementation("org.jetbrains.kotlinx:kotlinx-html:0.11.0") // https://mvnrepository.com/artifact/org.jetbrains.kotlinx/kotlinx-html
         }
+    }
+}
+
+tasks["build"].doFirst {
+    copy {
+        from("src/jsMain/kotlin/Properties.kt.tmpl")
+        into("src/jsMain/kotlin/")
+        rename { "Properties.kt" }
+        filter { it.replace("$[APPKEY]", System.getenv("APPKEY") ?: "'export APPKEY=...'") }
     }
 }
