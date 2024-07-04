6e342c3b8eaaef65721accafe9c8cbb3b466b820
kyoya.p <kyoya.p@gmail.com>
2024-07-05 08:34:06 +0900

    n

--- 2024/KtMPs/KtMpApp/KtNodeSvr/src/jsMain/kotlin/index.kt
+++ 2024/KtMPs/KtMpApp/KtNodeSvr/src/jsMain/kotlin/index.kt
@@ -2,57 +2,74 @@ import dev.gitlive.firebase.Firebase
 import dev.gitlive.firebase.FirebaseOptions
 import dev.gitlive.firebase.firestore.*
 import dev.gitlive.firebase.initialize
+import kotlin.coroutines.resume
+import kotlin.coroutines.resumeWithException
+import kotlin.coroutines.suspendCoroutine
+import kotlin.js.Promise
 
 external val process: dynamic
 
+val options = FirebaseOptions(
+    apiKey = process.env.APPKEY as String,
+    projectId = "road-to-iot",
+    databaseUrl = "https://road-to-iot.firebaseio.com",
+    applicationId = "1:307495712434:web:638b142c284daabef33bab",
+)
+
+val app = Firebase.initialize(Unit, options)
+val db = Firebase.firestore(app).apply {
+    settings = firestoreSettings(settings) { cacheSettings = memoryCacheSettings { } }
+}
+
 suspend fun main() {
     val args = (process.argv as Array<String>).drop(2)
-    val appKey = process.env.APPKEY as String
     val (tg, pw) = args
-
-    println("appKey=$appKey tg=$tg")
-
-    val options = FirebaseOptions(
-        apiKey = appKey,
-        projectId = "road-to-iot",
-        databaseUrl = "https://road-to-iot.firebaseio.com",
-        applicationId = "1:307495712434:web:638b142c284daabef33bab",
-    )
-
-    val app = Firebase.initialize(Unit, options)
-    val db = Firebase.firestore(app)
-    db.settings = firestoreSettings(db.settings) { cacheSettings = memoryCacheSettings { } }
-
     val tgRef = db.collection("fireshell").document(tg)
-    val d = tgRef.get().data<Map<String, String>>()
-    println(d)
-
-//    tgRef.collection("requests").add(mapOf("time" to now()))
-//    val d = tgRef.collection("requests").limit(1).snapshots.first().documents[0].data<Map<String, String>>()
-//    println(d)
 
     tgRef.collection("requests")
         .orderBy("time", Direction.ASCENDING)
         .where { "state" equalTo "" }
         .limit(10)
         .snapshots.collect {
-            it.documents.forEach { execCommand(it.data<Map<String, String>>()) }
+            it.documents.forEach { execCommand(it) }
         }
+}
+
+
+suspend fun execCommand(req: DocumentSnapshot) {
+    val data = req.data<Map<String, String>>()
+    println(data)
+    val r0 = execExecia<dynamic>(data["command"]!!) //.await()
+    println("X1")
+    val r = r0.await()
+//    val mData = data.toMutableMap()
+    println(r.stdout)
+//    println(r.exitCode)
+
+//    mData.set("exitCode", r.exitCode)
+//    mData.set("stdout", r.stdout)
+//    req.reference.set(mData)
+}
+
+// Javascript inter-op
+
+suspend fun <T> Promise<T>.await(): T = suspendCoroutine { cont ->
+    then({ println("L1");cont.resume(it) })
+    catch { println("L2"); cont.resumeWithException(it) }
 }
 
 external fun require(module: String): dynamic
 
-val childProcess = require("child_process")
-val execa = require("execa")
+@Suppress("UNUSED_VARIABLE")
+fun <T> execExecia(run: String): Promise<T> {
+    val execa = require("execa")
 
-suspend fun execCommand(data: Map<String, String>) {
-    val cmd = data["command"] ?: ""
-    val args = (data["command"] ?: "").split(" ").toTypedArray()
+    val (cmd, args) = run.split(" ").let { it.first() to it.drop(1).toTypedArray() }
     println(cmd)
-//    val r = childProcess.spawn(cmd)
-    val r = execa.execa(cmd).stdout
-    println(r)
-    val mData = data.toMutableMap()
-    mData["state"] = "x"
-
+    println(args)
+    println("E0")
+//    val res = js("execa(cmd,args);")
+    val res = execa(cmd, args)
+    println("E1")
+    return res
 }
