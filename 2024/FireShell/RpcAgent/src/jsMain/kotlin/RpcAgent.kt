41f6d658359d79f76796daaaead6ed99eaca34fc
kk <kyoya.p@gmail.com>
2024-08-08 22:33:08 +0000

    no

--- 2024/FireShell/RpcAgent/src/jsMain/kotlin/RpcAgent.kt
+++ 2024/FireShell/RpcAgent/src/jsMain/kotlin/RpcAgent.kt
@@ -5,19 +5,6 @@ import kotlin.coroutines.suspendCoroutine
 
 
 external val process: dynamic
-
-//val options = FirebaseOptions(
-//    apiKey = "AIzaSyBg5ssUSPQlEKxZ6zoBrg-hwhoMzwWLQPQ",
-//    projectId = "riot-7a79a",
-//    applicationId = "1:749774078339:web:9d60dff9671ab8e9ad76b6",
-//)
-//
-//val app = Firebase.initialize(Unit, options)
-//val auth = Firebase.auth(app)
-//val db = Firebase.firestore(app)
-//    .apply { settings = firestoreSettings(settings) { cacheSettings = memoryCacheSettings { } } }
-
-
 external fun require(module: String): dynamic
 
 suspend fun main() = runCatching {
