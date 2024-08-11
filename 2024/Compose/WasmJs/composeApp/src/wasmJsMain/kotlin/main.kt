f19f386526e1bf7e1976e63c443afdb40994f8ec
kyoya.p <kyoya.p@gmail.com>
2024-08-11 12:11:36 +0900

    n

---
+++ 2024/Compose/WasmJs/composeApp/src/wasmJsMain/kotlin/main.kt
@@ -0,0 +1,12 @@
+import androidx.compose.material.Button
+import androidx.compose.material.Text
+import androidx.compose.ui.ExperimentalComposeUiApi
+import androidx.compose.ui.window.ComposeViewport
+import kotlinx.browser.document
+
+external fun dialog()
+
+@OptIn(ExperimentalComposeUiApi::class)
+fun main() = ComposeViewport(document.body!!) {
+    Button(onClick = { dialog() }) { Text("alert()") }
+}
