9b76b2169f8cc82ba26e730a5ac6356edfe1cbb8
kyoya.p <kyoya.p@gmail.com>
2024-09-19 08:23:11 +0900

    n

--- 2024/Gemini/GemCode/build.gradle.kts
+++ 2024/Gemini/GemCode/build.gradle.kts
@@ -11,8 +11,11 @@ repositories {
 }
 
 dependencies {
+    implementation("io.ktor:ktor-client-core:2.3.12")
     implementation("io.ktor:ktor-client-cio:2.3.12")
     implementation("io.ktor:ktor-client-json:2.3.12")
+    implementation("io.ktor:ktor-client-content-negotiation:2.3.12")
+
     implementation("org.jetbrains.kotlinx:kotlinx-serialization-json:1.5.1")
 
     testImplementation(kotlin("test"))
--- 2024/Gemini/GemCode/src/main/kotlin/Main.kt
+++ 2024/Gemini/GemCode/src/main/kotlin/Main.kt
@@ -1,69 +1,76 @@
 package org.example
 
 import io.ktor.client.*
-import io.ktor.client.call.*
 import io.ktor.client.engine.cio.*
+import io.ktor.client.plugins.contentnegotiation.*
 import io.ktor.client.request.*
+import io.ktor.client.statement.*
 import io.ktor.http.*
-import io.ktor.http.ContentType.Application.Json
 import kotlinx.serialization.Serializable
 
 suspend fun main() {
-    val apiKey = System.getenv("GOOGLE_API_KEY")
-    gemini(apiKey, "fun fib():Int{} 完成させて").candidates.forEach {
-        it.content.parts.forEach {
-            println(it.text)
-        }
-    }
+    val apiKey = System.getenv("GOOGLE_API_KEY") ?: throw Exception("Not Found Env.Var. GOOGLE_API_KEY")
+    gemini(apiKey, "fun fib():Int{} 完成させて")
+//        .candidates.forEach {
+//        it.content.parts.forEach {
+//            println(it.text)
+//        }
+//    }
+
+        .let { println(it) }
 }
 
 
 const val GEMINI_1_5_FLUSH =
     "https://generativelanguage.googleapis.com/v1beta/models/gemini-1.5-flash-latest:generateContent"
 
-suspend fun gemini(apiKey: String, directives: String): GeminiResponse {
-    val client = HttpClient(CIO)
-    val response = client.post("$GEMINI_1_5_FLUSH?key=$apiKey") {
-        contentType(Json)
-        setBody(GeminiRequest(listOf(Content(listOf(Part(directives))))))
+suspend fun gemini(apiKey: String, directives: String): String {
+    val client = HttpClient(CIO){
+        install(ContentNegotiation){
+            json()
+        }
     }
-    return response.body<GeminiResponse>()
+    val response = client.post("$GEMINI_1_5_FLUSH?key=$apiKey") {
+        contentType(ContentType.Application.Json)
+        setBody(GeminiRequest(listOf(Req_Content(listOf(Req_Part(directives))))))
+    }
+    return response.bodyAsText()
 }
 
 @Serializable
-data class GeminiRequest(val contents: List<Content>)
+data class GeminiRequest(val contents: List<Req_Content>)
 
 @Serializable
-data class Content(val parts: List<Part>)
+data class Req_Content(val parts: List<Req_Part>)
 
 @Serializable
-data class Part(val text: String)
+data class Req_Part(val text: String)
 
 
 @Serializable
 data class GeminiResponse(
-    val candidates: List<Candidate>,
+    val candidates: List<Res_Candidate>,
     val usageMetadata: UsageMetadata
 )
 
 @Serializable
-data class Candidate(
-    val content: Content,
+data class Res_Candidate(
+    val content: Res_Content,
     val finishReason: String,
     val index: Int,
     val safetyRatings: List<SafetyRating>
 )
-//
-//@Serializable
-//data class Content(
-//    val parts: List<Part>,
-//    val role: String
-//)
-//
-//@Serializable
-//data class Part(
-//    val text: String
-//)
+
+@Serializable
+data class Res_Content(
+    val parts: List<Res_Part>,
+    val role: String
+)
+
+@Serializable
+data class Res_Part(
+    val text: String
+)
 
 @Serializable
 data class SafetyRating(
