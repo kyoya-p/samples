2e6fc23992268b312c0e5b84e083e6a53acc974a
kyoya.p <kyoya.p@gmail.com>
2024-09-24 07:34:32 +0900

    n

--- 2024/Gemini/GemCode4_TS/src/generateContent.ts
+++ 2024/Gemini/GemCode4_TS/src/generateContent.ts
@@ -3,7 +3,8 @@ import { GoogleGenerativeAI } from "@google/generative-ai"
 async function main() {
     const key = process.env.GOOGLE_API_KEY ?? ""
     const genAI = new GoogleGenerativeAI(key)
-    const model = genAI.getGenerativeModel({ model: "gemini-1.5-flash" });
+    const model = genAI.getGenerativeModel({ model: "gemini-1.5-flash" })
+    
     async function qa(q: string) {
         const a = await model.generateContent(q)
         console.log(`Q: ${q}`)
