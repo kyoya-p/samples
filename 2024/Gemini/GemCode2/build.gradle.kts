bacea9a04addf3b335f151532f5d0f2b97cf59ea
kyoya.p <kyoya.p@gmail.com>
2024-09-23 09:18:04 +0900

    n

--- 2024/Gemini/GemCode2/build.gradle.kts
+++ 2024/Gemini/GemCode2/build.gradle.kts
@@ -1,6 +1,6 @@
 plugins {
     kotlin("jvm") version "2.0.0"
-    kotlin("plugin.serialization") version "2.0.0" // https://plugins.gradle.org/plugin/org.jetbrains.kotlin.plugin.serialization
+    kotlin("plugin.serialization") version "2.0.0"
     application
 }
 
--- 2024/Gemini/GemCode2/src/main/kotlin/Main.kt
+++ 2024/Gemini/GemCode2/src/main/kotlin/Main.kt
@@ -9,13 +9,13 @@ import kotlinx.serialization.json.Json
 import okio.FileSystem
 import okio.Path.Companion.toPath
 
-suspend fun main(args: Array<String>): Unit = with(FileSystem.SYSTEM) {
+suspend fun main(): Unit = with(FileSystem.SYSTEM) {
     val key = System.getenv("GOOGLE_API_KEY") ?: throw IllegalArgumentException("Not Found: GOOGLE_API_KEY")
     fun srcs() = listRecursively(".".toPath()).filter { it.name.endsWith(".kt") }.map { read(it) { readUtf8() } }
     val client = HttpClient(CIO) { install(ContentNegotiation) { json(Json { ignoreUnknownKeys = true }) } }
     client.post("https://generativelanguage.googleapis.com/v1beta/models/gemini-1.5-flash-latest:generateContent?key=$key") {
         contentType(ContentType.Application.Json)
-        setBody(ReqGemini(listOf(ReqContent(listOf(ReqPart("コードレビューして: ${srcs().joinToString("\n")}"))))))
+        setBody(ReqGemini(listOf(ReqContent(listOf(ReqPart("コードレビューして: ${srcs().joinToString()}"))))))
     }.body<ResGemini>().also(::println)
 }
 
