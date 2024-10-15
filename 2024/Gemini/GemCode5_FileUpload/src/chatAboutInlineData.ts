19d03451e563a469214123d21a5b5cf93c9f307e
kk <kyoya.p@gmail.com>
2024-10-16 08:28:50 +0900

    no

--- 2024/Gemini/GemCode5_FileUpload/src/chatAboutInlineData.ts
+++ 2024/Gemini/GemCode5_FileUpload/src/chatAboutInlineData.ts
@@ -4,12 +4,13 @@ import { Jimp } from "jimp"
 
 
 async function main() {
+    const file = process.argv[2]
     const genAI = new GoogleGenerativeAI(process.env.GOOGLE_API_KEY ?? "")
     const model = genAI.getGenerativeModel({ model: "gemini-1.5-flash-latest" })
     function filePart(path: string, mimeType: string): Part { return { inlineData: { data: Buffer.from(fs.readFileSync(path)).toString("base64"), mimeType, } } }
 
     const q = "画像左半分(カード部分)で透かしの文言は? またその矩形領域を文字ごとに示せ[[xmin,ymin,xmax,ymax],...]"
-    const imageParts = [filePart("2.jpg", "image/jpeg"), q,]
+    const imageParts = [filePart(file, "image/jpeg"), q,]
     const a = await model.generateContent(imageParts)
     console.log(`Q: ${q}\nA: ${a.response.text()}`)
     console.log(a.response.usageMetadata)
