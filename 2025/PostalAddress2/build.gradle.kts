565da31d33681c0513daa292610f981669f0e766
kk <kyoya.p@gmail.com>
2024-12-31 19:28:21 +0900

    n

--- 2025/PostalAddress2/build.gradle.kts
+++ 2025/PostalAddress2/build.gradle.kts
@@ -4,6 +4,7 @@ plugins {
     kotlin("jvm") version "2.1.0"
     id("org.jetbrains.compose")
     id("org.jetbrains.kotlin.plugin.compose")
+    kotlin("plugin.serialization") version "2.1.0"
 }
 
 group = "org.example"
@@ -22,9 +23,9 @@ dependencies {
     implementation("io.ktor:ktor-client-cio:$ktor_version")
     implementation("io.ktor:ktor-client-auth:$ktor_version")
     implementation("io.ktor:ktor-client-content-negotiation:$ktor_version")
-
     implementation("io.ktor:ktor-server-core:$ktor_version")
     implementation("io.ktor:ktor-server-cio:$ktor_version")
+    implementation("org.jetbrains.kotlinx:kotlinx-datetime:0.6.1")
 
     implementation("io.ktor:ktor-serialization-kotlinx-json:$ktor_version")
 
--- 2025/PostalAddress2/src/main/kotlin/Main.kt
+++ 2025/PostalAddress2/src/main/kotlin/Main.kt
@@ -1,3 +1,5 @@
+//@file:Suppress("PLUGIN_IS_NOT_ENABLED")
+
 import io.ktor.client.*
 import io.ktor.client.call.*
 import io.ktor.client.engine.cio.*
@@ -11,28 +13,32 @@ import io.ktor.serialization.kotlinx.json.*
 import io.ktor.server.engine.*
 import io.ktor.server.response.*
 import io.ktor.server.routing.*
-import kotlinx.coroutines.runBlocking
-import kotlinx.serialization.SerialName
+import kotlinx.datetime.Clock.System.now
+import kotlinx.datetime.TimeZone
+import kotlinx.datetime.toLocalDateTime
 import kotlinx.serialization.Serializable
+import kotlinx.serialization.decodeFromString
+import kotlinx.serialization.json.Json
+import kotlin.time.Duration.Companion.seconds
+
 
 @Serializable
 data class TokenInfo(
-    @SerialName("access_token") val accessToken: String,
-    @SerialName("expires_in") val expiresIn: Int,
-    @SerialName("refresh_token") val refreshToken: String? = null,
+    val access_token: String,
+    val expires_in: Int,
+    val refresh_token: String? = null,
     val scope: String,
-    @SerialName("token_type") val tokenType: String,
-    @SerialName("id_token") val idToken: String,
+    val token_type: String,
+    val id_token: String,
 )
 
 @Serializable
 data class UserInfo(
     val id: String,
     val name: String,
-    @SerialName("given_name") val givenName: String,
-    @SerialName("family_name") val familyName: String,
+    val given_name: String,
+    val family_name: String,
     val picture: String,
-    val locale: String
 )
 
 @Serializable
@@ -45,7 +51,7 @@ data class ErrorDetails(
     val status: String,
 )
 
-fun startServer() = embeddedServer(io.ktor.server.cio.CIO, port = 8080) {
+fun startRedirectReceiverServer() = embeddedServer(io.ktor.server.cio.CIO, port = 8080) {
     routing {
         get("/") {
             call.respondText("Authorization code: ${call.request.queryParameters["code"]}")
@@ -54,81 +60,79 @@ fun startServer() = embeddedServer(io.ktor.server.cio.CIO, port = 8080) {
 }.start()
 
 suspend fun main() {
-    startServer()
+    startRedirectReceiverServer()
+    val tokenOriginTime by lazy { now() }
+    // Step 1: Get an authorization code
+    val authorizationUrlQuery = parameters {
+        append("client_id", System.getenv("GOOGLE_CLIENT_ID"))
+        append("scope", "https://www.googleapis.com/auth/userinfo.profile")
+        append("response_type", "code")
+        append("redirect_uri", "http://127.0.0.1:8080")
+        append("access_type", "offline")
+    }.formUrlEncode()
+    println("https://accounts.google.com/o/oauth2/auth?$authorizationUrlQuery")
+    println("Open a link above, get the authorization code, insert it below, and press Enter.")
+    val authorizationCode = readln()
 
-        // Step 1: Get an authorization code
-        val authorizationUrlQuery = parameters {
-            append("client_id", System.getenv("GOOGLE_CLIENT_ID"))
-            append("scope", "https://www.googleapis.com/auth/userinfo.profile")
-            append("response_type", "code")
-            append("redirect_uri", "http://127.0.0.1:8080")
-            append("access_type", "offline")
-        }.formUrlEncode()
-        println("https://accounts.google.com/o/oauth2/auth?$authorizationUrlQuery")
-        println("Open a link above, get the authorization code, insert it below, and press Enter.")
-        val authorizationCode = readln()
+    // Step 2: Create a storage for tokens
+    val bearerTokenStorage = mutableListOf<BearerTokens>()
 
-        // Step 2: Create a storage for tokens
-        val bearerTokenStorage = mutableListOf<BearerTokens>()
-
-        // Step 3: Configure the client for receiving tokens and accessing the protected API
-        val client = HttpClient(CIO) {
-            install(ContentNegotiation) {
-                json()
-            }
-
-            install(Auth) {
-                bearer {
-                    loadTokens {
-                        bearerTokenStorage.last()
-                    }
-                    refreshTokens {
-                        val refreshTokenInfo: TokenInfo = client.submitForm(
-                            url = "https://accounts.google.com/o/oauth2/token",
-                            formParameters = parameters {
-                                append("grant_type", "refresh_token")
-                                append("client_id", System.getenv("GOOGLE_CLIENT_ID"))
-                                append("refresh_token", oldTokens?.refreshToken ?: "")
-                            }
-                        ) { markAsRefreshTokenRequest() }.body()
-                        bearerTokenStorage.add(BearerTokens(refreshTokenInfo.accessToken, oldTokens?.refreshToken!!))
-                        bearerTokenStorage.last()
-                    }
-                    sendWithoutRequest { request ->
-                        request.url.host == "www.googleapis.com"
-                    }
+    // Step 3: Configure the client for receiving tokens and accessing the protected API
+    val client = HttpClient(CIO) {
+        install(ContentNegotiation) { json() }
+        install(Auth) {
+            bearer {
+                loadTokens { bearerTokenStorage.last() }
+                refreshTokens {
+                    val refreshTokenInfo: TokenInfo = client.submitForm(
+                        url = "https://accounts.google.com/o/oauth2/token",
+                        formParameters = parameters {
+                            append("grant_type", "refresh_token")
+                            append("client_id", System.getenv("GOOGLE_CLIENT_ID"))
+                            append("refresh_token", oldTokens?.refreshToken ?: "")
+                        }
+                    ) { markAsRefreshTokenRequest() }.body()
+                    bearerTokenStorage.add(BearerTokens(refreshTokenInfo.access_token, oldTokens?.refreshToken!!))
+                    bearerTokenStorage.last()
                 }
-            }
-        }
-
-        // Step 4: Exchange the authorization code for tokens and save tokens in the storage
-        val tokenInfo: TokenInfo = client.submitForm(
-            url = "https://accounts.google.com/o/oauth2/token",
-            formParameters = parameters {
-                append("grant_type", "authorization_code")
-                append("code", authorizationCode)
-                append("client_id", System.getenv("GOOGLE_CLIENT_ID"))
-                append("client_secret", System.getenv("GOOGLE_CLIENT_SECRET"))
-                append("redirect_uri", "http://127.0.0.1:8080")
-            }
-        ).body()
-        bearerTokenStorage.add(BearerTokens(tokenInfo.accessToken, tokenInfo.refreshToken!!))
-
-        // Step 5: Make a request to the protected API
-        while (true) {
-            println("Make a request? Type 'yes' and press Enter to proceed.")
-            when (readln()) {
-                "yes" -> {
-                    val response = client.get("https://www.googleapis.com/oauth2/v2/userinfo")
-                    try {
-                        val userInfo: UserInfo = response.body()
-                        println("Hello, ${userInfo.name}!")
-                    } catch (e: Exception) {
-                        val errorInfo: ErrorInfo = response.body()
-                        println(errorInfo.error.message)
-                    }
+                sendWithoutRequest { request ->
+                    request.url.host == "www.googleapis.com"
                 }
-                else -> return
             }
         }
     }
+
+    // Step 4: Exchange the authorization code for tokens and save tokens in the storage
+    val tokenInfo: TokenInfo = client.submitForm(
+        url = "https://accounts.google.com/o/oauth2/token",
+        formParameters = parameters {
+            append("grant_type", "authorization_code")
+            append("code", authorizationCode)
+            append("client_id", System.getenv("GOOGLE_CLIENT_ID"))
+            append("client_secret", System.getenv("GOOGLE_CLIENT_SECRET"))
+            append("redirect_uri", "http://127.0.0.1:8080")
+        }
+    ).body()
+    println(tokenOriginTime.toLocalDateTime(TimeZone.currentSystemDefault()))
+    println(tokenInfo)
+    bearerTokenStorage.add(BearerTokens(tokenInfo.access_token, tokenInfo.refresh_token!!))
+
+    // Step 5: Make a request to the protected API
+    while (true) {
+        println("Make a request? Type 'yes' and press Enter to proceed.")
+        println("Expire: ${(tokenOriginTime + tokenInfo.expires_in.seconds).toLocalDateTime(TimeZone.currentSystemDefault())}")
+        when (readln()) {
+            "yes" -> {
+                val res: String = client.get("https://www.googleapis.com/oauth2/v2/userinfo").body()
+                try {
+                    val userInfo: UserInfo = Json.decodeFromString(res)
+                    println("Hello, ${res}!")
+                } catch (e: Exception) {
+                    val errorInfo: ErrorInfo = Json.decodeFromString(res)
+                    println(errorInfo.error.message)
+                }
+            }
+            else -> return
+        }
+    }
+}
