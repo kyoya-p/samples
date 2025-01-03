15d393650797364bbcb5dab70822b4a93a8828c2
kk <kyoya.p@gmail.com>
2025-01-03 11:20:37 +0900

    n

---
+++ 2025/OAuth/Step11/src/main/kotlin/Main.kt
@@ -0,0 +1,62 @@
+import io.ktor.client.*
+import io.ktor.client.engine.cio.*
+import io.ktor.client.request.forms.*
+import io.ktor.client.statement.*
+import io.ktor.http.*
+import io.ktor.server.engine.*
+import io.ktor.server.response.*
+import io.ktor.server.routing.*
+import kotlinx.coroutines.cancel
+import kotlin.coroutines.resume
+import kotlin.coroutines.suspendCoroutine
+
+suspend fun main() {
+    val client = HttpClient(CIO)
+    val redirectUri = "http://127.0.0.1:28080"
+    // 1. 認可リクエストURLの構築
+    val authorizationUrl = URLBuilder("https://accounts.google.com/o/oauth2/auth").apply {
+        parameters.append("client_id", System.getenv("GOOGLE_CLIENT_ID")!!)
+        parameters.append("redirect_uri", redirectUri)
+        parameters.append("response_type", "code")
+        parameters.append("scope", "https://www.googleapis.com/auth/userinfo.profile")
+    }.buildString()
+    println("ブラウザで以下のURLを開いて認証してください:\n$authorizationUrl")
+
+    val authorizationCode = redirectReceiverServer()!!  // ユーザーがブラウザで認証を完了し、redirectUriにリダイレクトされるのを待つ
+    println("authorizationCode=$authorizationCode")
+
+    // 2. トークンリクエスト
+    val tokenResponse = client.submitForm(
+        url = "https://accounts.google.com/o/oauth2/token",
+        formParameters = Parameters.build {
+            append("grant_type", "authorization_code")
+            append("code", authorizationCode)
+            append("redirect_uri", redirectUri)
+            append("client_id", System.getenv("GOOGLE_CLIENT_ID")!!)
+            append("client_secret", System.getenv("GOOGLE_CLIENT_SECRET")!!)
+        }
+    )
+
+    if (tokenResponse.status.isSuccess()) {
+        val tokenResponseBody = tokenResponse.bodyAsText()
+        println("アクセストークン取得成功:\n$tokenResponseBody")
+    } else {
+        println("アクセストークン取得失敗: ${tokenResponse.status}")
+        println(tokenResponse.bodyAsText())
+    }
+
+    client.close()
+}
+
+suspend fun redirectReceiverServer() = suspendCoroutine { ctn ->
+    embeddedServer(io.ktor.server.cio.CIO, port = 28080) {
+        routing {
+            get("/") {
+                call.respondText("認証完了しました")
+                ctn.resume(call.request.queryParameters["code"])
+                cancel()
+            }
+        }
+    }.start(wait = false)
+}
+
