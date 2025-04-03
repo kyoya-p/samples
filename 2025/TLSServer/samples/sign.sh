fbb2b7a767fca396ca793105bddb64b304514afc
kk <kk>
2025-04-03 14:22:04 +0900

    n

--- 2025/TLSServer/samples/sign.sh
+++ 2025/TLSServer/samples/sign.sh
@@ -9,7 +9,8 @@ keytool -genkeypair -alias tomcat -keyalg RSA -keysize 2048 -keystore .keystore 
 keytool -certreq -alias tomcat -keystore .keystore -storepass changeit -file server.csr
 
 # 署名要求に従いCA証明書で署名
-echo 'subjectAltName =DNS:localhost, IP:127.0.0.1' > server.ext
+#echo 'subjectAltName =DNS:localhost, IP:127.0.0.1' > server.ext
+echo 'subjectAltName =DNS:localhost, IP.1:127.0.0.1, IP.2:10.36.104.102' > server.ext  # 複数アドレス
 openssl x509 -req -days 365 -CA ca.crt -CAkey ca.key -CAcreateserial -in server.csr -extfile server.ext -out server.crt
 
 # CA証明書、サーバ証明書をkeystoreに登録
