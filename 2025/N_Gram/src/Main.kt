915139f1715832c5b96242bacc2adf1c2d250e19
kk <kyoya.p@gmail.com>
2025-04-16 08:32:07 +0900

    n

--- 2025/N_Gram/src/Main.kt
+++ 2025/N_Gram/src/Main.kt
@@ -1,33 +1,27 @@
-fun generateNGrams(text: String, n: Int): List<String> {
-    val ngrams = mutableListOf<String>()
-    for (i in 0..text.length - n) {
-        ngrams.add(text.substring(i, i + n))
-    }
-    return ngrams
-}
-
-//fun generateNGrams(text: String, n: Int) = text.windowed(n)
-    
-fun search(query: String, documents: List<String>, n: Int): List<String> {
-    val queryNGrams = generateNGrams(query, n)
-    return documents.filter { document ->
-        val documentNGrams = generateNGrams(document, n)
-        queryNGrams.all { ngram ->
-            documentNGrams.contains(ngram)
+fun search(db: List<Record>, query: String, n: Int) {
+    val queryNGrams = query.windowed(n) // N-Gram生成
+    println("Query: $query($queryNGrams)")
+    db.forEach { record ->
+        println(record.ngrams)
+        if (queryNGrams.all { ngram -> record.ngrams.contains(ngram) }) {
+            println("=> ${record.text}")
         }
     }
+    println()
 }
 
+class Record(val text: String, val n: Int, val ngrams: List<String> = text.windowed(n))
+
 fun main() {
-    val documents = listOf(
-        "今日はいい天気です。",
-        "明日は雨が降るでしょう。",
-        "昨日は晴れていました。"
+    val n = 3
+    val db = listOf(
+        Record("今日はいい天気です。", n),
+        Record("明日は雨が降るでしょう。", n),
+        Record("昨日は晴れていました。", n),
+        Record("明日の天気予報では雪になりそう", n),
+        Record("明日の天気は雪の予報です", n),
     )
 
-    val query = "天気"
-    val n = 2
-
-    val results = search(query, documents, n)
-    println(results) // [今日はいい天気です。]
+    search(db, "天気", n)
+    search(db, "天気予報", n)
 }
