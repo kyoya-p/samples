320cb3f502a5b67f291737f9f3ded8e30bd5f344
kk <kyoya.p@gmail.com>
2025-04-18 08:46:15 +0900

    n

--- 2025/N_Gram/src/Main.kt
+++ 2025/N_Gram/src/Main.kt
@@ -4,27 +4,24 @@ import kotlinx.io.files.Path
 import kotlinx.io.files.SystemFileSystem
 import kotlinx.io.readLine
 
+fun main(): Unit = with(SystemFileSystem) {
+    val R = 1..2
+    val texts = (readLines("SampleData_ja.txt") + readLines("SampleData_en.txt")).toList()
 
-class Record(val text: String, val n: Int, val ngrams: List<String> = text.windowed(n))
+    val ngramIndexes: Map<String, Set<Int>> = texts.mapIndexed { ix, tx -> makeNgrams(tx, R).map { it to ix } }
+        .flatten().groupBy({ it.first }, { it.second }).mapValues { it.value.toSet() }
 
-fun main() = with(SystemFileSystem) {
-    val texts = (readLines("SampleData_ja.txt")).joinToString("\n")
-    for (i in 1..20) {
-        makeNgrams(texts, i..i).let { println("$i: ${it.size}") }
-    }
-    //    ngramMatchingSearch(records, "革新的な再", n)
-}
-
-fun makeNgrams(text: String, nRange: IntRange) = nRange.asSequence().flatMap { n -> text.windowed(n) }.toSet()
-
-fun ngramMatchingSearch(db: Collection<Record>, query: String, n: Int) {
-    val queryNGrams = query.windowed(n) // N-Gram生成
-    println("Query: $query($queryNGrams)")
-    db.filter { record -> queryNGrams.all { ngram -> record.ngrams.contains(ngram) } }.forEach {
-        println("  => ${it.text.take(64)}...")
+    ngramIndexSearch(ngramIndexes, makeNgrams("革新的な再", R)).forEach {
+        println("[$it] ${texts[it].take(64)}")
     }
 }
 
+fun makeNgrams(text: String, nRange: IntRange) = nRange.flatMap { n -> text.windowed(n).distinct() }.toSet()
+
+fun ngramIndexSearch(nGramIdx: Map<String, Set<Int>>, qNgrams: Set<String>): Set<Int> {
+    val q = qNgrams.map { nGramIdx[it] ?: return@map emptySet() }
+    return q.drop(1).fold(q.first()) { a, e -> a.intersect(e) }
+}
 
 fun FileSystem.readLines(file: String) = source(Path(file)).buffered().let { generateSequence { it.readLine() } }
 
