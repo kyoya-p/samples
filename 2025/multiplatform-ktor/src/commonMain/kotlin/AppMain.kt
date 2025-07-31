77e7fc4e850cc0c7efe65a7b04534dd7abbae675
kk <kyoya.q@gmail.com>
2025-08-01 06:55:36 +0900

    n

--- 2025/multiplatform-ktor/src/commonMain/kotlin/AppMain.kt
+++ 2025/multiplatform-ktor/src/commonMain/kotlin/AppMain.kt
@@ -4,46 +4,64 @@ import io.ktor.client.engine.cio.*
 import io.ktor.client.request.*
 import io.ktor.client.plugins.contentnegotiation.ContentNegotiation
 import io.ktor.client.plugins.defaultRequest
+import io.ktor.client.statement.bodyAsText
 import io.ktor.serialization.kotlinx.json.json
 import io.ktor.server.application.Application
 import io.ktor.server.application.install
+import io.ktor.server.cio.unixConnector
 import io.ktor.server.engine.embeddedServer
 import io.ktor.server.response.respond
 import io.ktor.server.routing.get
 import io.ktor.server.routing.routing
+import kotlinx.coroutines.delay
+import kotlinx.io.files.Path
 
 import kotlinx.serialization.Serializable
+import kotlin.time.Duration.Companion.milliseconds
 
 @Serializable
 data class Echo(val message: String)
 
-suspend fun requestEcho(server: String, from: String): Echo {
-    val client = HttpClient(CIO) { install(ContentNegotiation) { json() } }
-    return client.get("$server/echo?from=$from").body<Echo>()
+suspend fun appMain() {
+    testService()
+    unixDomainSocket_TODO()
 }
 
-
-fun Application.testServer() {
+fun Application.testService() {
     install(io.ktor.server.plugins.contentnegotiation.ContentNegotiation) { json() }
     routing {
         get("/echo") {
             val message = call.request.queryParameters["from"]?.let { "Hello, $it!" } ?: "Who are you?"
+            println("Resp: $message")
             call.respond(Echo(message = message))
         }
     }
 }
 
-suspend fun appMain() {
-    val svr = embeddedServer(io.ktor.server.cio.CIO, 28080) {
-        testServer()
-    }.startSuspend(wait = false)
+suspend fun testService() {
+    fun testServer(module: Application.() -> Unit) = embeddedServer(io.ktor.server.cio.CIO, port = 28080) { module() }
 
-    val client = HttpClient(CIO) {
-        defaultRequest { unixSocket("/tmp/test-unix-socket-client.sock") }
-        install(ContentNegotiation) { json() }
-    }
-    client.get("$/echo?from=Shokkaa").body<Echo>()
-
-    svr.stopSuspend()
+    val serverJob = testServer { testService() }.startSuspend()
+    println("Start:")
+    val client = HttpClient(CIO) { install(ContentNegotiation) { json() } }
+    val res = client.get("http://localhost:28080/echo?from=Shokkaa").body<Echo>()
+    println("Recv: ${res.message}")
+    serverJob.stopSuspend()
 }
 
+suspend fun unixDomainSocket_TODO() {
+    val socketPath = Path("./.test.sock")
+    fun testServer_UnixConnector(module: Application.() -> Unit) = embeddedServer(io.ktor.server.cio.CIO, configure = {
+        unixConnector(socketPath.toString())
+    }, module = module)
+
+    val serverJob = testServer_UnixConnector { testService() }.startSuspend()
+    println("Start:")
+    delay(100.milliseconds)
+    val client = HttpClient(CIO) {
+        defaultRequest { unixSocket("$socketPath") }
+    }
+    val res = client.get("$/echo?from=Shokkaa").bodyAsText()
+    println("Recv: $res")
+    serverJob.stopSuspend()
+}
