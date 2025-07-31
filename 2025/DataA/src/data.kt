d63bb45cfb9aef090e109991d2c0b4c58922cd3e
kk <kyoya.q@gmail.com>
2025-08-01 08:31:34 +0900

    n

--- 2025/DataA/src/data.kt
+++ 2025/DataA/src/data.kt
@@ -13,15 +13,27 @@ fun listItem() = with(SystemFileSystem) {
     val src = source(Path("C:\\Users\\kyoya\\Desktop\\works\\ag\\agent.log")).buffered()
     generateSequence { src.readLine() }
         .mapNotNull { Regex("StatusReport\\[(.*)]").find(it)?.groupValues?.getOrNull(1) }
-        .onEach { println(it) }
+//        .onEach { println(it) }
         .mapNotNull { runCatching { it.toRecord() }.onFailure { }.getOrNull() }
 }
-fun dataSet1() = listItem().asFlow()
-
+fun dataSet1() = mapOf(
+    "system.cpuUsage" to listItem().map{it.environmentalInfo.usageInfo.systemUsage.cpuUsage}.toList(),
+    "system.cpuLoadAverage" to listItem().map{it.environmentalInfo.usageInfo.systemUsage.cpuLoadAverage}.toList(),
+    "system.diskSpaceTotal" to listItem().map{it.environmentalInfo.usageInfo.systemUsage.diskSpaceTotal}.toList(),
+    "system.swapSpaceTotal" to listItem().map{it.environmentalInfo.usageInfo.systemUsage.swapSpaceTotal}.toList(),
+    "heap.usage" to listItem().map{it.environmentalInfo.usageInfo.heapUsage.usage.toDouble()}.toList(),
+    "heap.max" to listItem().map{it.environmentalInfo.usageInfo.heapUsage.max.toDouble()}.toList(),
+    "heap.init" to listItem().map{it.environmentalInfo.usageInfo.heapUsage.init.toDouble()}.toList(),
+    "heap.commited" to listItem().map{it.environmentalInfo.usageInfo.heapUsage.commited.toDouble()}.toList(),
+    "vmProcess.uptime" to listItem().map{it.environmentalInfo.usageInfo.vmProcessUsage.uptime.toDouble()}.toList(),
+    "vmProcess.cpuTime" to listItem().map{it.environmentalInfo.usageInfo.vmProcessUsage.cpuTime.toDouble()}.toList(),
+    "vmProcess.startTime" to listItem().map{it.environmentalInfo.usageInfo.vmProcessUsage.startTime.toDouble()}.toList(),
+    "vmProcess.cpuUsage" to listItem().map{it.environmentalInfo.usageInfo.vmProcessUsage.cpuUsage.toDouble()}.toList(),
+)
 @Serializable
 data class Record(
     val environmentalInfo: EnvironmentalInfo,
-//    val srdmUsageInfo: SrdmUsageInfo
+    val srdmUsageInfo: SrdmUsageInfo
 )
 
 @Serializable
@@ -77,18 +89,17 @@ data class HwInfo(
 @Serializable
 data class SrdmUsageInfo(
     val devices: Int,
-//    val reportLog: Map<String, String>,
-//    val dbStorageSize: Int,
-//    val userNumber: Int,
-//    val accessCount: AccessCount,
-//    val accessBrowserCount: Map<String, String>,
-//    val storageFreeSpace: Int,
-//    val dbFileSize: Int
+    val reportLog: Map<String, String>,
+    val dbStorageSize: Int,
+    val userNumber: Int,
+    val accessCount: AccessCount,
+    val accessBrowserCount: Map<String, String>,
+    val storageFreeSpace: Int,
+    val dbFileSize: Int
 )
 
 @Serializable
 data class AccessCount(val mib: Map<String, String>, val fss: Map<String, String>, val tco: Map<String, String>)
-
 
 /*
 {"status":"Regular","timeStamp":1747972464577,"agentInfo":{"ipAddress":"192.168.24.76","classifier":"embedded","domainName":"","port":8088,"hostName":""},"softwareUniqueInfo":{"softwareName":"Synappx Manage Agent","softwareIdentificationCode":"","softwareVersion":"1.9.17393"},"environmentalInfo":{"usageInfo":{"systemUsage":{"cpuUsage":0.05788658248795789,"cpuLoadAverage":5.30029296875,"physicalMemoryTotal":3272142848,"physicalMemoryFree":296202240,"swapSpaceTotal":3640647680,"swapSpaceFree":3640647680,"diskSpaceTotal":16729894912,"diskSpaceFree":15600713728,"diskUsabeSpace":14724943872},"vmProcessUsage":{"cpuUsage":5.023683077364719E-4,"cpuTime":160620000000,"uptime":15642893,"startTime":1747956821674},"heapUsage":{"usage":109014336,"init":52428800,"commited":164626432,"max":819986432},"threadUsage":{"threadCount":153,"totalStartedThreadCount":409,"peakThreadCount":163,"daemonThreadCount":133}},"systemInfo":{"osInfo":{"osname":"Linux","architecture":"amd64","version":"5.15.133-rt69-intel-pk-preempt-rt"},"hwInfo":{"numberOfProcessor":4,"physicalMemory":3272142848,"virtualMemory":4695703552,"swapSpace":3640647680,"diskSpace":16729894912},"vmInfo":{"vmName":"OpenJDK 64-Bit Server VM","vmVendor":"Eclipse Adoptium","vmVersion":"17.0.15+6"}}},"srdmUsageInfo":{"devices":0,"reportLog":{},"dbStorageSize":0,"userNumber":0,"accessCount":{"mib":{},"fss":{},"tco":{}},"accessBrowserCount":{},"storageFreeSpace":0,"dbFileSize":0},"instanceInfo":{"country":"USA","organizationName":""}}
--- 2025/DataA/src/main.kt
+++ 2025/DataA/src/main.kt
@@ -1,19 +1,14 @@
-import androidx.compose.foundation.layout.Column
-import androidx.compose.foundation.layout.IntrinsicSize
-import androidx.compose.foundation.layout.Row
-import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.foundation.layout.fillMaxWidth
-import androidx.compose.foundation.layout.height
-import androidx.compose.foundation.layout.padding
-import androidx.compose.foundation.layout.width
+import androidx.compose.foundation.layout.*
 import androidx.compose.foundation.selection.selectable
 import androidx.compose.material.MaterialTheme
 import androidx.compose.material.RadioButton
 import androidx.compose.material.Text
-import androidx.compose.runtime.*
+import androidx.compose.runtime.Composable
+import androidx.compose.runtime.MutableState
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.window.Window
 import androidx.compose.ui.window.application
@@ -42,8 +37,10 @@ fun main() = application {
             Column(
                 modifier = Modifier.fillMaxSize().padding(start = 10.dp, top = 10.dp, end = 10.dp, bottom = 10.dp),
             ) {
+                val dataSet = dataXYZ(1.0)
+                val dataSet2 = dataSet1()
                 PlotPanel(
-                    figure = letsPlot(dataXYZ(1.0)+dataXYZ(2.0)){color="Z"}
+                    figure = letsPlot(dataSet2){color="Z"}
 //                    +geomPolygon { x = "X"; y = "Y"; }
                             + geomLine { x = "X"; y = "X"; }
                             + geomLine { x = "X"; y = "Z" }
