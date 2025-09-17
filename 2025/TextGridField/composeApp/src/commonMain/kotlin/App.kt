c2c7c46c8555bdc9d17e890f969aecdbe900b217
kk <kyoya.q@gmail.com>
2025-09-18 06:19:49 +0900

    n

--- 2025/TextGridField/composeApp/src/commonMain/kotlin/App.kt
+++ 2025/TextGridField/composeApp/src/commonMain/kotlin/App.kt
@@ -21,7 +21,6 @@ expect fun defaultTextStyle(): TextStyle
 fun App() = MaterialTheme {
     var textField by remember { mutableStateOf(TextFieldValue("||l|\n|あ|ああ|")) }
     var newTextField by remember { mutableStateOf(textField) }
-    var text by remember { mutableStateOf("||1|11|111|あ|ああ|") }
     Column {
         TextField(
             textField,
@@ -43,63 +42,13 @@ fun App() = MaterialTheme {
 
 typealias Grid<T> = List<List<T>>
 
-val textWidth = mutableMapOf<String, Int>()
-
-//@Composable
-//fun TextStyle.textWidth(s: String): Int {
-//    if (textWidth.containsKey(s)) return textWidth[s]!!
-//    val w = rememberTextMeasurer().measure(s, this).size.width
-//    textWidth[s] = w
-//    return w
-//}
-
 @Composable
 fun String.textWidth(style: TextStyle): Int {
-//    if (textWidth.containsKey(s)) return textWidth[s]!!
     val w = rememberTextMeasurer().measure(this, style).size.width
-//    textWidth[s] = w
     return w
 }
 
-//@Composable
-//fun TextGridField(
-//    value: TextFieldValue,
-//    textStyle: TextStyle = defaultTextStyle(),
-//    paddingLetter: Char = ' ',
-//    onValueChange: (TextFieldValue) -> Unit
-//) = Column(Modifier.padding(4.dp)) {
-//    var textField by remember { mutableStateOf(value) }
-//    var newTextField by remember { mutableStateOf(value) }
-//    TextField(value, textStyle = textStyle, onValueChange = { textField = it })
-//    Text(
-//        """
-//len=${value.text.length}
-//selection=${value.selection}
-//===========
-//${value.text.alignPipes()}}
-//===========
-//ver: 0.2
-//        """.trimIndent(), style = textStyle
-//    )
-//    newTextField = TextFieldValue(
-//        text = value.text.alignPipes(),
-//        selection = value.selection, //キャレット座標保持
-//        composition = value.composition
-//    )
-//
-//    LaunchedEffect(newTextField) {
-//        onValueChange(newTextField)
-//    }
-//}
-
-/*
-各行の左端から各 "|"(パイプ)の位置(位置はUI上の位置でありString.textWidth()関数で算出される)を同じ位置にするよう"|"の前に空白を挿入する
-- 文字列を"\n"で分離しrows:List<String>とする
-- 各行の条件:
-  - 左端からn文字までの文字幅は String.textWidth()関数で算出される。
-  - 左端からm番目の"|"(パイプ)は、他の行のm番目の"|"(パイプ) と同じ位置(左端からの文字幅の位置)に最も近く配置される
-- 返値は各行を"\n"をセパレータとして結合したもの
-   */
+// 各行の左端から各"|"の前に空白を挿入しUI上の位置を揃える
 @Composable
 fun String.alignPipes(
     style: TextStyle = defaultTextStyle(),
@@ -112,7 +61,7 @@ fun String.alignPipes(
 
     // 各行n番目の"|"までの文字幅の最大値を返す
     val maxPipePos = grid.map { r ->
-        r.mapIndexed { x, c -> r.take(x + 1).joinToString("|").textWidth(style) }
+        r.mapIndexed { x, _ -> r.take(x + 1).joinToString("|").textWidth(style) }
     }.max()
 
     // 指定文字幅に最も近くなるよう文字列に" "を加える
