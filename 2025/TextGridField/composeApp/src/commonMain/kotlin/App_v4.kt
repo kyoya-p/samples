e945693a725145541c3f036b9b32c60f249bc03b
kk <kyoya.q@gmail.com>
2025-09-20 15:28:31 +0900

    n

--- 2025/TextGridField/composeApp/src/commonMain/kotlin/App_v4.kt
+++ 2025/TextGridField/composeApp/src/commonMain/kotlin/App_v4.kt
@@ -1,19 +1,16 @@
 package v4
 
 import androidx.compose.foundation.layout.Column
-import androidx.compose.foundation.layout.padding
 import androidx.compose.material3.HorizontalDivider
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Text
 import androidx.compose.material3.TextField
 import androidx.compose.runtime.*
-import androidx.compose.ui.Modifier
 import androidx.compose.ui.text.TextMeasurer
 import androidx.compose.ui.text.TextRange
 import androidx.compose.ui.text.TextStyle
 import androidx.compose.ui.text.input.TextFieldValue
 import androidx.compose.ui.text.rememberTextMeasurer
-import androidx.compose.ui.unit.dp
 import defaultTextStyle
 import kotlin.math.abs
 
@@ -21,26 +18,23 @@ import kotlin.math.abs
 fun App() = MaterialTheme {
     val textMeasurer = rememberTextMeasurer()
     val defaultTextStyle = defaultTextStyle()
-    var textField by remember { mutableStateOf(TextFieldValue("||l|\n|あ|ああ|")) }
+    var textField by remember { mutableStateOf(TextFieldValue("|aaa|wwww|\n|,,,,|MMMMM|")) }
     var newTextField by remember { mutableStateOf(textField) }
     Column {
         var t by remember { mutableStateOf("test") }
-        var tf by remember { mutableStateOf(TextFieldValue("test TextField")) }
-        TextField(
-            value = t,
-            textStyle = defaultTextStyle,
-            onValueChange = {
-//                t = it
-                t = it.alignPipes2(textMeasurer, defaultTextStyle, ' ')
-            })
+        var tf by remember { mutableStateOf(TextFieldValue("|aaa|wwww|\n|,,,,|MMMMM|")) }
 
-        TextField(
-            textField,
-            modifier = Modifier.padding(4.dp),
-            textStyle = defaultTextStyle(),
-            onValueChange = { newTextField = it }
-        )
-        textField = newTextField.align(defaultTextStyle(), paddingLetter = ' ')
+        TextField(tf, textStyle = defaultTextStyle, onValueChange = {
+            tf = it.alignPipes(textMeasurer, defaultTextStyle, ' ')
+        })
+        Text(tf.selection.toString())
+        val xy1 = tf.text.posToXY(tf.selection.end)
+        val xy2 = tf.text.posToXY(tf.selection.start)
+        Text("$xy1 - $xy2")
+
+        val p1 = tf.text.xyToPos(xy1.first, xy1.second)
+        val p2 = tf.text.xyToPos(xy2.first, xy2.second)
+        Text("$p1 - $p2")
 
         HorizontalDivider()
         Text("TextGridField-0.4.250918")
@@ -62,69 +56,80 @@ fun String.textWidth(textMeasurer: TextMeasurer, style: TextStyle): Int {
     return w
 }
 
-// カーソル位置にマーカーを埋め込む
-@Composable
-fun TextFieldValue.alignWithMarker(style: TextStyle = defaultTextStyle(), paddingLetter: Char = ' '): TextFieldValue {
-    fun String.insMarker(p: Int) = take(p) + zeroWidthSpace + drop(p)
-    fun String.delMarker(): Pair<Int, String> = indexOf(zeroWidthSpace).let { it to take(it) + drop(it + 1) }
-    val (s, e) = with(selection) { if (start > end) end to start else start to end }
-    return text.insMarker(e).insMarker(s).alignPipes(style, paddingLetter)
-        .delMarker().let { (p1, s1) ->
-            s1.delMarker().let { (p2, s2) ->
-                TextFieldValue(
-                    text = s2,
-                    selection = with(selection) { if (start > end) TextRange(p2, p1) else TextRange(p1, p2) }
-                )
-            }
-        }
-}
 
 // カーソル位置にマーカーを埋め込まない
-@Composable
-fun TextFieldValue.align(style: TextStyle = defaultTextStyle(), paddingLetter: Char = ' '): TextFieldValue {
-    return this
+fun TextFieldValue.align(textMeasurer: TextMeasurer, style: TextStyle, paddingLetter: Char): TextFieldValue {
+//    fun String.insMarker(p: Int) = take(p) + zeroWidthSpace + drop(p)
+//    fun String.delMarker(): Pair<Int, String> = indexOf(zeroWidthSpace).let { it to take(it) + drop(it + 1) }
+    var (s, e) = with(selection) { if (start > end) end to start else start to end }
+    return TextFieldValue(
+        text = text.alignPipes(textMeasurer, style, paddingLetter),
+        selection = selection,
+        composition = composition,
+    )
 }
 
+fun String.posToXY(p: Int): Pair<Int, Int> =
+    p - take(p).dropLastWhile { it != '\n' }.count() to take(p).count { it == '\n' }
+
+fun String.xyToPos(x: Int, y: Int): Int = split('\n').take(y).sumOf { it.length + 1 } + x
+
+
 // 各行の各"|"の前に空白を挿入しUI上の位置を揃える
-@Composable
-fun String.alignPipes(
+fun TextFieldValue.alignPipes(
+    textMeasurer: TextMeasurer,
     style: TextStyle,
     paddingLetter: Char,
-): String = runCatching {
-    val grid = split("\n").map { it.split("|") }
+): TextFieldValue {
+    var (x1, y1) = text.posToXY(selection.start)
+    var (x2, y2) = text.posToXY(selection.end)
+
+    val grid = text.split("\n").map { it.split("|") }
     fun Grid<Int>.max(): List<Int> = (0..<maxOf { it.size }).map { x -> maxOf { it.getOrNull(x) ?: 0 } }
 
     // 各行n番目の"|"までの文字幅の最大値を返す
     val maxPipePos = grid.map { r ->
-        r.mapIndexed { x, _ -> r.take(x + 1).joinToString("|").textWidth(style) }
+        r.mapIndexed { x, _ -> r.take(x + 1).joinToString("|").textWidth(textMeasurer, style) }
     }.max()
 
     // 指定文字幅に最も近くなるよう文字列に" "を加える
-    @Composable
-    fun String.padding(w: Int): String {
+    fun String.padding(w: Int, onIns: (Int) -> Unit): String {
         var s1 = this
         while (true) {
             val s2 = "$s1$paddingLetter"
-            if (abs(w - s1.textWidth(style)) <= abs(w - s2.textWidth(style))) return s1
+            if (abs(w - s1.textWidth(textMeasurer, style)) <= abs(w - s2.textWidth(textMeasurer, style))) return s1
+            onIns(s1.count())
             s1 = s2
         }
     }
 
     // 各列の左端からの文字幅を最大の文字幅に合わせる
-    val rows = MutableList(grid.size) { grid[it][0].dropLastWhile { it == paddingLetter } }
+    val rows = MutableList(grid.size) { y ->
+//        if (y == y1 && y1 >) ++x1
+//        if (y == y2) ++x2
+        grid[y][0].dropLastWhile { it == paddingLetter }
+    }
     for (x in 1..<maxPipePos.size) {
-        val mw = rows.maxOf { it.textWidth(style) }
+        val mw = rows.maxOf { it.textWidth(textMeasurer, style) }
         for (y in grid.indices) {
-            rows[y] = rows[y].padding(mw)
+            rows[y] = rows[y].padding(mw) { ipos ->
+                if (y == y1 && ipos < x1) ++x1
+                if (y == y2 && ipos < x2) ++x2
+            }
             val c = grid[y].getOrNull(x)?.dropLastWhile { it == paddingLetter } ?: continue
             rows[y] += "|" + c
         }
     }
-    return rows.joinToString("\n")
-}.getOrElse { it.stackTraceToString() }
+    return TextFieldValue(
+        text = rows.joinToString("\n"),
+        selection = TextRange(text.xyToPos(x1, y1), text.xyToPos(x2, y2)),
+        composition = composition,
+    )
+}
+
 
 // 各行の各"|"の前に空白を挿入しUI上の位置を揃える
-fun String.alignPipes2(
+fun String.alignPipes(
     textMeasurer: TextMeasurer,
     style: TextStyle,
     paddingLetter: Char,
