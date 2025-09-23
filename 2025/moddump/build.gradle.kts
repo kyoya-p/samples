0afa879719658ce28835fe7405d7a6bc840deac1
kk <kyoya.q@gmail.com>
2025-09-24 08:30:53 +0900

    n

--- 2025/moddump/build.gradle.kts
+++ 2025/moddump/build.gradle.kts
@@ -1,5 +1,8 @@
 plugins {
     kotlin("jvm") version "2.0.20"
+//    application
+//    id("com.github.johnrengelman.shadow") version "7.1.2" // https://plugins.gradle.org/plugin/com.github.johnrengelman.shadow
+    id("org.panteleyev.jpackageplugin") version "1.7.6" // https://plugins.gradle.org/plugin/org.panteleyev.jpackageplugin
 }
 
 group = "org.example"
@@ -20,3 +23,22 @@ tasks.test {
 kotlin {
     jvmToolchain(21)
 }
+
+application {
+//    mainClass.set("mypackage.MainKt") // package mypackageかつファイル名main.ktのmain()を実行する場合
+    mainClass="MainKt"
+}
+
+
+jpackage {
+    mainJar.set(tasks.jar.flatMap { it.archiveFile }) // applicationプラグインのjarタスクを利用
+    appName = "YourAppName"
+    appVersion = "1.0.0"
+    vendor = "Your Company"
+    destination = layout.buildDirectory.dir("dist")
+
+    // Windows向け設定
+    winMenu = true
+    winShortcut = true
+}
+
