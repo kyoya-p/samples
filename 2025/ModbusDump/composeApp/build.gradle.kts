5741b02667afcbd7a3f020169d3575da67d99c2b
kyoya-p <kyoya.p@gmail.com>
2025-09-24 20:47:35 +0900

    n

---
+++ 2025/ModbusDump/composeApp/build.gradle.kts
@@ -0,0 +1,45 @@
+import org.jetbrains.compose.desktop.application.dsl.TargetFormat
+
+plugins {
+    alias(libs.plugins.kotlinMultiplatform)
+    alias(libs.plugins.composeMultiplatform)
+    alias(libs.plugins.composeCompiler)
+    alias(libs.plugins.composeHotReload)
+}
+
+kotlin {
+    jvm()
+    
+    sourceSets {
+        commonMain.dependencies {
+            implementation(compose.runtime)
+            implementation(compose.foundation)
+            implementation(compose.material3)
+            implementation(compose.ui)
+            implementation(compose.components.resources)
+            implementation(compose.components.uiToolingPreview)
+            implementation(libs.androidx.lifecycle.viewmodelCompose)
+            implementation(libs.androidx.lifecycle.runtimeCompose)
+        }
+        commonTest.dependencies {
+            implementation(libs.kotlin.test)
+        }
+        jvmMain.dependencies {
+            implementation(compose.desktop.currentOs)
+            implementation(libs.kotlinx.coroutinesSwing)
+        }
+    }
+}
+
+
+compose.desktop {
+    application {
+        mainClass = "org.example.project.MainKt"
+
+        nativeDistributions {
+            targetFormats(TargetFormat.Dmg, TargetFormat.Msi, TargetFormat.Deb)
+            packageName = "org.example.project"
+            packageVersion = "1.0.0"
+        }
+    }
+}
--- 2025/moddump/build.gradle.kts
+++ 2025/moddump/build.gradle.kts
@@ -1,8 +1,9 @@
 plugins {
-    kotlin("jvm") version "2.0.20"
+    kotlin("jvm") version "2.1.21"
     application
-//    id("com.github.johnrengelman.shadow") version "7.1.2" // https://plugins.gradle.org/plugin/com.github.johnrengelman.shadow
-    id("org.panteleyev.jpackageplugin") version "1.7.6" // https://plugins.gradle.org/plugin/org.panteleyev.jpackageplugin
+//    id("com.github.johnrengelman.shadow") version "8.1.1" // https://plugins.gradle.org/plugin/com.github.johnrengelman.shadow
+//    id("org.panteleyev.jpackageplugin") version "1.7.6" // https://plugins.gradle.org/plugin/org.panteleyev.jpackageplugin
+    id("org.beryx.jlink") version "3.1.3" // https://plugins.gradle.org/plugin/org.beryx.jlink
 }
 
 group = "org.example"
@@ -30,15 +31,21 @@ application {
 }
 
 
-jpackage {
-    mainJar.set(tasks.jar.flatMap { it.archiveFile }) // applicationプラグインのjarタスクを利用
-    appName = "YourAppName"
-    appVersion = "1.0.0"
-    vendor = "Your Company"
-    destination = layout.buildDirectory.dir("dist")
+//jpackage {
+//    mainJar.set(tasks.jar.flatMap { it.archiveFile }) // applicationプラグインのjarタスクを利用
+//    appName = "YourAppName"
+//    appVersion = "1.0.0"
+//    vendor = "Your Company"
+//    destination = layout.buildDirectory.dir("dist")
+//
+//    // Windows向け設定
+//    winMenu = true
+//    winShortcut = true
+//}
 
-    // Windows向け設定
-    winMenu = true
-    winShortcut = true
+jlink{
+    options.set(listOf("--strip-debug", "--compress", "2", "--no-header-files", "--no-man-pages"))
+    launcher {
+        name = "moddump"
+    }
 }
-
