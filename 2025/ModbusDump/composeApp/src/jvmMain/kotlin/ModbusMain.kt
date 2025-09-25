4631b06198bcf57e47abeccba6db8b05df24ac19
kk <xxxxxxx@gmail.com>
2025-09-25 21:05:52 +0900

    n

--- 2025/ModbusDump/composeApp/src/jvmMain/kotlin/ModbusMain.kt
+++ 2025/ModbusDump/composeApp/src/jvmMain/kotlin/ModbusMain.kt
@@ -1,29 +1,42 @@
 import com.ghgande.j2mod.modbus.facade.ModbusTCPMaster
+import kotlinx.coroutines.flow.flow
 
 @OptIn(ExperimentalStdlibApi::class)
 fun modbusMain(args: Array<String>) {
-    val target = args[0]
-    val start = args[1].toInt()
+    val host = args[0]
+    val offset = args[1].toInt()
     val length = args[2].toInt()
     val windowSize = args[3].toInt()
-    val master = ModbusTCPMaster(target)
+    val master = ModbusTCPMaster(host)
     master.connect()
-    for (offset in start..<start + length step windowSize) {
-        master.readMultipleRegisters(offset, windowSize)?.forEachIndexed { i, e ->
-            val ix = offset + i
-            val v = e.value
-            val c = (v shr 8).toPrintable() + (v and 0xff).toPrintable()
-            val b = v.toString(2)
-            println("$ix,${ix.toHexString().takeLast(4)},$v,${v.toHexString().takeLast(4)},\"$c\",$b")
-        }
+    master.modbusScan(offset, length, windowSize, ModbusMode.READ_HOLDING_REGISTERS).forEach {
+//        println(it.toText())
+        println(it)
     }
     master.disconnect()
 }
 
-data class Record(val address:Int, val data:Int)
-fun Record.toText() =
+fun ModbusTCPMaster.modbusScan(start: Int, length: Int, windowSize: Int, mode: ModbusMode) = sequence {
+    for (offset in start..<start + length step windowSize) {
+        readMultipleRegisters(offset, windowSize)?.forEachIndexed { i, e ->
+            yield(Record(offset + i, e.value))
+        }
+    }
+}
 
-//@OptIn(ExperimentalStdlibApi::class)
+enum class ModbusMode {
+    READ_COILS, READ_DISCRETE_INPUTS, READ_HOLDING_REGISTERS, READ_INPUT_REGISTERS
+}
+
+data class Record(val offset: Int, val data: Int)
+
+fun Record.toText(): String {
+    val v = data
+    val c = (v shr 8).toPrintable() + (v and 0xff).toPrintable()
+    val b = v.toString(2)
+    return "$offset,${offset.toHexString().takeLast(4)},$v,${v.toHexString().takeLast(4)},\"$c\",$b"
+}
+
 fun Int.toPrintable() = if (this !in 0x20..<0x80 || this == ','.code) "<${toHexString().takeLast(2)}>"
 else toChar().toString()
 
--- 2025/ModbusDump/composeApp/src/jvmMain/kotlin/UI.kt
+++ 2025/ModbusDump/composeApp/src/jvmMain/kotlin/UI.kt
@@ -1,7 +1,15 @@
+import androidx.compose.foundation.VerticalScrollbar
+import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.fillMaxHeight
 import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.foundation.layout.safeContentPadding
-import androidx.compose.foundation.text.selection.SelectionContainer
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.lazy.LazyColumn
+import androidx.compose.foundation.lazy.rememberLazyListState
+import androidx.compose.foundation.rememberScrollState
+import androidx.compose.foundation.rememberScrollbarAdapter
+import androidx.compose.foundation.verticalScroll
 import androidx.compose.material3.Button
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Text
@@ -9,36 +17,61 @@ import androidx.compose.material3.TextField
 import androidx.compose.runtime.*
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
-import org.jetbrains.compose.ui.tooling.preview.Preview
-
+import androidx.compose.ui.unit.dp
+import com.ghgande.j2mod.modbus.facade.ModbusTCPMaster
 
 @Composable
-@Preview
 fun UI() = MaterialTheme {
-    var showContent by remember { mutableStateOf(false) }
-    Column(
-        modifier = Modifier
-//            .background(MaterialTheme.colorScheme.primaryContainer)
-//            .safeContentPadding()
-//            .fillMaxSize(),
-//        horizontalAlignment = Alignment.CenterHorizontally,
-    ) {
+    Row {
         var host by remember { mutableStateOf("127.0.0.1") }
-        var adrFrom by remember { mutableStateOf(0) }
-        var adrTo by remember { mutableStateOf(1) }
+        var offset by remember { mutableStateOf(0) }
+        var length by remember { mutableStateOf(64) }
         var windowSize by remember { mutableStateOf(1) }
-        var mode by remember { mutableStateOf("") }
-        TextField(host, label = { Text("Target Address") }, onValueChange = { host = it })
-        IntField(adrFrom, label = "Data Address From", onValueChange = { adrFrom = it })
-        IntField(adrTo, label = "Data Address To", onValueChange = { adrTo = it })
-        IntField(windowSize, label = "Data Windows Size", onValueChange = { windowSize = it })
-        TextField(mode, label = { Text("Mode") }, onValueChange = { mode = it })
+        var result by remember { mutableStateOf("") }
+        Column {
+            TextField(host, label = { Text("Device address") }, onValueChange = { host = it })
+            IntField(offset, label = "Data address", onValueChange = { offset = it })
+            IntField(length, label = "# of Data items", onValueChange = { length = it })
+            IntField(windowSize, label = "Data acquisition quantity", onValueChange = { windowSize = it })
 
-        var result by remember { mutableStateOf("no-data") }
 
-        Button(onClick = { showContent = !showContent }) { Text("Dump") }
-        SelectionContainer {
-            Text(result)
+            var run by remember { mutableStateOf(false) }
+            Button(onClick = {
+                result = ""
+                run = true
+            }) { Text("Dump") }
+
+            LaunchedEffect(run) {
+                if (run) {
+                    result += """host: $host
+                    |offset: $offset
+                    |length: $length
+                    |windowSize: $windowSize
+                    |
+                """.trimMargin()
+
+                    runCatching {
+                        val master = ModbusTCPMaster(host)
+                        master.connect()
+                        master.modbusScan(offset, length, windowSize, ModbusMode.READ_HOLDING_REGISTERS).forEach {
+                            result += it.toText() + "\n"
+                        }
+                        master.disconnect()
+                    }.onFailure { result += "${it.message}\n${it.stackTraceToString()}" }
+                    run = false
+                }
+            }
+        }
+        Box(modifier = Modifier.fillMaxSize()) {
+            val state = rememberScrollState()
+            Text(text = result, modifier = Modifier.verticalScroll(state = state))
+            VerticalScrollbar(
+                modifier = Modifier
+                    .align(Alignment.CenterEnd)
+                    .fillMaxHeight()
+                    .padding(horizontal = 4.dp),
+                adapter = rememberScrollbarAdapter(scrollState = state)
+            )
         }
     }
 }
