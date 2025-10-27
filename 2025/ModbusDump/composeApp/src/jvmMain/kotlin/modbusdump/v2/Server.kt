90268ee60974451e5c67ee8943d785afada0096c
kk <kyoya.q@gmail.com>
2025-10-28 04:03:08 +0900

    n

---
+++ 2025/ModbusDump/composeApp/src/jvmMain/kotlin/modbusdump/v2/Server.kt
@@ -0,0 +1,16 @@
+package modbusdump.modbusdump.v3
+
+import com.ghgande.j2mod.modbus.procimg.SimpleInputRegister
+import com.ghgande.j2mod.modbus.procimg.SimpleProcessImage
+import com.ghgande.j2mod.modbus.slave.ModbusSlave
+import com.ghgande.j2mod.modbus.slave.ModbusSlaveFactory
+
+fun main() {
+    val slave: ModbusSlave = ModbusSlaveFactory.createTCPSlave(502, 5)
+    val spi = SimpleProcessImage(1)
+    spi.addRegister(0, SimpleInputRegister(1234))
+    slave.addProcessImage(1, spi)
+    slave.open()
+    Thread.sleep(10_000)
+    slave.close()
+}
