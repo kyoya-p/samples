415c1cdbd32037b103a0aedae2da0110fed0d411
kyoya <kyoya.xxx@gmail.com>
2026-01-12 10:37:32 +0900

    n

--- 2026/BS/tools/bsModel.main.kts
+++ 2026/BS/tools/bsModel.main.kts
@@ -12,7 +12,7 @@ data class SearchCard(
     val name: String,
     val rarity: String,
     val cost: String,
-    val type: String, // S:スピリット、U:アルティメット, B:ブラヴ、N:ネクサス、M:マジック
+    val category: String, // S:スピリット、U:アルティメット, B:ブラヴ、N:ネクサス、M:マジック
     val attribute: String, // 全色なら"赤紫緑白黄青"
     val systems: List<String>,
     val imgUrl: String,
@@ -24,14 +24,13 @@ data class Card(
     val name: String,
     val rarity: String,
     val cost: Int,
-    val reductionSymbols: List<String>,
-    val attributes: List<String>,
-    val category: String,
+    val symbols: String, // 紫シンボル1個と緑シンボル1個なら "紫1緑1", 赤神シンボル1個なら "赤神1"
+    val reductionSymbols: String, // 赤軽減１個、青軽減2個なら "赤1青2"
+    val category: String, // S:スピリット、U:アルティメット, B:ブラヴ、N:ネクサス、M:マジック
+    val attributes: String, // 全色なら"赤紫緑白黄青"
     val systems: List<String>,
-    val lvInfo: List<LvInfo>,
+    val lvInfo: List<String>, // Lv1維持コア2個でBP1000の場合、 "1,2,1000"。ネクサスの場合でLv2維持コア1個の場合、"2,1"
     val effect: String,
     val imageUrl: String
 )
-
-data class LvInfo(val level: Int, val core: Int, val bp: Int?)
 
