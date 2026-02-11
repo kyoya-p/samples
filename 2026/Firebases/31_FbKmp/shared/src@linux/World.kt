0f39686ceda88f307f18d3057523bd2af67ff7d0
kk <kyoya.xxx@gmail.com>
2026-02-12 06:44:33 +0900

    n

--- 2026/Firebases/31_FbKmp/shared/src@linux/World.kt
+++ 2026/Firebases/31_FbKmp/shared/src@linux/World.kt
@@ -1,1 +1,9 @@
-actual fun getWorld() = "Linux World"
+
+import ftxui.*
+import kotlinx.cinterop.*
+
+@OptIn(ExperimentalForeignApi::class)
+actual fun getWorld(): String {
+    hello_ftxui()
+    return "Linux World with FTXUI"
+}
