d018edc245b676d83b8b28529e1d9a1c84783175
kk <kyoya.xxx@gmail.com>
2026-02-12 21:01:14 +0900

    n

--- 2026/PAU/build.gradle.kts
+++ 2026/PAU/build.gradle.kts
@@ -5,5 +5,14 @@ plugins {
     alias(libs.plugins.kotlinMultiplatform) apply false
 
     id("org.owasp.dependencycheck") version "12.1.8" // https://plugins.gradle.org/plugin/org.owasp.dependencycheck
+    id("org.cyclonedx.bom") version "2.2.0"
+}
+
+group = "jp.wjg.shokkaa"
+version = "1.0.9"
+
+tasks.withType<org.cyclonedx.gradle.CycloneDxTask> {
+    includeConfigs.set(listOf("jvmRuntimeClasspath"))
+    skipConfigs.set(listOf("commonMainImplementation", "commonTestImplementation", "metadataCompileClasspath"))
 }
 
