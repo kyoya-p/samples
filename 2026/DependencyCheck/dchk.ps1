6f5e5d0396591606184ab5a5e7738e5087b8251d
kk <kyoya.xxx@gmail.com>
2026-02-18 07:56:03 +0900

    n

--- 2026/DependencyCheck/dchk.ps1
+++ 2026/DependencyCheck/dchk.ps1
@@ -1,6 +1,7 @@
 param($Path = ".")
 
 Invoke-WebRequest "https://github.com/jeremylong/DependencyCheck/releases/download/v12.1.0/dependency-check-12.1.0-release.zip" -OutFile "dchk.zip"
-mkdir dchk -Force | Out-Null
 tar -xf "dchk.zip" -C "dchk"
+
+
 & "dchk/dependency-check/bin/dependency-check.bat" --project "DependencyCheck" --scan "$Path" --format "HTML" --out "." --nvdApiKey $env:NVD_API_KEY --exclude "dchk/**" --disableOssIndex
